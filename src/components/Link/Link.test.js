a6cb552a529c2c0c1920b27159c779a81140c2ff
Eric Liu <ericyl.us@gmail.com>
2020-01-01 12:12:41 -0800

    test(link): use jest-dom matchers, remove snapshots

--- src/components/Link/Link.test.js
+++ src/components/Link/Link.test.js
@@ -1,33 +1,23 @@
 import { render } from '@testing-library/svelte';
 import Component from './Link.Story.svelte';
 
-describe('Link', () => {
-  function hasClass(element, className) {
-    return element.classList.contains(className);
-  }
-
-  test('default', () => {
-    const { getByText, container, rerender } = render(Component, {
-      href: '#',
-      inline: false,
-      disabled: false
-    });
-
-    const selector = '.bx--link';
-    let element = null;
-
-    element = container.querySelector(selector);
-    expect(element).toBeInTheDocument();
-    expect(element.getAttribute('href')).toEqual('#');
-    expect(hasClass(element, 'bx--link--inline')).toEqual(false);
-    expect(hasClass(element, 'bx--link--disabled')).toEqual(false);
-    expect(getByText('Link')).toBeInTheDocument();
-    expect(container.innerHTML).toMatchSnapshot();
-
-    rerender({ props: { inline: true, disabled: true } });
-    element = container.querySelector(selector);
-    expect(hasClass(element, 'bx--link--inline')).toEqual(true);
-    expect(hasClass(element, 'bx--link--disabled')).toEqual(true);
-    expect(container.innerHTML).toMatchSnapshot();
+test('Link', () => {
+  const { container, rerender } = render(Component, {
+    href: '#',
+    inline: false,
+    disabled: false
   });
+
+  const selector = '.bx--link';
+  let element = null;
+
+  element = container.querySelector(selector);
+  expect(element).toHaveAttribute('href', '#');
+  expect(element).not.toHaveClass('bx--link--inline', 'bx--link--disabled');
+  expect(element).toHaveTextContent('Link');
+
+  rerender({ props: { inline: true, disabled: true } });
+
+  element = container.querySelector(selector);
+  expect(element).toHaveClass('bx--link--inline', 'bx--link--disabled');
 });
