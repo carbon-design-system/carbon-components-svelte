26aea06c94fdb507fe7868d3597e9621b21fc14e
Eric Liu <ericyl.us@gmail.com>
2020-07-17 12:37:47 -0700

    chore: revise scripts

--- gh-pages.js
+++
@@ -1,1 +0,0 @@
-require("gh-pages").publish("storybook-static");
--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,26 @@
-import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
 
-export default ['es', 'umd'].map(format => {
-  const UMD = format === 'umd';
+export default ["es", "umd"].map((format) => {
+  const UMD = format === "umd";
 
   const output = {
     format,
     file: UMD ? pkg.main : pkg.module,
-    globals: {
-      flatpickr: 'flatpickr'
-    }
+    globals: { flatpickr: "flatpickr" },
   };
 
   if (UMD) {
-    output.name = 'carbon-components-svelte';
+    output.name = "carbon-components-svelte";
   }
 
   return {
-    input: 'src',
+    input: "src",
     output,
-    external: Object.keys(pkg.dependencies || {}),
-    plugins: [svelte(), resolve(), commonjs(), UMD && terser()]
+    external: Object.keys(pkg.dependencies),
+    plugins: [svelte(), resolve(), commonjs(), UMD && terser()],
   };
 });
