1d479eb83544970ca6c5024858d134638fe25ba2
Eric Liu <ericyl.us@gmail.com>
2020-09-07 14:51:19 -0700

    chore(examples): add routify set-up

---
+++ examples/routify/src/pages/_fallback.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { url } from "@sveltech/routify";
+  import { Row, Column, Link } from "carbon-components-svelte";
+</script>
+
+<Row>
+  <Column lg="{16}">
+    <h1>404</h1>
+    <div>
+      Page not found.
+      <Link inline href="{$url('..')}">Return home</Link>
+    </div>
+  </Column>
+</Row>
