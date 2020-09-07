c1b98ced953c8248dd35cb4f1dc078bb543d7a4a
Eric Liu <ericyl.us@gmail.com>
2020-09-07 15:35:20 -0700

    chore(examples): add rollup set-up

---
+++ examples/rollup/README.md
@@ -0,0 +1,13 @@
+# rollup
+
+> Example set-up using [Rollup](https://github.com/rollup/rollup).
+
+## Available scripts
+
+### `yarn dev`
+
+Starts the app in development mode.
+
+### `yarn build`
+
+Builds the app for production.
---
+++ examples/rollup/src/App.svelte
@@ -0,0 +1,96 @@
+<script>
+  import {
+    Content,
+    Breadcrumb,
+    BreadcrumbItem,
+    Grid,
+    Row,
+    Column,
+    Tabs,
+    TabContent,
+    Tab,
+    Select,
+    SelectItem,
+  } from "carbon-components-svelte";
+  import Header from "./components/Header.svelte";
+  import Theme from "./components/Theme.svelte";
+
+  let theme = "g10";
+</script>
+
+<style lang="scss" global>
+  @import "carbon-components-svelte/css/all";
+</style>
+
+<Theme persist bind:theme>
+  <Header />
+  <Content style="background: none; padding: 1rem">
+    <Grid>
+      <Row>
+        <Column lg="{16}">
+          <Breadcrumb noTrailingSlash aria-label="Page navigation">
+            <BreadcrumbItem href="/">Getting started</BreadcrumbItem>
+          </Breadcrumb>
+          <h1 style="margin-bottom: 1.5rem">Design &amp; build with Carbon</h1>
+        </Column>
+      </Row>
+
+      <Row>
+        <Column noGutter>
+          <Tabs aria-label="Tab navigation">
+            <Tab label="About" />
+            <Tab label="Design" />
+            <Tab label="Develop" />
+            <div slot="content" class="tabbed-content">
+              <Grid as fullWidth let:props>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md="{4}" lg="{7}">
+                      <Select
+                        labelText="Carbon theme"
+                        bind:selected="{theme}"
+                        style="margin-bottom: 1rem">
+                        <SelectItem value="white" text="White" />
+                        <SelectItem value="g10" text="Gray 10" />
+                        <SelectItem value="g90" text="Gray 90" />
+                        <SelectItem value="g100" text="Gray 100" />
+                      </Select>
+                      <p>
+                        Carbon is IBM’s open-source design system for digital
+                        products and experiences. With the IBM Design Language
+                        as its foundation, the system consists of working code,
+                        design tools and resources, human interface guidelines,
+                        and a vibrant community of contributors.
+                      </p>
+                    </Column>
+                  </Row>
+                </TabContent>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md="{4}" lg="{7}">
+                      <p>
+                        Rapidly build beautiful and accessible experiences. The
+                        Carbon kit contains all resources you need to get
+                        started.
+                      </p>
+                    </Column>
+                  </Row>
+                </TabContent>
+                <TabContent {...props}>
+                  <Row>
+                    <Column md="{4}" lg="{7}">
+                      <p>
+                        Carbon provides styles and components in Vanilla, React,
+                        Angular, Vue and Svelte for anyone building on the web.
+                      </p>
+                    </Column>
+                  </Row>
+                </TabContent>
+              </Grid>
+            </div>
+          </Tabs>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+</Theme>
