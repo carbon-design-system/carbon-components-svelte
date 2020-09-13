a85dade8ffbb144b06d619581d5d37065b32499c
Eric Liu <ericyl.us@gmail.com>
2020-09-13 12:43:23 -0700

    build(scripts): publish examples using gh-pages

---
+++ scripts/publish-examples.js
@@ -0,0 +1,31 @@
+const ghpages = require("gh-pages");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const ghPublish = promisify(ghpages.publish);
+
+async function publishExample(name) {
+  try {
+    const folder_path = path.join("examples", name);
+    await ghPublish(folder_path, {
+      branch: name,
+      dotfiles: true,
+      history: false,
+    });
+    console.log("Published example:", name);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  ghpages.clean();
+
+  await publishExample("rollup");
+  await publishExample("rollup-typescript");
+  await publishExample("routify");
+  await publishExample("sapper");
+  await publishExample("svite");
+  await publishExample("webpack");
+})();
