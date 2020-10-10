d0defc556f7fba17dc27db5b1f9b7230c95f7d34
Eric Liu <ericyl.us@gmail.com>
2020-10-09 18:54:12 -0700

    docs(build): link library in build script

--- docs/scripts/build.js
+++ docs/scripts/build.js
@@ -1,8 +1,11 @@
 const shell = require("shelljs");
 
 shell.cd("../");
-shell.exec("yarn install");
+shell.exec("yarn");
 shell.exec("yarn build:css");
+shell.exec("yarn link");
 shell.cd("docs");
+shell.exec("yarn link 'carbon-components-svelte'");
+shell.exec("yarn");
 shell.exec("routify run -b");
 shell.exec("svite build");
