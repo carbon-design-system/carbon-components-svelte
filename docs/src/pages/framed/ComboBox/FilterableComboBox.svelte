3b24d21168117a99d24ba11d9425c055e45cfd18
Eric Liu <ericyl.us@gmail.com>
2020-10-14 15:14:54 -0700

    docs(combo-box): add filterable example

---
+++ docs/src/pages/framed/ComboBox/FilterableComboBox.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { ComboBox } from "carbon-components-svelte";
+
+  function shouldFilterItem(item, value) {
+    if (!value) return true;
+    return item.text.toLowerCase().includes(value.toLowerCase());
+  }
+</script>
+
+<ComboBox
+  titleText="Contact"
+  placeholder="Select contact method"
+  items="{[{ id: '0', text: 'Slack' }, { id: '1', text: 'Email' }, { id: '2', text: 'Fax' }]}"
+  shouldFilterItem="{shouldFilterItem}"
+/>
