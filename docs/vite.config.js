9aee535a28ec6269040384bd2d3c45d680a61076
Eric Liu <ericyl.us@gmail.com>
2020-10-14 14:43:16 -0700

    docs(build): omit flatpickr l10n from vite optimizable deps

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -2,7 +2,6 @@ module.exports = {
   optimizeDeps: {
     include: [
       "flatpickr",
-      "flatpickr/dist/l10n/index.js",
       "flatpickr/dist/plugins/rangePlugin",
       "clipboard-copy",
     ],
