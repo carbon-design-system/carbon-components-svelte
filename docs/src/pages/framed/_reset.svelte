a02be58ac82e1a2ea0e24ca46037c242513f3495
Eric Liu <ericyl.us@gmail.com>
2020-10-15 17:27:06 -0700

    docs(framed): add min height, padding to framed components

--- docs/src/pages/framed/_reset.svelte
+++ docs/src/pages/framed/_reset.svelte
@@ -4,4 +4,12 @@
   $: document.documentElement.setAttribute("theme", currentTheme);
 </script>
 
+<style>
+  :global(body) {
+    min-height: 100vh;
+    width: 100%;
+    padding: var(--cds-spacing-06) var(--cds-spacing-05);
+  }
+</style>
+
 <slot />
