bc29d9ee0dd6140e4df8ef5c97f62b76f3914c19
Eric Liu <ericyl.us@gmail.com>
2020-11-22 09:25:05 -0800

    docs(radio-button): add reactive example

---
+++ docs/src/pages/framed/RadioButton/ProgrammaticRadioButton.svelte
@@ -0,0 +1,36 @@
+<script>
+  import {
+    ButtonSet,
+    Button,
+    FormGroup,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
+
+  let plan = "standard";
+</script>
+
+<FormGroup legendText="Storage tier (disk)">
+  <RadioButtonGroup bind:selected="{plan}">
+    <RadioButton labelText="Free (1 GB)" value="free" />
+    <RadioButton labelText="Standard (10 GB)" value="standard" />
+    <RadioButton labelText="Pro (128 GB)" value="pro" />
+  </RadioButtonGroup>
+</FormGroup>
+
+<ButtonSet>
+  {#each ['free', 'standard', 'pro'] as value}
+    <Button
+      on:click="{() => {
+        plan = value;
+      }}"
+    >
+      Select "{value}"
+    </Button>
+  {/each}
+</ButtonSet>
+
+<div style="margin: var(--cds-layout-01) 0">
+  Selected plan:
+  <strong>{plan}</strong>
+</div>
