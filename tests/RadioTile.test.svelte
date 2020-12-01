ed9d109b33bbdd947b0ca8108556b6486fd56687
Richard O'flynn <richard.oflynn@brighter-software.co.uk>
2020-12-01 00:47:54 +0000

    Fix failing RadioTile test

--- tests/RadioTile.test.svelte
+++ tests/RadioTile.test.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-  import { RadioTileGroup, RadioTile } from "../types";
+  import { TileGroup, RadioTile } from "../types";
 </script>
 
-<RadioTileGroup legend="Service pricing tiers">
+<TileGroup legend="Service pricing tiers">
   <RadioTile value="0" checked>Lite plan</RadioTile>
   <RadioTile value="1">Standard plan</RadioTile>
   <RadioTile value="2">Plus plan</RadioTile>
-</RadioTileGroup>
-
-<RadioTileGroup legend="Service pricing tiers">
-  <RadioTile value="0" checked>Lite plan</RadioTile>
-  <RadioTile value="1">Standard plan</RadioTile>
-  <RadioTile value="2">Plus plan</RadioTile>
-</RadioTileGroup>
+</TileGroup>
