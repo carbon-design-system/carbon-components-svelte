e0740a5f9434286910740512fedfe9b5329b1340
Eric Liu <ericyl.us@gmail.com>
2020-12-05 14:40:19 -0800

    refactor(search): resolve svelte file paths

--- src/Search/Search.svelte
+++ src/Search/Search.svelte
@@ -70,9 +70,9 @@
   export let ref = null;
 
   import { createEventDispatcher } from "svelte";
-  import Close16 from "carbon-icons-svelte/lib/Close16";
-  import Close20 from "carbon-icons-svelte/lib/Close20";
-  import Search16 from "carbon-icons-svelte/lib/Search16";
+  import Close16 from "carbon-icons-svelte/lib/Close16/Close16.svelte";
+  import Close20 from "carbon-icons-svelte/lib/Close20/Close20.svelte";
+  import Search16 from "carbon-icons-svelte/lib/Search16/Search16.svelte";
   import SearchSkeleton from "./SearchSkeleton.svelte";
 
   const dispatch = createEventDispatcher();
