72ac7192aed0c7a132d01ed3ce6ceaf8110548ea
Eric Liu <ericyl.us@gmail.com>
2020-12-05 15:24:30 -0800

    docs(toggle): add reactive example

---
+++ docs/src/pages/framed/Toggle/ToggleReactive.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Toggle, Button } from "carbon-components-svelte";
+
+  let toggled = true;
+</script>
+
+<style>
+  div {
+    margin-top: var(--cds-spacing-05);
+  }
+</style>
+
+<Toggle labelText="Push notifications" bind:toggled />
+
+<div>
+  <Button size="small" on:click="{() => (toggled = !toggled)}">
+    Toggle
+    {toggled ? 'off' : 'on'}
+  </Button>
+</div>
+
+<div>Toggled: {toggled}</div>
