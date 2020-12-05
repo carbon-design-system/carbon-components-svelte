9c710e9e9ea28acbf7d772d9203a29d4da10c3ff
Eric Liu <ericyl.us@gmail.com>
2020-12-05 14:40:41 -0800

    docs(Search): add reactive example

---
+++ docs/src/pages/framed/Search/SearchReactive.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Search, ButtonSet, Button } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<style>
+  div {
+    margin-top: var(--cds-spacing-05);
+  }
+</style>
+
+<Search bind:value />
+
+<div>
+  <ButtonSet>
+    <Button
+      size="small"
+      disabled="{value === 'Cloud functions'}"
+      on:click="{() => (value = 'Cloud functions')}"
+    >
+      Set value
+    </Button>
+    <Button
+      kind="ghost"
+      size="small"
+      disabled="{value.length === 0}"
+      on:click="{() => (value = '')}"
+    >
+      Clear value
+    </Button>
+  </ButtonSet>
+</div>
+
+<div>Value: {value}</div>
