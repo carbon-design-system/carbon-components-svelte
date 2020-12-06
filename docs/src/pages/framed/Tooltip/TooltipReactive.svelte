5c3c70e249a43d72df36dfd2c98579b60563b186
Eric Liu <ericyl.us@gmail.com>
2020-12-06 04:51:51 -0800

    docs(tooltip): add reactive example, hidden icon example

---
+++ docs/src/pages/framed/Tooltip/TooltipReactive.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Tooltip, Button } from "carbon-components-svelte";
+
+  let open = true;
+</script>
+
+<style>
+  div {
+    margin-top: var(--cds-spacing-05);
+  }
+</style>
+
+<Tooltip bind:open triggerText="Resource list" align="start">
+  <p>Resources are provisioned based on your account's organization.</p>
+</Tooltip>
+
+<div style="margin-top: var(--cds-spacing-12);">
+  <Button size="small" on:click="{() => (open = !open)}">
+    {open ? 'Close tooltip' : 'Open tooltip'}
+  </Button>
+</div>
+
+<div>Open: {open}</div>
