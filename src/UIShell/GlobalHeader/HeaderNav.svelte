b1b483472f860a98169e8a8f5735efd9b06856b5
Eric Liu <ericyl.us@gmail.com>
2021-01-21 14:34:11 -0800

    fix(ui-shell): deprecate ariaLabel prop in HeaderNav
    - remove menubar role from HeaderNav
    - obtain "aria-label" and "aria-labelledby" from $$props

--- src/UIShell/GlobalHeader/HeaderNav.svelte
+++ src/UIShell/GlobalHeader/HeaderNav.svelte
@@ -1,17 +1,19 @@
 <script>
   /**
    * Specify the ARIA label for the nav
+   * @deprecated use "aria-label" instead
    * @type {string}
    */
   export let ariaLabel = undefined;
+
+  $: props = {
+    "aria-label": ariaLabel || $$props["aria-label"],
+    "aria-labelledby": $$props["aria-labelledby"],
+  };
 </script>
 
-<nav aria-label="{ariaLabel}" class:bx--header__nav="{true}" {...$$restProps}>
-  <ul
-    role="menubar"
-    aria-label="{ariaLabel}"
-    class:bx--header__menu-bar="{true}"
-  >
+<nav {...props} class:bx--header__nav="{true}" {...$$restProps}>
+  <ul {...props} class:bx--header__menu-bar="{true}">
     <slot />
   </ul>
 </nav>
