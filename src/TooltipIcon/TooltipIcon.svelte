6b75607dce15c4c2ef30df9e340c406147ae1d97
Eric Liu <ericyl.us@gmail.com>
2021-02-03 06:10:15 -0800

    feat(tooltip-icon): make tooltipText slottable

--- src/TooltipIcon/TooltipIcon.svelte
+++ src/TooltipIcon/TooltipIcon.svelte
@@ -1,5 +1,8 @@
 <script>
-  /** Specify the tooltip text */
+  /**
+   * Specify the tooltip text.
+   * Alternatively, use the "text" slot
+   */
   export let tooltipText = "";
 
   /**
@@ -56,6 +59,8 @@
     hidden = false;
   }}"
 >
-  <span id="{id}" class:bx--assistive-text="{true}">{tooltipText}</span>
+  <span id="{id}" class:bx--assistive-text="{true}"
+    ><slot name="text">{tooltipText}</slot></span
+  >
   <slot />
 </button>
