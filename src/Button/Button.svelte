1cc491a9ac75facc3b2a9c8898cf90bfafc74e82
Eric Liu <ericyl.us@gmail.com>
2021-02-17 06:06:59 -0800

    chore: patch prettier-plugin-svelte

--- src/Button/Button.svelte
+++ src/Button/Button.svelte
@@ -122,49 +122,47 @@
     on:mouseenter
     on:mouseleave
   />
+{:else if as}
+  <slot props="{buttonProps}" />
+{:else if href && !disabled}
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <a
+    bind:this="{ref}"
+    {...buttonProps}
+    on:click
+    on:mouseover
+    on:mouseenter
+    on:mouseleave
+  >
+    {#if hasIconOnly}
+      <span class:bx--assistive-text="{true}">{iconDescription}</span>
+    {/if}
+    <slot />
+    <svelte:component
+      this="{icon}"
+      aria-hidden="true"
+      class="bx--btn__icon"
+      aria-label="{iconDescription}"
+    />
+  </a>
 {:else}
-  {#if as}
-    <slot props="{buttonProps}" />
-  {:else if href && !disabled}
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <a
-      bind:this="{ref}"
-      {...buttonProps}
-      on:click
-      on:mouseover
-      on:mouseenter
-      on:mouseleave
-    >
-      {#if hasIconOnly}
-        <span class:bx--assistive-text="{true}">{iconDescription}</span>
-      {/if}
-      <slot />
-      <svelte:component
-        this="{icon}"
-        aria-hidden="true"
-        class="bx--btn__icon"
-        aria-label="{iconDescription}"
-      />
-    </a>
-  {:else}
-    <button
-      bind:this="{ref}"
-      {...buttonProps}
-      on:click
-      on:mouseover
-      on:mouseenter
-      on:mouseleave
-    >
-      {#if hasIconOnly}
-        <span class:bx--assistive-text="{true}">{iconDescription}</span>
-      {/if}
-      <slot />
-      <svelte:component
-        this="{icon}"
-        aria-hidden="true"
-        class="bx--btn__icon"
-        aria-label="{iconDescription}"
-      />
-    </button>
-  {/if}
+  <button
+    bind:this="{ref}"
+    {...buttonProps}
+    on:click
+    on:mouseover
+    on:mouseenter
+    on:mouseleave
+  >
+    {#if hasIconOnly}
+      <span class:bx--assistive-text="{true}">{iconDescription}</span>
+    {/if}
+    <slot />
+    <svelte:component
+      this="{icon}"
+      aria-hidden="true"
+      class="bx--btn__icon"
+      aria-label="{iconDescription}"
+    />
+  </button>
 {/if}
