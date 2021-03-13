b5161b6b9ef1e847a4bfc3537ab8c0c061b7ea4c
Eric Y Liu <ericyl.us@gmail.com>
2021-03-13 13:58:24 -0800

    test: add ImageLoader

---
+++ tests/ImageLoader.test.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { ImageLoader } from "../types";
+
+  let loading = false;
+  let loaded = false;
+  let error = false;
+</script>
+
+<ImageLoader
+  bind:loading
+  bind:loaded
+  bind:error
+  fadeIn
+  ratio="16x9"
+  src=""
+  on:load="{() => {}}"
+  on:error="{() => {}}"
+/>
