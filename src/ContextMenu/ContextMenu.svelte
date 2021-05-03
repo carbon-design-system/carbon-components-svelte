6b95fb6db0bb7db545f102659c3504a3a1f52c42
Moritz Bischof <moritz.bischof1@gmail.com>
2021-05-03 19:50:55 +0200

    add JSDocs

--- src/ContextMenu/ContextMenu.svelte
+++ src/ContextMenu/ContextMenu.svelte
@@ -4,16 +4,26 @@
   /**
    * Set to `true` to open the menu
    * Either `x` and `y` must be greater than zero
+   * @type {boolean}
    */
   export let open = false;
 
-  /** Specify the horizontal offset of the menu position */
+  /**
+   * Specify the horizontal offset of the menu position
+   * @type {number}
+   */
   export let x = 0;
 
-  /** Specify the vertical offset of the menu position */
+  /**
+   * Specify the vertical offset of the menu position
+   * @type {number}
+   */
   export let y = 0;
 
-  /** Obtain a reference to the unordered list HTML element */
+  /**
+   * Obtain a reference to the unordered list HTML element
+   * @type {HTMLUListElement | null}
+   */
   export let ref = null;
 
   import { getContext } from "svelte";
