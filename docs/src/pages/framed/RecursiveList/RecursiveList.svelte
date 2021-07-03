0cc3ede6e4c31098c0574f85786d6c428ab3d433
Eric Y Liu <ericyl.us@gmail.com>
2021-07-03 04:06:23 -0700

    docs(recursive-list): add full examples

---
+++ docs/src/pages/framed/RecursiveList/RecursiveList.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { RecursiveList } from "carbon-components-svelte";
+
+  const children = [
+    {
+      text: "Item 1",
+      children: [
+        {
+          text: "Link",
+          href: "/",
+          children: [
+            {
+              html: "<h5>Hello world</h5>",
+              children: [{ text: "Item 1a" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Item 2",
+      children: [
+        {
+          text: "Item 2 content",
+        },
+      ],
+    },
+    {
+      text: "Item 3",
+    },
+  ];
+</script>
+
+<RecursiveList children="{children}" />
---
+++ docs/src/pages/framed/RecursiveList/RecursiveListOrderedNative.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { RecursiveList } from "carbon-components-svelte";
+
+  const items = [
+    {
+      text: "Item 1",
+      items: [
+        {
+          text: "Link",
+          href: "/",
+          items: [
+            {
+              html: "<h5>Hello world</h5>",
+              items: [{ text: "Item 1a" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Item 2",
+      items: [
+        {
+          text: "Item 2 content",
+        },
+      ],
+    },
+    {
+      text: "Item 3",
+    },
+  ];
+</script>
+
+<RecursiveList type="ordered-native" items="{items}" />
