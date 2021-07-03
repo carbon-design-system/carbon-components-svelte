b74a7494b5f8a2f823aa8f4bcb754c56458e5364
Eric Y Liu <ericyl.us@gmail.com>
2021-07-03 04:06:43 -0700

    test(recursive-list): add types test

---
+++ tests/RecursiveList.test.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { RecursiveList } from "../types";
+
+  const children = [
+    {
+      text: "Item 1",
+      children: [
+        {
+          text: "Link",
+          href: "/",
+          children: [
+            {
+              html: "<h5>Hello world</h5>",
+              children: [{ text: "Item 1a" }],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Item 2",
+      children: [
+        {
+          text: "Item 2 content",
+        },
+      ],
+    },
+    {
+      text: "Item 3",
+    },
+  ];
+</script>
+
+<RecursiveList type="ordered" nested children="{children}" />
