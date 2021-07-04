01ee8697dad47d12af61517d6db8ff36ca7f7758
Eric Y Liu <ericyl.us@gmail.com>
2021-07-04 06:26:16 -0700

    docs(composed-modal): add button to re-open modal

--- docs/src/pages/framed/Modal/3ButtonComposedModal.svelte
+++ docs/src/pages/framed/Modal/3ButtonComposedModal.svelte
@@ -1,5 +1,6 @@
 <script>
   import {
+    Button,
     ComposedModal,
     ModalHeader,
     ModalBody,
@@ -11,7 +12,9 @@
   let checked = false;
 </script>
 
-<ComposedModal bind:open>
+<Button on:click="{() => (open = true)}">Review changes</Button>
+
+<ComposedModal bind:open on:submit="{() => (open = false)}">
   <ModalHeader label="Changes" title="Confirm changes" />
   <ModalBody hasForm>
     <Checkbox labelText="I have reviewed the changes" bind:checked />
