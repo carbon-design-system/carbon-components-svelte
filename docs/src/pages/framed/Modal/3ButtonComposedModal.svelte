8bf2cf48398b7590fd5fd09f097d703a629f516d
Eric Y Liu <ericyl.us@gmail.com>
2021-07-04 06:15:11 -0700

    docs(modal): add multiple secondary button example for ComposedModal

---
+++ docs/src/pages/framed/Modal/3ButtonComposedModal.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {
+    ComposedModal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Checkbox,
+  } from "carbon-components-svelte";
+
+  let open = true;
+  let checked = false;
+</script>
+
+<ComposedModal bind:open>
+  <ModalHeader label="Changes" title="Confirm changes" />
+  <ModalBody hasForm>
+    <Checkbox labelText="I have reviewed the changes" bind:checked />
+  </ModalBody>
+  <ModalFooter
+    primaryButtonText="Proceed"
+    primaryButtonDisabled="{!checked}"
+    secondaryButtons="{[{ text: 'Cancel' }, { text: 'Review' }]}"
+    on:click:button--secondary="{({ detail }) => {
+      if (detail.text === 'Cancel') open = false;
+      if (detail.text === 'Review') console.log('Review');
+    }}"
+  />
+</ComposedModal>
