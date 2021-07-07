d03909e26cd51d9b68014d98a74fc09429204e62
Eric Y Liu <ericyl.us@gmail.com>
2021-07-07 12:25:21 -0700

    docs(breakpoint): update docs

--- docs/src/pages/framed/Breakpoint/Breakpoint.svelte
+++ docs/src/pages/framed/Breakpoint/Breakpoint.svelte
@@ -7,8 +7,17 @@
 
 <Breakpoint bind:size on:match="{(e) => (events = [...events, e.detail])}" />
 
+<p>Resize the width of your browser.</p>
+<h6>Current breakpoint size</h6>
 <h1>{size}</h1>
 
 <pre>
-    {JSON.stringify(events,null, 2)}
-    </pre>
+  {JSON.stringify(events,null, 2)}
+</pre>
+
+<style>
+  p,
+  h1 {
+    margin-bottom: var(--cds-spacing-08);
+  }
+</style>
