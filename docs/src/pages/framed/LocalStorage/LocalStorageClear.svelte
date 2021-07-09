20577a013208846e30ca2f32069e2bf0ffc2749f
Eric Y Liu <ericyl.us@gmail.com>
2021-07-09 12:18:35 -0700

    docs(local-storage): simplify clear example

--- docs/src/pages/framed/LocalStorage/LocalStorageClear.svelte
+++ docs/src/pages/framed/LocalStorage/LocalStorageClear.svelte
@@ -3,41 +3,15 @@
 
   let storage;
   let toggled = false;
-  let events = [];
 
   $: document.documentElement.setAttribute("theme", toggled ? "g100" : "white");
 </script>
 
-<LocalStorage
-  bind:this="{storage}"
-  key="dark-mode"
-  bind:value="{toggled}"
-  on:save="{() => {
-    events = [...events, { event: 'on:save' }];
-  }}"
-  on:update="{({ detail }) => {
-    events = [...events, { event: 'on:update', detail }];
-  }}"
-/>
+<LocalStorage bind:this="{storage}" bind:value="{toggled}" />
 
 <Toggle size="sm" labelText="Dark mode" bind:toggled />
 
 <br />
 <br />
 
-<Button
-  size="small"
-  on:click="{() => {
-    events = [];
-    storage.clearAll();
-  }}"
->
-  Clear local storage
-</Button>
-
-<br />
-<br />
-
-<pre>
-    {JSON.stringify(events, null, 2)}
-  </pre>
+<Button size="small" on:click="{storage.clearAll}">Clear local storage</Button>
