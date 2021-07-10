a97f72d3e3e2cf78d4bb76c340ca6175c9e4ce0b
Eric Y Liu <ericyl.us@gmail.com>
2021-07-10 15:31:39 -0700

    docs(theme): add examples

--- docs/src/pages/framed/Theme/Theme.svelte
+++ docs/src/pages/framed/Theme/Theme.svelte
@@ -1,5 +1,17 @@
 <script>
-  import { Theme } from "carbon-components-svelte";
+  import {
+    Theme,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
+
+  let theme = "g90";
 </script>
 
-<Theme theme="g90" />
+<Theme bind:theme />
+
+<RadioButtonGroup legendText="Carbon theme" bind:selected="{theme}">
+  {#each ["white", "g10", "g80", "g90", "g100"] as value}
+    <RadioButton labelText="{value}" value="{value}" />
+  {/each}
+</RadioButtonGroup>
--- docs/src/pages/framed/Theme/ThemeTokens.svelte
+++ docs/src/pages/framed/Theme/ThemeTokens.svelte
@@ -2,6 +2,13 @@
   import { Theme, Button } from "carbon-components-svelte";
 </script>
 
-<Theme theme="g90" tokens="{{ 'button-primary': 'violet' }}" />
+<Theme
+  theme="g90"
+  tokens="{{
+    'interactive-01': '#d02670',
+    'hover-primary': '#ee5396',
+    'active-primary': '#9f1853',
+  }}"
+/>
 
 <Button>Primary button</Button>
