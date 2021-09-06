dfc00f6668435f43b906e25ec936286286625c64
Rafael Dessotti <42934425+rafadess@users.noreply.github.com>
2021-09-06 13:47:48 -0300

    Clear ComboBox Programaticaly (#804)
    * Added clear function
    
    * Dispatching clear event
    
    * Exporting function clear
    
    * Added example

--- src/ComboBox/ComboBox.svelte
+++ src/ComboBox/ComboBox.svelte
@@ -123,6 +123,14 @@
     highlightedIndex = index;
   }
 
+  export function clear() {
+    selectedIndex = -1;
+    highlightedIndex = -1;
+    open = false;
+    inputValue = "";
+    ref?.focus();
+  }
+
   afterUpdate(() => {
     if (open) {
       ref.focus();
@@ -140,6 +148,9 @@
   $: if (selectedIndex > -1) {
     selectedId = items[selectedIndex].id;
     dispatch("select", { selectedId, selectedIndex, selectedItem });
+  } else {
+    clear();
+    dispatch("clear");
   }
   $: ariaLabel = $$props["aria-label"] || "Choose an item";
   $: menuId = `menu-${id}`;
@@ -286,13 +297,7 @@
       {#if inputValue}
         <ListBoxSelection
           on:clear
-          on:clear="{() => {
-            selectedIndex = -1;
-            highlightedIndex = -1;
-            open = false;
-            inputValue = '';
-            ref.focus();
-          }}"
+          on:clear="{clear}"
           translateWithId="{translateWithId}"
           disabled="{disabled}"
           open="{open}"
