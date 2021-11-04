db520b99dc54d8e32ef1809713db0c63846b1f31
Nicco <hi@nicco.io>
2021-11-04 23:51:57 +0000

    move to frame

---
+++ docs/src/pages/framed/DatePicker/DatePickerModal.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Modal, DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<Modal>
+  <DatePicker flatpickrProps="{{ static: true }}">
+    <DatePickerInput />
+  </DatePicker>"/>
+</Modal>
