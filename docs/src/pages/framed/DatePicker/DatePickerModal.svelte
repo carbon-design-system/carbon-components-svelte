a317c7ed52fc0fd790424a1f4bdd3cc6d6705a4b
Nicco <hi@nicco.io>
2021-11-05 18:02:56 +0100

    document static property on datepicker modal (#879)
    * document static property on datepicker modal
    
    * move to frame
    
    * docs(date-picker): add missing props to DatePickerModal.svelte
    
    Co-authored-by: metonym <ericyl.us@gmail.com>

---
+++ docs/src/pages/framed/DatePicker/DatePickerModal.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Modal, DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<Modal open modalHeading="DatePicker">
+  <DatePicker datePickerType="single" flatpickrProps="{{ static: true }}">
+    <DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
+  </DatePicker>
+</Modal>
