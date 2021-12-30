f0cf4e7ba54c2dd0787231c110c68815ba73614b
Gregor Wassmann <gregor.wassmann@gmail.com>
2021-12-30 22:31:33 +0100

    Enhance Checkbox to use bind:group (#947)
    * Enhance Checkbox to use bind:group
    
    * Use custom logic
    
    * Move multiple checkboxes above skeleton
    
    * Incorprate PR feedback
    
    * Any instead of string

---
+++ docs/src/pages/framed/Checkbox/MultipleCheckboxes.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Checkbox } from "carbon-components-svelte";
+
+  let options = ["Apple", "Banana", "Coconut"];
+  let selection = options.slice(0, 2);
+</script>
+
+{#each options as option}
+  <Checkbox bind:group="{selection}" labelText="{option}" value="{option}" />
+{/each}
+
+<div style="margin: var(--cds-layout-01) 0">
+  Selected options:
+  <strong>{selection.join(", ")}</strong>
+</div>
