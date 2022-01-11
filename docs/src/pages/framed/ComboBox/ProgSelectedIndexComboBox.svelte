11bb2a3b3898c5e5931490c85d8143d6d769fc8e
István Pató <istvan.pato@gmail.com>
2022-01-11 12:59:44 +0100

    fix(ComboBox): programmatically set selected index

---
+++ docs/src/pages/framed/ComboBox/ProgSelectedIndexComboBox.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { ComboBox, Button } from "carbon-components-svelte";
+  let selectedIndex = 1;
+</script>
+
+<ComboBox
+  titleText="Contact"
+  placeholder="Select contact method"
+  bind:selectedIndex
+  items="{[
+    { id: '0', text: 'Slack' },
+    { id: '1', text: 'Email' },
+    { id: '2', text: 'Fax' },
+  ]}"
+/>
+<br />
+<Button on:click="{() => (selectedIndex = -1)}">Set to -1 (unselected)</Button>
+<Button on:click="{() => (selectedIndex = 2)}">Set to 2 (Fax)</Button>
