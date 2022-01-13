95a327f60d1bd620447a207e589e33090ec8b299
metonym <ericyl.us@gmail.com>
2022-01-12 16:07:03 -1000

    docs(search): refactor "Expandable variant" to demonstrate reactivity

---
+++ docs/src/pages/framed/Search/SearchExpandableReactive.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Search } from "carbon-components-svelte";
+
+  let expanded = false;
+</script>
+
+<Search expandable bind:expanded on:expand on:collapse />
+
+<br />
+
+<strong>Expanded:</strong>
+{expanded}
