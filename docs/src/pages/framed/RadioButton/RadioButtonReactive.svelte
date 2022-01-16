58de79d66bafb2a5e9dd7dc389aaaae5b7a11603
Eric Liu <ericyl.us@gmail.com>
2022-01-16 15:06:29 -0800

    docs(radio-button): simplify reactive example (#1012)
    * docs(radio-button): simplify reactive radio button example
    
    * docs(radio-button): rename to "Reactive example"

---
+++ docs/src/pages/framed/RadioButton/RadioButtonReactive.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {
+    Button,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
+
+  const plans = ["Free (1 GB)", "Standard (10 GB)", "Pro (128 GB)"];
+
+  let plan = plans[1];
+</script>
+
+<RadioButtonGroup legendText="Storage tier (disk)" bind:selected="{plan}">
+  {#each plans as value}
+    <RadioButton labelText="{value}" value="{value}" />
+  {/each}
+</RadioButtonGroup>
+
+<div style="margin: var(--cds-layout-01) 0">
+  {#each plans as value}
+    <Button
+      kind="secondary"
+      disabled="{plan === value}"
+      on:click="{() => (plan = value)}"
+    >
+      Select "{value}"
+    </Button>
+  {/each}
+</div>
+
+Selected plan:
+<strong>{plan}</strong>
