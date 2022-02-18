2f8d80f9f73b97b386debc5dd5507d4d52f93d24
Ricky de Laveaga <git@rdela.com>
2022-02-17 19:20:04 -0800

    chore(examples): remove config.kit.target from sveltekit (#1105)

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -3,9 +3,6 @@ import { optimizeImports } from "carbon-preprocess-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [optimizeImports()],
-  kit: {
-    target: "#svelte",
-  },
 };
 
 export default config;
