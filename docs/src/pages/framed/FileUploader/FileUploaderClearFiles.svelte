6dc47c377a0ca492a15b4b55f17a304878a15993
Eric Liu <ericyl.us@gmail.com>
2022-02-21 14:06:29 -0800

    docs(file-uploader): add "Clear files" example

---
+++ docs/src/pages/framed/FileUploader/FileUploaderClearFiles.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { FileUploader, Button } from "carbon-components-svelte";
+
+  let fileUploader;
+  let files = [];
+</script>
+
+<FileUploader
+  bind:this="{fileUploader}"
+  multiple
+  labelTitle="Upload files"
+  buttonLabel="Add files"
+  status="complete"
+  bind:files
+/>
+
+<br />
+
+<Button
+  kind="tertiary"
+  disabled="{!files.length}"
+  on:click="{fileUploader.clearFiles}"
+>
+  Clear (programmatic)
+</Button>
+<Button
+  kind="tertiary"
+  disabled="{!files.length}"
+  on:click="{() => (files = [])}"
+>
+  Clear (two-way binding)
+</Button>
