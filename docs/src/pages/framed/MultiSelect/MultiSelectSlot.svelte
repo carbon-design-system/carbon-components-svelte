453ac707b9c96b8d9e6f70075ed99ce1f621bc0e
Eric Liu <ericyl.us@gmail.com>
2022-03-19 09:05:46 -0700

    docs(multi-select): add "Custom slot" example

---
+++ docs/src/pages/framed/MultiSelect/MultiSelectSlot.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { MultiSelect } from "carbon-components-svelte";
+</script>
+
+<MultiSelect
+  titleText="Contact"
+  label="Select contact methods..."
+  items="{[
+    { id: '0', text: 'Slack' },
+    { id: '1', text: 'Email' },
+    { id: '2', text: 'Fax' },
+  ]}"
+  let:item
+  let:index
+>
+  <div>
+    <strong>{item.text}</strong>
+  </div>
+  <div>
+    id: {item.id} - index:
+    {index}
+  </div>
+</MultiSelect>
+
+<style>
+  :global(.bx--list-box__menu-item, .bx--list-box__menu-item__option) {
+    height: auto;
+  }
+
+  :global(.bx--checkbox-label-text) {
+    display: block;
+  }
+</style>
