f820f001c040dfe259c6ef4b561505c915c2fae7
Eric Liu <ericyl.us@gmail.com>
2022-04-17 11:06:48 -0700

    chore(examples): bump deps [ci skip]

--- examples/sveltekit/src/routes/index.svelte
+++ examples/sveltekit/src/routes/index.svelte
@@ -1,5 +1,12 @@
 <script>
-  import { Button } from "carbon-components-svelte";
+  import { Button, truncate, breakpoints } from "carbon-components-svelte";
+  import { Airplane } from "carbon-pictograms-svelte";
 </script>
 
 <Button>Primary button</Button>
+
+<Airplane />
+
+<div use:truncate>Text...</div>
+
+{JSON.stringify(breakpoints)}
--- examples/webpack/src/App.svelte
+++ examples/webpack/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import "carbon-components-svelte/css/white.css";
-  import { Button } from "carbon-components-svelte";
+  import { Button, HeaderGlobalAction } from "carbon-components-svelte";
 </script>
 
+<HeaderGlobalAction />
 <Button>Primary button</Button>
