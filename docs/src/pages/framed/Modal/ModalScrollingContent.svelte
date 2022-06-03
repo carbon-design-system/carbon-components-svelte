3ef64fb7b38565f6edd6e7dc3347d8112519879b
Eric Liu <ericyl.us@gmail.com>
2022-06-02 19:08:44 -0700

    docs(modal): add example "Has scrolling content"

---
+++ docs/src/pages/framed/Modal/ModalScrollingContent.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Modal } from "carbon-components-svelte";
+</script>
+
+<Modal open passiveModal modalHeading="About Cloudant" hasScrollingContent>
+  <p>
+    Cloudant is a fully managed, distributed database optimized for heavy
+    workloads and fast-growing web and mobile apps, IBM Cloudant is available as
+    an IBM Cloud® service with a 99.99% SLA.
+  </p>
+  <br />
+  <p>
+    The database elastically scales throughput and storage, and its API and
+    replication protocols are compatible with Apache CouchDB for hybrid or
+    multicloud architectures.
+  </p>
+</Modal>
