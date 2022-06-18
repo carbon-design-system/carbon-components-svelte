ecc4e521bb50c501d28b4c72421c5c0112320530
Eric Liu <ericyl.us@gmail.com>
2022-06-18 06:12:19 -0700

    feat(slider): add fullWidth prop

--- src/Slider/Slider.svelte
+++ src/Slider/Slider.svelte
@@ -35,6 +35,12 @@
   /** Set to `true` to hide the text input */
   export let hideTextInput = false;
 
+  /**
+   * Set to `true` for the slider to span
+   * the full width of its containing element.
+   */
+  export let fullWidth = false;
+
   /** Set an id for the slider div element */
   export let id = "ccs-" + Math.random().toString(36);
 
@@ -143,7 +149,10 @@
       {labelText}
     </slot>
   </label>
-  <div class:bx--slider-container="{true}">
+  <div
+    class:bx--slider-container="{true}"
+    style="{fullWidth ? 'width: 100%' : undefined}"
+  >
     <span class:bx--slider__range-label="{true}">{minLabel || min}</span>
     <div
       bind:this="{ref}"
