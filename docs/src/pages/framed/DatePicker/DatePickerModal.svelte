45627aad1f12828b953b086e5c07a89678f28181
metonym <ericyl.us@gmail.com>
2022-06-26 16:43:34 -0700

    docs: rename `ComboBox` example, do not open modal in `DatePicker` by default (#1374)
    * docs(combo-box): "Selected id" -> " Initial selected id"
    
    * docs(date-picker): do not open modal by default to prevent stealing focusing

--- docs/src/pages/framed/DatePicker/DatePickerModal.svelte
+++ docs/src/pages/framed/DatePicker/DatePickerModal.svelte
@@ -1,12 +1,22 @@
 <script>
-  import { Modal, DatePicker, DatePickerInput } from "carbon-components-svelte";
+  import {
+    Button,
+    Modal,
+    DatePicker,
+    DatePickerInput,
+  } from "carbon-components-svelte";
+
+  let open = false;
 </script>
 
+<Button on:click="{() => (open = true)}">Select date</Button>
+
 <Modal
-  open
+  bind:open
   modalHeading="Meeting date"
   primaryButtonText="Confirm"
   secondaryButtonText="Cancel"
+  on:click:button--secondary="{() => (open = false)}"
 >
   <DatePicker datePickerType="single" style="min-height: 420px">
     <DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
