6b515b7589cd901d5adb303517ca5c2cb232ba86
metonym <ericyl.us@gmail.com>
2022-08-23 07:59:15 -0700

    docs(ui-shell): make `SideNav` in rail example a sibling to `Content`
    Related #1459

--- docs/src/pages/framed/UIShell/HeaderNavRail.svelte
+++ docs/src/pages/framed/UIShell/HeaderNavRail.svelte
@@ -36,22 +36,22 @@
     </HeaderNavMenu>
     <HeaderNavItem href="/" text="Link 4" />
   </HeaderNav>
-
-  <SideNav bind:isOpen="{isSideNavOpen}" rail>
-    <SideNavItems>
-      <SideNavLink icon="{Fade}" text="Link 1" href="/" isSelected />
-      <SideNavLink icon="{Fade}" text="Link 2" href="/" />
-      <SideNavLink icon="{Fade}" text="Link 3" href="/" />
-      <SideNavMenu icon="{Fade}" text="Menu">
-        <SideNavMenuItem href="/" text="Link 1" />
-        <SideNavMenuItem href="/" text="Link 2" />
-        <SideNavMenuItem href="/" text="Link 3" />
-      </SideNavMenu>
-      <SideNavDivider />
-      <SideNavLink icon="{Fade}" text="Link 4" href="/" />
-    </SideNavItems>
-  </SideNav>
 </Header>
+
+<SideNav bind:isOpen="{isSideNavOpen}" rail>
+  <SideNavItems>
+    <SideNavLink icon="{Fade}" text="Link 1" href="/" isSelected />
+    <SideNavLink icon="{Fade}" text="Link 2" href="/" />
+    <SideNavLink icon="{Fade}" text="Link 3" href="/" />
+    <SideNavMenu icon="{Fade}" text="Menu">
+      <SideNavMenuItem href="/" text="Link 1" />
+      <SideNavMenuItem href="/" text="Link 2" />
+      <SideNavMenuItem href="/" text="Link 3" />
+    </SideNavMenu>
+    <SideNavDivider />
+    <SideNavLink icon="{Fade}" text="Link 4" href="/" />
+  </SideNavItems>
+</SideNav>
 
 <Content>
   <Grid>
