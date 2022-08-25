3dc6b6bb12608007c244f59d9690dc9c764bb38e
metonym <ericyl.us@gmail.com>
2022-08-25 11:20:58 -0700

    build: convert scripts from CJS to ESM

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -1,35 +1,33 @@
-const fs = require("fs");
-const sass = require("sass");
-const autoprefixer = require("autoprefixer");
-const postcss = require("postcss");
-const path = require("path");
+import fs from "fs";
+import sass from "sass";
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
+import path from "path";
 
-(async () => {
-  const scss = fs
-    .readdirSync("css")
-    .filter((file) => file.endsWith(".scss") && !/^\_popover/.test(file))
-    .map((file) => path.parse(file));
+const scss = fs
+  .readdirSync("css")
+  .filter((file) => file.endsWith(".scss") && !/^\_popover/.test(file))
+  .map((file) => path.parse(file));
 
-  for (const { name, base } of scss) {
-    const file = `css/${base}`;
-    const outFile = `css/${name}.css`;
+for (const { name, base } of scss) {
+  const file = `css/${base}`;
+  const outFile = `css/${name}.css`;
 
-    console.log("[build-css]", file, "-->", outFile);
+  console.log("[build-css]", file, "-->", outFile);
 
-    const { css } = sass.renderSync({
-      file,
-      outFile,
-      outputStyle: "compressed",
-      omitSourceMapUrl: true,
-      includePaths: ["node_modules"],
-    });
+  const { css } = sass.renderSync({
+    file,
+    outFile,
+    outputStyle: "compressed",
+    omitSourceMapUrl: true,
+    includePaths: ["node_modules"],
+  });
 
-    const prefixed = await postcss([
-      autoprefixer({
-        overrideBrowserslist: ["last 1 version", "ie >= 11", "Firefox ESR"],
-      }),
-    ]).process(css, { from: undefined });
+  const prefixed = await postcss([
+    autoprefixer({
+      overrideBrowserslist: ["last 1 version", "ie >= 11", "Firefox ESR"],
+    }),
+  ]).process(css, { from: undefined });
 
-    fs.writeFileSync(outFile, prefixed.css);
-  }
-})();
+  fs.writeFileSync(outFile, prefixed.css);
+}
