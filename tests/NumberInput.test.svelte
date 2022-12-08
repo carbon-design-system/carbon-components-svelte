e6f5766e46449df8dfc4692b8bc19b6ffd25365c
Jonathan Quintin <jqlio18@gmail.com>
2022-12-07 23:06:29 -0500

    fix(number-input): support floating point values (#1555)
    Fixes #233, fixes #486, fixes #1554

--- tests/NumberInput.test.svelte
+++ tests/NumberInput.test.svelte
@@ -16,6 +16,30 @@
   label="Clusters"
   helperText="Clusters provisioned in your region"
   invalidText="Number must be between 4 and 20."
+  on:input="{(e) => {
+    console.log({ input: e.detail }); // null | number
+  }}"
+  on:change="{(e) => {
+    console.log({ change: e.detail }); // null | number
+  }}"
+  on:keydown
+  on:keyup
+  on:paste
+/>
+
+<NumberInput
+  disabled
+  light
+  min="{1}"
+  max="{10}"
+  value="{4}"
+  step="{0.1}"
+  label="Clusters"
+  helperText="Clusters provisioned in your region"
+  invalidText="Number must be between 1 and 10."
+  on:input="{(e) => {
+    console.log({ input: e.detail }); // null | number
+  }}"
   on:change="{(e) => {
     console.log(e.detail); // null | number
   }}"
