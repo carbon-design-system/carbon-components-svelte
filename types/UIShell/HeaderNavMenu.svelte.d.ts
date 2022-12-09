9536e8b62117c5b6ab09b36ef07b6ec68df9daeb
Eric Liu <ericyl.us@gmail.com>
2022-12-09 08:48:23 -0800

    Run "yarn build:docs"

--- types/UIShell/HeaderNavMenu.svelte.d.ts
+++ types/UIShell/HeaderNavMenu.svelte.d.ts
@@ -26,30 +26,6 @@ export interface HeaderNavMenuProps
    * @default null
    */
   ref?: null | HTMLAnchorElement;
-
-  /**
-   * SvelteKit attribute to enable data prefetching
-   * if a link is hovered over or touched on mobile.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-prefetch
-   * @default false
-   */
-  "sveltekit:prefetch"?: boolean;
-
-  /**
-   * SvelteKit attribute to trigger a full page
-   * reload after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-reload
-   * @default false
-   */
-  "sveltekit:reload"?: boolean;
-
-  /**
-   * SvelteKit attribute to prevent scrolling
-   * after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-noscroll
-   * @default false
-   */
-  "sveltekit:noscroll"?: boolean;
 }
 
 export default class HeaderNavMenu extends SvelteComponentTyped<
--- types/UIShell/HeaderPanelLink.svelte.d.ts
+++ types/UIShell/HeaderPanelLink.svelte.d.ts
@@ -14,30 +14,6 @@ export interface HeaderPanelLinkProps
    * @default null
    */
   ref?: null | HTMLAnchorElement;
-
-  /**
-   * SvelteKit attribute to enable data prefetching
-   * if a link is hovered over or touched on mobile.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-prefetch
-   * @default false
-   */
-  "sveltekit:prefetch"?: boolean;
-
-  /**
-   * SvelteKit attribute to trigger a full page
-   * reload after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-reload
-   * @default false
-   */
-  "sveltekit:reload"?: boolean;
-
-  /**
-   * SvelteKit attribute to prevent scrolling
-   * after the link is clicked.
-   * @see https://kit.svelte.dev/docs/a-options#sveltekit-noscroll
-   * @default false
-   */
-  "sveltekit:noscroll"?: boolean;
 }
 
 export default class HeaderPanelLink extends SvelteComponentTyped<
