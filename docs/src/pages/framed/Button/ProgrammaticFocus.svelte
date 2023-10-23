a828625c668b9fa9aba7806d287f7118fff3276b
Eric Liu <ericyl.us@gmail.com>
2023-10-23 09:55:00 -0700

    docs(button): add "Programmatic focus" example (#1834)
    Related #1830

---
+++ docs/src/pages/framed/Button/ProgrammaticFocus.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Button } from "carbon-components-svelte";
+
+  let ref;
+</script>
+
+<Button bind:ref>Primary button</Button>
+<Button
+  kind="ghost"
+  on:click="{() => {
+    ref?.focus();
+  }}"
+>
+  Click to focus the Primary button
+</Button>
