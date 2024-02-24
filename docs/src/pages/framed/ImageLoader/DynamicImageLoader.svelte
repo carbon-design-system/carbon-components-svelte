c6af8bdafe3985a4a444ddd3b5b000ebb12d557b
Eric Liu <ericyl.us@gmail.com>
2024-02-24 13:36:33 -0800

    docs(image-loader): add "Dynamic image source" example

---
+++ docs/src/pages/framed/ImageLoader/DynamicImageLoader.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { ImageLoader, Button } from "carbon-components-svelte";
+
+  const images = [
+    "https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b9/Carbon-design-system-logo.png",
+  ];
+
+  let index = 0;
+
+  $: src = images[index];
+</script>
+
+<Button
+  kind="ghost"
+  on:click="{() => {
+    index = index === 0 ? 1 : 0;
+  }}"
+>
+  Toggle image
+</Button>
+
+<div style:margin-top="1rem" style:width="100%" style:max-width="120px">
+  <ImageLoader ratio="1x1" fadeIn src="{src}" alt="{src}" />
+</div>
