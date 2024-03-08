79d941bdffa51f996ec668eb47b5b5aaf72147cb
Samuel Janda <hi@simpleprogramming.com.au>
2024-03-08 13:02:04 +1100

    Draft commit for review for Popover v11 #1896

--- src/Popover/PopoverContent.svelte
+++ src/Popover/PopoverContent.svelte
@@ -1,9 +1,13 @@
 <script>
-  export let className = null;
+  /**
+   * Specify the content `span` props
+   * @type {import('svelte/elements').HTMLDivAttributes}
+   */
+  export let contentProps = {};
 </script>
 
-<span class:bx--popover="{true}" {...$$restProps}>
-  <span class:bx--popover-content="{true}" class="{className}">
+<span class:bx--popover="{true}">
+  <span class:bx--popover-content="{true}" {...contentProps}>
     <slot />
   </span>
   <span class:bx--popover-caret="{true}"></span>
