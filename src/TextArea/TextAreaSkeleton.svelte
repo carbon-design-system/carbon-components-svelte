d7f0ed19473659e71611f98e87166345cec5e887
Eric Liu <ericyl.us@gmail.com>
2024-04-29 21:50:33 -0700

    feat(text-area)!: integrate `TextArea` with v11 (#1967)

--- src/TextArea/TextAreaSkeleton.svelte
+++ src/TextArea/TextAreaSkeleton.svelte
@@ -1,18 +1,11 @@
 <script>
+  // @ts-check
+
   /** Set to `true` to visually hide the label text */
   export let hideLabel = false;
 </script>
 
-<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-  class:bx--form-item="{true}"
-  {...$$restProps}
-  on:click
-  on:mouseover
-  on:mouseenter
-  on:mouseleave
->
+<div class:bx--form-item="{true}">
   {#if !hideLabel}
     <span class:bx--label="{true}" class:bx--skeleton="{true}"></span>
   {/if}
--- types/TextArea/TextAreaSkeleton.svelte.d.ts
+++ types/TextArea/TextAreaSkeleton.svelte.d.ts
@@ -1,25 +1,15 @@
 import type { SvelteComponentTyped } from "svelte";
-import type { SvelteHTMLElements } from "svelte/elements";
 
-type RestProps = SvelteHTMLElements["div"];
-
-export interface TextAreaSkeletonProps extends RestProps {
+export interface TextAreaSkeletonProps {
   /**
    * Set to `true` to visually hide the label text
    * @default false
    */
   hideLabel?: boolean;
-
-  [key: `data-${string}`]: any;
 }
 
 export default class TextAreaSkeleton extends SvelteComponentTyped<
   TextAreaSkeletonProps,
-  {
-    click: WindowEventMap["click"];
-    mouseover: WindowEventMap["mouseover"];
-    mouseenter: WindowEventMap["mouseenter"];
-    mouseleave: WindowEventMap["mouseleave"];
-  },
+  Record<string, any>,
   {}
 > {}
