9ab6b9bb032755a5c5654c39aea2ae8013e7fc92
Eric Liu <ericyl.us@gmail.com>
2025-01-29 19:28:16 -0800

    test(overflow-menu): add unit tests

---
+++ tests/OverflowMenu/OverflowMenu.test.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { OverflowMenu, OverflowMenuItem } from "carbon-components-svelte";
+</script>
+
+<OverflowMenu
+  on:close={(e) => {
+    console.log("close", e.detail); // { index: number; text: string; }
+  }}
+>
+  <OverflowMenuItem
+    text="Manage credentials"
+    on:click={() => {
+      console.log("click", "Manage credentials");
+    }}
+  />
+  <OverflowMenuItem
+    href="https://cloud.ibm.com/docs/api-gateway/"
+    text="API documentation"
+    on:click={() => {
+      console.log("click", "API documentation");
+    }}
+  />
+  <OverflowMenuItem
+    danger
+    text="Delete service"
+    on:click={() => {
+      console.log("click", "Delete service");
+    }}
+  />
+</OverflowMenu>
