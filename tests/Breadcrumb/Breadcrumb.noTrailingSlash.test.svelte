63fdb7566469b626946d4f206d42edad0c033430
Eric Liu <ericyl.us@gmail.com>
2025-02-26 19:41:32 -0800

    test(breadcrumb): add unit tests

---
+++ tests/Breadcrumb/Breadcrumb.noTrailingSlash.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+
+<Breadcrumb noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/profile" isCurrentPage>Profile</BreadcrumbItem>
+</Breadcrumb>
