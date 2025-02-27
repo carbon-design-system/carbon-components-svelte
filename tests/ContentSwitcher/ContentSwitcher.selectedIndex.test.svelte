b57c829e120f8d1b993f7b035ebb678bf2bd159d
Eric Liu <ericyl.us@gmail.com>
2025-02-26 19:45:15 -0800

    test(content-switcher): add unit tests

---
+++ tests/ContentSwitcher/ContentSwitcher.selectedIndex.test.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { ContentSwitcher, Switch } from "carbon-components-svelte";
+
+  export let selectedIndex = 1;
+</script>
+
+<ContentSwitcher {selectedIndex}>
+  <Switch text="First" />
+  <Switch text="Second" />
+  <Switch text="Third" />
+</ContentSwitcher>
