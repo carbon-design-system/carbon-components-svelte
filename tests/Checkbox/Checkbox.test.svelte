e100429374af8a32bce3239d504e4778da8c8dda
Eric Liu <ericyl.us@gmail.com>
2025-02-26 20:51:37 -0800

    test(checkbox): add unit tests

---
+++ tests/Checkbox/Checkbox.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Checkbox } from "carbon-components-svelte";
+
+  export let checked = false;
+  export let indeterminate = false;
+  export let disabled = false;
+  export let hideLabel = false;
+  export let labelText = "Checkbox label";
+</script>
+
+<Checkbox
+  bind:checked
+  bind:indeterminate
+  {disabled}
+  {hideLabel}
+  {labelText}
+  data-testid="checkbox"
+  on:check={() => console.log("check")}
+  on:click={() => console.log("click")}
+/>
