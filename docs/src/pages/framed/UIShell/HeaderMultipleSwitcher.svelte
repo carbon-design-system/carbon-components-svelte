e59b8784f37c281d60f41a2d8d8f6c7cd2153c31
Eric Liu <ericyl.us@gmail.com>
2025-03-08 21:16:46 -0800

    docs(ui-shell): add "Header with multiple switchers" example

---
+++ docs/src/pages/framed/UIShell/HeaderMultipleSwitcher.svelte
@@ -0,0 +1,85 @@
+<script>
+  import {
+    Header,
+    HeaderUtilities,
+    HeaderAction,
+    HeaderPanelLinks,
+    HeaderPanelDivider,
+    HeaderPanelLink,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
+  import SettingsAdjust from "carbon-icons-svelte/lib/SettingsAdjust.svelte";
+  import UserAvatarFilledAlt from "carbon-icons-svelte/lib/UserAvatarFilledAlt.svelte";
+
+  let isOpen1 = false;
+  let isOpen2 = false;
+  let isOpen3 = false;
+</script>
+
+<Header company="IBM" platformName="Carbon Svelte" isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderUtilities>
+    <HeaderAction
+      bind:isOpen={isOpen1}
+      iconDescription="Settings"
+      tooltipAlignment="start"
+      icon={SettingsAdjust}
+      on:open={() => {
+        isOpen2 = false;
+        isOpen3 = false;
+      }}
+    >
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+    <HeaderAction
+      bind:isOpen={isOpen2}
+      iconDescription="Profile"
+      icon={UserAvatarFilledAlt}
+      on:open={() => {
+        isOpen1 = false;
+        isOpen3 = false;
+      }}
+    >
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+        <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+    <HeaderAction
+      bind:isOpen={isOpen3}
+      text="Switcher text"
+      on:open={() => {
+        isOpen1 = false;
+        isOpen2 = false;
+      }}
+    >
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+        <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+  </HeaderUtilities>
+</Header>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <h1>Welcome</h1>
+      </Column>
+    </Row>
+  </Grid>
+</Content>
