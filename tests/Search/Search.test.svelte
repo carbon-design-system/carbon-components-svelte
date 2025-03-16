7f68d4e639c9e24793f8d87c75f628fd771d805b
Eric Liu <ericyl.us@gmail.com>
2025-03-15 17:32:47 -0700

    test(search): add unit tests

---
+++ tests/Search/Search.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Search } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<Search
+  bind:value
+  labelText="Default search"
+  placeholder="Search"
+  closeButtonLabelText="Clear value"
+  on:clear={() => {
+    console.log("clear");
+  }}
+/>
+
+<Search disabled labelText="Disabled search" placeholder="Search disabled..." />
---
+++ tests/Search/SearchSkeleton.test.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Search } from "carbon-components-svelte";
+</script>
+
+<Search skeleton labelText="Default skeleton" />
+
+<Search size="lg" skeleton labelText="Large skeleton" />
+
+<Search size="sm" skeleton labelText="Small skeleton" />
