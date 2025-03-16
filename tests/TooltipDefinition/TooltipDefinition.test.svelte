46728fe06503ca54244b0ffafc6506ec09a960de
Eric Liu <ericyl.us@gmail.com>
2025-03-16 16:59:41 -0700

    test(tooltip-definition): add unit tests

---
+++ tests/TooltipDefinition/TooltipDefinition.test.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { TooltipDefinition } from "carbon-components-svelte";
+
+  export let tooltipText = "Test tooltip text";
+  export let open = false;
+  export let align: "start" | "center" | "end" = "center";
+  export let direction: "top" | "bottom" = "bottom";
+  export let id = "test-tooltip";
+  export let triggerContent = "Tooltip trigger";
+</script>
+
+<TooltipDefinition
+  {tooltipText}
+  {open}
+  {align}
+  {direction}
+  {id}
+  on:open={() => {
+    console.log("open");
+  }}
+  on:close={() => {
+    console.log("close");
+  }}
+>
+  {triggerContent}
+</TooltipDefinition>
