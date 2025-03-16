a71f8c2c90896930cc6107d7deda9f0fe5f6a5b7
Eric Liu <ericyl.us@gmail.com>
2025-03-15 17:50:24 -0700

    test(copy-button): add unit tests

---
+++ tests/CopyButton/CopyButton.test.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { CopyButton } from "carbon-components-svelte";
+</script>
+
+<CopyButton
+  text="text"
+  iconDescription="Basic"
+  on:copy={() => {
+    console.log("copied");
+  }}
+/>
+
+<CopyButton
+  iconDescription="Custom feedback"
+  text="text"
+  feedback="Copied to clipboard"
+  feedbackTimeout={0}
+/>
+
+<CopyButton
+  text="Custom copy function"
+  iconDescription="Custom copy function"
+  copy={(text) => {
+    console.log(`Custom copy: ${text}`);
+  }}
+/>
