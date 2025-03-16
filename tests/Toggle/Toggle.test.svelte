d14153245191f9fad0aab8f704487e1ff79db47a
Eric Liu <ericyl.us@gmail.com>
2025-03-15 18:01:12 -0700

    test(toggle): add unit tests

---
+++ tests/Toggle/Toggle.test.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Toggle } from "carbon-components-svelte";
+
+  let toggled = false;
+</script>
+
+<Toggle
+  bind:toggled
+  labelText="Default toggle"
+  on:toggle={(e) => {
+    console.log("toggled:", e.detail.toggled);
+  }}
+/>
+
+<Toggle labelText="Custom labels" labelA="Inactive" labelB="Active" />
+
+<Toggle size="sm" labelText="Small toggle" />
+
+<Toggle disabled labelText="Disabled toggle" />
+
+<Toggle
+  hideLabel
+  labelText="Hidden label toggle"
+  aria-label="Hidden label toggle"
+/>
