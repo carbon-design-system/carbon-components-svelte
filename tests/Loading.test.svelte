8be87921f0a81247d91357a96d70242b2583ee26
Eric Liu <ericyl.us@gmail.com>
2025-03-16 12:09:38 -0700

    test(loading): add unit tests

--- tests/Loading.test.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { Loading } from "carbon-components-svelte";
-</script>
-
-<Loading withOverlay={false} />
-
-<Loading withOverlay={false} small />
---
+++ tests/Loading/Loading.test.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Loading } from "carbon-components-svelte";
+</script>
+
+<!-- Default loading (with overlay) -->
+<div data-testid="default-loader">
+  <Loading />
+</div>
+
+<!-- Loading without overlay -->
+<div data-testid="loader-no-overlay">
+  <Loading withOverlay={false} />
+</div>
+
+<!-- Small loading -->
+<div data-testid="loader-small">
+  <Loading withOverlay={false} small />
+</div>
+
+<!-- Inactive loading -->
+<div data-testid="loader-inactive">
+  <Loading active={false} />
+</div>
+
+<!-- Custom description -->
+<div data-testid="loader-description">
+  <Loading description="Processing data..." />
+</div>
