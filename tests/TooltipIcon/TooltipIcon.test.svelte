ca5f836c1ce89efb3ad1fcf8957dc1591b0a4139
Eric Liu <ericyl.us@gmail.com>
2025-03-16 17:16:04 -0700

    test(tooltip-icon): add unit tests

---
+++ tests/TooltipIcon/TooltipIcon.test.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { TooltipIcon } from "carbon-components-svelte";
+  import Carbon from "carbon-icons-svelte/lib/Carbon.svelte";
+
+  export let tooltipText = "Test tooltip text";
+  export let disabled = false;
+  export let align: "start" | "center" | "end" = "center";
+  export let direction: "top" | "right" | "bottom" | "left" = "bottom";
+  export let id = "test-tooltip";
+  export let icon = Carbon;
+</script>
+
+<TooltipIcon
+  {tooltipText}
+  {disabled}
+  {align}
+  {direction}
+  {id}
+  {icon}
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+  on:focus
+>
+  <slot name="tooltipText" />
+  <slot />
+</TooltipIcon>
