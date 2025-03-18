6eda673aaf8d8bb31fbaeca83b0fdbb1b6519ce8
Eric Liu <ericyl.us@gmail.com>
2025-03-17 17:45:18 -0700

    test(pagination-nav): add unit tests

---
+++ tests/PaginationNav/PaginationNav.test.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { PaginationNav } from "carbon-components-svelte";
+
+  export let page = 1;
+  export let total = 10;
+  export let shown = 10;
+  export let loop = false;
+  export let forwardText = "Next page";
+  export let backwardText = "Previous page";
+  export let tooltipPosition:
+    | "top"
+    | "right"
+    | "bottom"
+    | "left"
+    | "outside"
+    | "inside" = "bottom";
+</script>
+
+<PaginationNav
+  bind:page
+  {total}
+  {shown}
+  {loop}
+  {forwardText}
+  {backwardText}
+  {tooltipPosition}
+  on:change={(e) => {
+    console.log("change", e.detail);
+  }}
+  on:click:button--previous={(e) => {
+    console.log("previous", e.detail);
+  }}
+  on:click:button--next={(e) => {
+    console.log("next", e.detail);
+  }}
+/>
