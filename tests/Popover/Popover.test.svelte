d25a85c8258ed5d627ee8937d8b95624b9c9e47d
Eric Liu <ericyl.us@gmail.com>
2025-03-17 19:15:52 -0700

    test(popover): add unit tests

---
+++ tests/Popover/Popover.test.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { Popover } from "carbon-components-svelte";
+
+  export let open = false;
+  export let align:
+    | "top"
+    | "top-left"
+    | "top-right"
+    | "bottom"
+    | "bottom-left"
+    | "bottom-right"
+    | "left"
+    | "left-bottom"
+    | "left-top"
+    | "right"
+    | "right-bottom"
+    | "right-top" = "top";
+  export let caret = false;
+  export let light = false;
+  export let highContrast = false;
+  export let relative = false;
+  export let closeOnOutsideClick = false;
+</script>
+
+<div data-testid="parent">
+  Parent
+  <Popover
+    {open}
+    {closeOnOutsideClick}
+    {align}
+    {caret}
+    {light}
+    {highContrast}
+    {relative}
+    on:click:outside={(e) => {
+      console.log("click:outside", e.detail);
+    }}
+  >
+    <div data-testid="content">Content</div>
+  </Popover>
+</div>
