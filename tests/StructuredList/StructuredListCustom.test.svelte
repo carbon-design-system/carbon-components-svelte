f5342d4b96ffc8cd9cf3479c51e308b31627dfd2
Eric Liu <ericyl.us@gmail.com>
2025-03-20 16:02:16 -0700

    test(structured-list): add unit tests

---
+++ tests/StructuredList/StructuredListCustom.test.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import {
+    StructuredList,
+    StructuredListBody,
+    StructuredListHead,
+    StructuredListCell,
+    StructuredListRow,
+  } from "carbon-components-svelte";
+</script>
+
+<StructuredList>
+  <StructuredListHead>
+    <StructuredListRow head>
+      <StructuredListCell head>
+        <div data-testid="custom-header">Custom Header</div>
+      </StructuredListCell>
+    </StructuredListRow>
+  </StructuredListHead>
+  <StructuredListBody>
+    <StructuredListRow>
+      <StructuredListCell>
+        <div data-testid="custom-content">Custom Content</div>
+      </StructuredListCell>
+    </StructuredListRow>
+  </StructuredListBody>
+</StructuredList>
