34e50618f609e6473028017ee37da739075faa54
Eric Liu <ericyl.us@gmail.com>
2025-03-20 12:52:19 -0700

    test(number-input): add unit tests

---
+++ tests/NumberInput/NumberInputCustom.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { NumberInput } from "carbon-components-svelte";
+</script>
+
+<NumberInput label="Custom label" value={0}>
+  <span slot="label">Custom Label Text</span>
+</NumberInput>
