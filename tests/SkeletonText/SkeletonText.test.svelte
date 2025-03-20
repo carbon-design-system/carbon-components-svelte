887b03797a5450f566291c33112cb15610b230bd
Eric Liu <ericyl.us@gmail.com>
2025-03-20 16:05:12 -0700

    test(skeleton-text): add unit tests

---
+++ tests/SkeletonText/SkeletonText.test.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { SkeletonText } from "carbon-components-svelte";
+
+  export let lines = 3;
+  export let heading = false;
+  export let paragraph = false;
+  export let width = "100%";
+</script>
+
+<SkeletonText
+  {lines}
+  {heading}
+  {paragraph}
+  {width}
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+/>
