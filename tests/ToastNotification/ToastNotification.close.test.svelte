7c436bd747c736c9ceac719fac4d094e5c6f4a59
Eric Liu <ericyl.us@gmail.com>
2025-03-20 12:27:37 -0700

    test(toast-notification): add unit tests

---
+++ tests/ToastNotification/ToastNotification.close.test.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { ToastNotification } from "carbon-components-svelte";
+</script>
+
+<ToastNotification
+  title="Custom"
+  subtitle="Message"
+  caption="2024-03-21 12:00:00"
+  on:close={(e) => {
+    e.preventDefault();
+    console.log("close", e.detail);
+  }}
+/>
