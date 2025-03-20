eb413a1fbac54503f9ab1464e6dc1538bb685b84
Eric Liu <ericyl.us@gmail.com>
2025-03-20 12:39:51 -0700

    test(text-input): add unit tests

---
+++ tests/TextInput/TextInputCustom.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { TextInput } from "carbon-components-svelte";
+</script>
+
+<TextInput labelText="Custom label">
+  <span slot="labelText">Custom Label Text</span>
+</TextInput>
