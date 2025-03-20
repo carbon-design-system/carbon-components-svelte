35de0fe0d67150d6bc69bc090f72d63594ac9f96
Eric Liu <ericyl.us@gmail.com>
2025-03-20 12:20:46 -0700

    test(inline-notification): add unit tests

---
+++ tests/InlineNotification/InlineNotification.test.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { InlineNotification } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let kind: ComponentProps<InlineNotification>["kind"] = "error";
+  export let lowContrast = false;
+  export let timeout = 0;
+  export let role = "alert";
+  export let title = "Error:";
+  export let subtitle = "An internal server error occurred.";
+  export let hideCloseButton = false;
+  export let statusIconDescription = "";
+  export let closeButtonDescription = "";
+</script>
+
+<InlineNotification
+  {kind}
+  {lowContrast}
+  {timeout}
+  {role}
+  {title}
+  {subtitle}
+  {hideCloseButton}
+  {statusIconDescription}
+  {closeButtonDescription}
+  on:close={(e) => {
+    console.log("close", e.detail);
+  }}
+/>
---
+++ tests/InlineNotification/InlineNotificationCustom.test.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import {
+    InlineNotification,
+    NotificationActionButton,
+  } from "carbon-components-svelte";
+</script>
+
+<InlineNotification kind="warning">
+  <strong slot="title">Custom Title:</strong>
+  <strong slot="subtitle">Custom subtitle content.</strong>
+  <svelte:fragment slot="actions">
+    <NotificationActionButton>Learn more</NotificationActionButton>
+  </svelte:fragment>
+</InlineNotification>
