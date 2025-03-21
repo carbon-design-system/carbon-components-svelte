66b5b0062717ea25860bb6a2b39c25d049117eab
Eric Liu <ericyl.us@gmail.com>
2025-03-20 17:20:53 -0700

    test: remove CSS import from set-up file

--- tests/setup-tests.ts
+++ tests/setup-tests.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest/globals" />
 import "@testing-library/jest-dom/vitest";
 import { userEvent } from "@testing-library/user-event";
-import "../css/all.css";
 
 // Mock scrollIntoView since it's not implemented in JSDOM
 Element.prototype.scrollIntoView = vi.fn();
