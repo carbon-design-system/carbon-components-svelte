9a05fc4f391c18f684dd091cd2b3792a7eefb73d
Eric Liu <ericyl.us@gmail.com>
2025-03-20 17:08:13 -0700

    test: remove old files

--- tests/CopyableCodeSnippet.test.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let copy = (text: string) => text;
-  export let code = "npm i carbon-component-svelte";
-
-  import { CodeSnippet } from "carbon-components-svelte";
-</script>
-
-<CodeSnippet on:click={() => copy(code)}>{code}</CodeSnippet>
