d45409c7f3245565c117b290718534e828fd164e
Eric Liu <ericyl.us@gmail.com>
2025-03-20 17:19:33 -0700

    test(grid): add unit tests

---
+++ tests/Grid/Grid.test.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Grid } from "carbon-components-svelte";
+
+  export let condensed = false;
+  export let narrow = false;
+  export let fullWidth = false;
+  export let noGutter = false;
+  export let noGutterLeft = false;
+  export let noGutterRight = false;
+  export let padding = false;
+  export let as = false;
+</script>
+
+{#if as}
+  <Grid
+    {as}
+    {condensed}
+    {narrow}
+    {fullWidth}
+    {noGutter}
+    {noGutterLeft}
+    {noGutterRight}
+    {padding}
+    {...$$restProps}
+    let:props
+  >
+    <header {...props}>
+      <slot />
+    </header>
+  </Grid>
+{:else}
+  <Grid
+    {condensed}
+    {narrow}
+    {fullWidth}
+    {noGutter}
+    {noGutterLeft}
+    {noGutterRight}
+    {padding}
+    {...$$restProps}
+  >
+    <slot />
+  </Grid>
+{/if}
