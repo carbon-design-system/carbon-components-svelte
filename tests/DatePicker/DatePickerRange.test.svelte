e358736e38d0643b7524c83376e3b4c65107307c
Eric Liu <ericyl.us@gmail.com>
2025-04-12 16:23:38 -0700

    test(date-picker): add unit tests

---
+++ tests/DatePicker/DatePickerRange.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="range" on:change>
+  <DatePickerInput labelText="Start date" placeholder="mm/dd/yyyy" />
+  <DatePickerInput labelText="End date" placeholder="mm/dd/yyyy" />
+</DatePicker>
