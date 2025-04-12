37a1ab30cf9a9f6fc41f5a2978842af512ec4263
Eric Liu <ericyl.us@gmail.com>
2025-04-12 13:22:23 -0700

    test(form): add unit tests

---
+++ tests/Form/Form.test.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Form, FormGroup, Checkbox, Button } from "carbon-components-svelte";
+
+  export let preventDefault = false;
+</script>
+
+<Form
+  data-testid="form"
+  on:submit={(e) => {
+    if (preventDefault) {
+      e.preventDefault();
+    }
+    console.log("submit", e);
+  }}
+>
+  <FormGroup legendText="Checkboxes">
+    <Checkbox id="checkbox-0" labelText="Checkbox Label" checked />
+    <Checkbox id="checkbox-1" labelText="Checkbox Label" />
+    <Checkbox id="checkbox-2" labelText="Checkbox Label" disabled />
+  </FormGroup>
+  <Button type="submit">Submit</Button>
+</Form>
