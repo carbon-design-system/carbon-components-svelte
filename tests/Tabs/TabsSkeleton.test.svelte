9584029a25dab6e66f295f705bc2cc9a9fdd55ae
Eric Liu <ericyl.us@gmail.com>
2025-09-05 09:48:41 -0700

    test(tabs): add more unit tests (#2201)

---
+++ tests/Tabs/TabsSkeleton.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { TabsSkeleton } from "carbon-components-svelte";
+
+  export let count = 4;
+  export let type: "default" | "container" = "default";
+</script>
+
+<TabsSkeleton {count} {type} />
