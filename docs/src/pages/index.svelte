4dc738f3c2d0d5001934ce2a6ecf10e226699a30
Eric Liu <ericyl.us@gmail.com>
2025-09-24 11:12:09 -0700

    docs: update number of `carbon-pictograms-svelte` to 1,400+

--- docs/src/pages/index.svelte
+++ docs/src/pages/index.svelte
@@ -247,7 +247,7 @@
           borderBottom
           borderRight
           title="Carbon Pictograms Svelte"
-          subtitle="1,300+ pictograms"
+          subtitle="1,400+ pictograms"
           target="_blank"
           href="https://github.com/carbon-design-system/carbon-pictograms-svelte"
         />
