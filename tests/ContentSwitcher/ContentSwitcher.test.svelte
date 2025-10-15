ee7fde4cae6e0c2ee3aff6935335cbd1021c702c
Eric Liu <ericyl.us@gmail.com>
2025-10-15 16:58:30 -0700

    test(content-switcher): add more unit tests

--- tests/ContentSwitcher/ContentSwitcher.test.svelte
+++ tests/ContentSwitcher/ContentSwitcher.test.svelte
@@ -1,9 +1,33 @@
+<svelte:options accessors />
+
 <script lang="ts">
   import { ContentSwitcher, Switch } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let customClass = "";
+  export let switchId: ComponentProps<Switch>["id"] = undefined;
+  export let switchRef: ComponentProps<Switch>["ref"] = null;
 </script>
 
-<ContentSwitcher>
+<ContentSwitcher
+  class={customClass}
+  on:change={(e) => {
+    console.log("change", e.detail);
+  }}
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+>
   <Switch text="Option 1" />
-  <Switch text="Option 2" />
+  <Switch text="Option 2" id={switchId} bind:ref={switchRef} />
   <Switch text="Option 3" />
 </ContentSwitcher>
