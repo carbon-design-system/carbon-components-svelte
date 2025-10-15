99a1167f859e5222589861942e8658eeec986f14
Eric Liu <ericyl.us@gmail.com>
2025-10-15 16:57:49 -0700

    test(row): add unit tests

---
+++ tests/Grid/Row.test.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { Row } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let condensed: ComponentProps<Row>["condensed"] = false;
+  export let narrow: ComponentProps<Row>["narrow"] = false;
+  export let noGutter: ComponentProps<Row>["noGutter"] = false;
+  export let noGutterLeft: ComponentProps<Row>["noGutterLeft"] = false;
+  export let noGutterRight: ComponentProps<Row>["noGutterRight"] = false;
+  export let padding: ComponentProps<Row>["padding"] = false;
+  export let as: ComponentProps<Row>["as"] = false;
+</script>
+
+{#if as}
+  <Row
+    {as}
+    {condensed}
+    {narrow}
+    {noGutter}
+    {noGutterLeft}
+    {noGutterRight}
+    {padding}
+    let:props
+  >
+    <section {...props} data-testid="custom-row">Row content</section>
+  </Row>
+{:else}
+  <Row
+    {condensed}
+    {narrow}
+    {noGutter}
+    {noGutterLeft}
+    {noGutterRight}
+    {padding}
+    data-testid="test-row"
+  >
+    Row content
+  </Row>
+{/if}
