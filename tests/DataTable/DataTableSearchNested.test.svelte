784938481f85ac14b5ae2687e075ad83c2342c58
Eric Liu <ericyl.us@gmail.com>
2025-10-15 21:40:59 -0700

    fix(data-table): default toolbar filtering supports nested keys
    Fixes #1897

---
+++ tests/DataTable/DataTableSearchNested.test.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import {
+    DataTable,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+  } from "carbon-components-svelte";
+
+  export let value = "";
+
+  const rows = [
+    { id: "1", contact: { company: "Super Wheels" } },
+    { id: "2", contact: { company: "Mini Wheels" } },
+    { id: "3", contact: { company: "Mega Corp" } },
+  ];
+
+  let filteredRowIds: string[] = [];
+</script>
+
+<DataTable
+  headers={[
+    { key: "id", value: "ID" },
+    { key: "contact.company", value: "Company name" },
+  ]}
+  {rows}
+>
+  <Toolbar>
+    <ToolbarContent>
+      <ToolbarSearch persistent {value} shouldFilterRows bind:filteredRowIds />
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
