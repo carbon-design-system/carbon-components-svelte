e82e1d2020d315805224bf67695cbed9c6043a36
Eric Liu <ericyl.us@gmail.com>
2025-10-16 10:15:56 -0700

    fix(file-uploader): synchronize `input.files` when files removed
    Fixes #1785

---
+++ tests/FileUploader/FileUploader.test.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { FileUploader } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  let files: ComponentProps<FileUploader>["files"] = [];
+
+  export function getInputElement() {
+    return document.querySelector('input[type="file"]') as HTMLInputElement;
+  }
+</script>
+
+<form data-testid="file-form">
+  <FileUploader multiple status="edit" buttonLabel="Add files" bind:files />
+</form>
