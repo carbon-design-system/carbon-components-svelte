a5053a59287b44a8467e153b11d47ac02f465750
Eric Liu <ericyl.us@gmail.com>
2025-10-16 10:39:52 -0700

    docs(radio-button): add standalone example

---
+++ docs/src/pages/framed/RadioButton/RadioButtonStandalone.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { RadioButton, Button } from "carbon-components-svelte";
+
+  let agreedToTerms = false;
+</script>
+
+<div style:display="flex">
+  <RadioButton
+    bind:checked={agreedToTerms}
+    labelText="I agree to the terms and conditions"
+    name="terms"
+    value="agreed"
+  />
+</div>
+
+<div style:margin="var(--cds-layout-02) 0">
+  <Button
+    size="small"
+    kind="secondary"
+    disabled={!agreedToTerms}
+    on:click={() => (agreedToTerms = !agreedToTerms)}
+  >
+    Reset
+  </Button>
+</div>
