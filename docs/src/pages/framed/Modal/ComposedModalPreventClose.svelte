8ba15f9dccb10757fee57fc0e7f7bde52f6cdc02
Eric Liu <ericyl.us@gmail.com>
2025-10-18 14:12:44 -0700

    docs(composed-modal): docs(modal): add "Prevent default close behavior" example

---
+++ docs/src/pages/framed/Modal/ComposedModalPreventClose.svelte
@@ -0,0 +1,59 @@
+<script>
+  import {
+    Button,
+    ComposedModal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    InlineNotification,
+  } from "carbon-components-svelte";
+
+  let open = false;
+  let hasUnsavedChanges = true;
+  let showWarning = false;
+</script>
+
+<Button
+  on:click={() => {
+    open = true;
+    hasUnsavedChanges = true;
+    showWarning = false;
+  }}>Open modal with unsaved changes</Button
+>
+
+<ComposedModal
+  bind:open
+  on:close={(e) => {
+    console.log("Close triggered by:", e.detail.trigger);
+    if (hasUnsavedChanges) {
+      e.preventDefault();
+      showWarning = true;
+    }
+  }}
+  on:submit={() => {
+    hasUnsavedChanges = false;
+    open = false;
+  }}
+>
+  <ModalHeader title="Edit profile" />
+  <ModalBody>
+    {#if showWarning}
+      <InlineNotification
+        kind="warning"
+        title="Unsaved changes"
+        subtitle="Please save or discard your changes before closing."
+        hideCloseButton
+      />
+    {/if}
+    <p>You have unsaved changes. Click "Save" to apply them.</p>
+  </ModalBody>
+  <ModalFooter
+    primaryButtonText="Save"
+    secondaryButtonText="Discard changes"
+    on:click:button--secondary={async () => {
+      hasUnsavedChanges = false;
+      showWarning = false;
+      open = false;
+    }}
+  />
+</ComposedModal>
