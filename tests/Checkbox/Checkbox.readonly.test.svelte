3f183487e4df3bd29cb4506a9b8f91bb8eaae4c0
Eric Liu <ericyl.us@gmail.com>
2025-10-18 14:01:20 -0700

    fix(checkbox): prevent state changes when `readonly` is true
    Fixes #1544

---
+++ tests/Checkbox/Checkbox.readonly.test.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Checkbox } from "carbon-components-svelte";
+
+  export let checked = false;
+  export let readonly = false;
+</script>
+
+<Checkbox
+  bind:checked
+  {readonly}
+  labelText="Readonly checkbox test"
+  data-testid="checkbox"
+/>
