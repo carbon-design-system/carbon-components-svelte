7fa5fe405e741c06585086b32f5d555a8ace5878
Eric Liu <ericyl.us@gmail.com>
2025-10-18 13:11:36 -0700

    test(text-input): add more unit tests

---
+++ tests/TextInput/TextInputFluid.test.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { TextInput, FluidForm } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let value: ComponentProps<TextInput>["value"] = "";
+  export let invalid = false;
+  export let invalidText = "";
+  export let warn = false;
+  export let warnText = "";
+  export let helperText = "";
+  export let inline = false;
+</script>
+
+<FluidForm>
+  <TextInput
+    bind:value
+    labelText="User name"
+    {invalid}
+    {invalidText}
+    {warn}
+    {warnText}
+    {helperText}
+    {inline}
+  />
+</FluidForm>
