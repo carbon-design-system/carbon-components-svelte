8ef7500d1f421ff1a92e88bf06a68e7274d58ca5
Eric Liu <ericyl.us@gmail.com>
2025-10-18 15:53:15 -0700

    fix(local-storage): key is also reactive
    Fixes #1204

---
+++ tests/LocalStorage/LocalStorageReactiveKey.test.svelte
@@ -0,0 +1,13 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import { LocalStorage } from "carbon-components-svelte";
+
+  export let storageKey: string = "key-a";
+  export let defaultValue: any = "";
+  export let isObject: boolean = false;
+
+  export let currentValue: any = defaultValue;
+</script>
+
+<LocalStorage key={storageKey} bind:value={currentValue} />
