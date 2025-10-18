89fc35e8a068b50d244ad0400640ea16e40a0cbe
Eric Liu <ericyl.us@gmail.com>
2025-10-18 13:48:37 -0700

    docs(modal): add "Prevent default close behavior" example

---
+++ docs/src/pages/framed/Modal/ModalPreventClose.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { Button, Modal, InlineNotification } from "carbon-components-svelte";
+
+  let open = false;
+  let hasUnsavedChanges = true;
+  let showWarning = false;
+</script>
+
+<Button
+  on:click={() => {
+    open = true;
+    hasUnsavedChanges = true;
+    showWarning = false;
+  }}>Open modal with unsaved changes</Button
+>
+
+<Modal
+  bind:open
+  modalHeading="Edit profile"
+  primaryButtonText="Save"
+  secondaryButtonText="Discard changes"
+  on:click:button--secondary={() => {
+    hasUnsavedChanges = false;
+    open = false;
+  }}
+  on:close={(e) => {
+    console.log("Close triggered by:", e.detail.trigger);
+    if (hasUnsavedChanges) {
+      e.preventDefault();
+      showWarning = true;
+    }
+  }}
+  on:submit={() => {
+    hasUnsavedChanges = false;
+    open = false;
+  }}
+>
+  {#if showWarning}
+    <InlineNotification
+      kind="warning"
+      title="Unsaved changes"
+      subtitle="Please save or discard your changes before closing."
+      hideCloseButton
+    />
+  {/if}
+  <p>You have unsaved changes. Click "Save" to apply them.</p>
+</Modal>
