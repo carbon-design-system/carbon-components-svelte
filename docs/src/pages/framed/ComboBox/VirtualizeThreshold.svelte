7b8e3c2048b4b3a28cb8197c487c31001cbe669c
Eric Liu <ericyl.us@gmail.com>
2025-10-19 10:29:21 -0700

    docs(combo-box): add virtualization example

---
+++ docs/src/pages/framed/ComboBox/VirtualizeThreshold.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { ComboBox } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 100 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let value = "";
+  let selectedId = undefined;
+</script>
+
+<ComboBox
+  virtualize={{ threshold: 200 }}
+  labelText="Custom threshold (threshold: 200)"
+  placeholder="Filter..."
+  {items}
+  shouldFilterItem={(item, value) =>
+    item.text.toLowerCase().includes(value.toLowerCase())}
+  bind:selectedId
+  bind:value
+/>
