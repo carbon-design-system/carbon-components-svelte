0574a9a7f9c965f18dde6eb8106a9769440a4461
Eric Liu <ericyl.us@gmail.com>
2025-10-18 22:28:44 -0700

    test(data-table): add more unit tests

---
+++ tests/DataTable/DataTableCustomDescription.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { DataTable } from "carbon-components-svelte";
+
+  const headers = [
+    { key: "name", value: "Name" },
+    { key: "protocol", value: "Protocol" },
+  ] as const;
+
+  const rows = [
+    {
+      id: "a",
+      name: "Load Balancer 1",
+      protocol: "HTTP",
+    },
+  ];
+</script>
+
+<DataTable {headers} {rows}>
+  <div slot="description" let:props {...props}>Custom Description</div>
+</DataTable>
