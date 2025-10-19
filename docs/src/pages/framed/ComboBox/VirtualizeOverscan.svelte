945390b657c2c5187496ce11bbcf959f516caee6
Eric Liu <ericyl.us@gmail.com>
2025-10-19 10:29:21 -0700

    docs(combo-box): add virtualization example

---
+++ docs/src/pages/framed/ComboBox/VirtualizeOverscan.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { ComboBox } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 10_000 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let value = "";
+  let selectedId = undefined;
+</script>
+
+<ComboBox
+  virtualize={{ overscan: 100 }}
+  labelText="High overscan (10,000 items, overscan: 100)"
+  placeholder="Filter..."
+  {items}
+  shouldFilterItem={(item, value) =>
+    item.text.toLowerCase().includes(value.toLowerCase())}
+  bind:selectedId
+  bind:value
+/>
