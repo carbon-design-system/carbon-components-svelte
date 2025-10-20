9b3e071899bdfa353f114a8f41f16d46d6cde481
Eric Liu <ericyl.us@gmail.com>
2025-10-20 06:58:27 -0700

    docs(combo-box): add "Clear filter on open" example

---
+++ docs/src/pages/framed/ComboBox/ClearFilterOnOpen.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { ComboBox } from "carbon-components-svelte";
+
+  function shouldFilterItem(item, value) {
+    if (!value) return true;
+    return item.text.toLowerCase().includes(value.toLowerCase());
+  }
+</script>
+
+<ComboBox
+  titleText="Contact"
+  placeholder="Select contact method"
+  items={[
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ]}
+  {shouldFilterItem}
+  clearFilterOnOpen
+  helperText="Select an item, then reopen to see all items again"
+/>
