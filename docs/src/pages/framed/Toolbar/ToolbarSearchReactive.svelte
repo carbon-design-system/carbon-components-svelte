6ef90eeb0579d5319aa5bedf20e1ab5da4662504
Eric Liu <ericyl.us@gmail.com>
2025-10-20 06:14:40 -0700

    docs(toolbar): add dedicated docs
    Closes #1515

---
+++ docs/src/pages/framed/Toolbar/ToolbarSearchReactive.svelte
@@ -0,0 +1,46 @@
+<script>
+  import {
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+    ButtonSet,
+    Button,
+  } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<Toolbar>
+  <ToolbarContent>
+    <ToolbarSearch bind:value placeholder="Search..." />
+    <Button size="small">Create</Button>
+  </ToolbarContent>
+</Toolbar>
+
+<div>
+  <ButtonSet>
+    <Button
+      size="small"
+      disabled={value === "products"}
+      on:click={() => (value = "products")}
+    >
+      Set value
+    </Button>
+    <Button
+      kind="ghost"
+      size="small"
+      disabled={value.length === 0}
+      on:click={() => (value = "")}
+    >
+      Clear value
+    </Button>
+  </ButtonSet>
+</div>
+
+<div>Search value: {value}</div>
+
+<style>
+  div {
+    margin-top: var(--cds-spacing-05);
+  }
+</style>
