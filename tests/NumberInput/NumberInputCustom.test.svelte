ee290e5057ef8791e00218502c13efa99ea5a677
spburtsev <spburtsev@proton.me>
2025-11-09 19:58:56 +0100

    fix(number-input)!: rename `label` to `labelText`

--- tests/NumberInput/NumberInputCustom.test.svelte
+++ tests/NumberInput/NumberInputCustom.test.svelte
@@ -2,6 +2,6 @@
   import { NumberInput } from "carbon-components-svelte";
 </script>
 
-<NumberInput label="Custom label" value={0}>
-  <span slot="label">Custom Label Text</span>
+<NumberInput labelText="Custom label" value={0}>
+  <span slot="labelText">Custom Label Text</span>
 </NumberInput>
