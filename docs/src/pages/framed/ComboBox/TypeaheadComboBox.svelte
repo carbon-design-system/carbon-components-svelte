40ab3b71791dc35714cf7e16f50abff5095f9362
Sergei Burtsev <spburtsev@proton.me>
2025-11-13 16:34:26 +0100

    feat(combo-box): add `typeahead` prop for autocomplete (#2340)
    Closes #2275

---
+++ docs/src/pages/framed/ComboBox/TypeaheadComboBox.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Button, ComboBox } from "carbon-components-svelte";
+
+  let selectedId = undefined;
+
+  const items = [
+    "Apple",
+    "Apricot",
+    "Avocado",
+    "Banana",
+    "Blackberry",
+    "Blueberry",
+    "Cantaloupe",
+  ].map((item) => ({ id: item, text: item }));
+</script>
+
+<ComboBox
+  labelText="Item"
+  placeholder="Select an item"
+  bind:selectedId
+  typeahead
+  {items}
+/>
+<br />
+<Button on:click={() => (selectedId = undefined)}
+  >Set to undefined (unselected)</Button
+>
