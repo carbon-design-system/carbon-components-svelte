c51ac5692981fbc3084342a669526f3f9648dce7
Eric Liu <ericyl.us@gmail.com>
2025-11-29 08:10:20 -0800

    docs(selectable-tile): add/update examples

---
+++ docs/src/pages/framed/SelectableTile/SelectableTileReactive.svelte
@@ -0,0 +1,35 @@
+<script>
+  import {
+    Button,
+    SelectableTile,
+    SelectableTileGroup,
+  } from "carbon-components-svelte";
+
+  const values = ["Lite plan", "Standard plan", "Plus plan"];
+
+  let selected = [values[0], values[1]];
+</script>
+
+<SelectableTileGroup legend="Service pricing tiers" name="plan" bind:selected>
+  {#each values as value}
+    <SelectableTile {value}>{value}</SelectableTile>
+  {/each}
+</SelectableTileGroup>
+
+<div>
+  Selected: <strong>{selected.join(", ") || "None"}</strong>
+</div>
+
+<Button
+  size="small"
+  disabled={selected.length === 1 && selected[0] === values[1]}
+  on:click={() => (selected = [values[1]])}
+>
+  Set to "{values[1]}" only
+</Button>
+
+<style>
+  div {
+    margin: var(--cds-spacing-05) 0;
+  }
+</style>
---
+++ docs/src/pages/framed/SelectableTile/SelectableTileReactiveOneWay.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {
+    SelectableTile,
+    SelectableTileGroup,
+  } from "carbon-components-svelte";
+
+  const values = ["Lite plan", "Standard plan", "Plus plan"];
+
+  let selected = [values[0], values[1]];
+</script>
+
+<SelectableTileGroup
+  legend="Service pricing tiers"
+  name="plan"
+  on:select={({ detail }) => (selected = [...selected, detail])}
+  on:deselect={({ detail }) => (selected = selected.filter((v) => v !== detail))}
+>
+  {#each values as value}
+    <SelectableTile {value} selected={selected.includes(value)}>
+      {value}
+    </SelectableTile>
+  {/each}
+</SelectableTileGroup>
+
+<br />
+
+Selected: <strong>{selected.join(", ") || "None"}</strong>
