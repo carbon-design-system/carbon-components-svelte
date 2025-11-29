72818c0c7a91938b1303119611c4838c94ac5b3a
Eric Liu <ericyl.us@gmail.com>
2025-11-29 11:19:39 -0800

    docs(portal): add examples

---
+++ docs/src/pages/framed/Portal/BasicPortal.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Portal } from "carbon-components-svelte";
+</script>
+
+<div>
+  <div>This is rendered inside the div</div>
+  <br />
+  <Portal>This is rendered outside of the div</Portal>
+</div>
---
+++ docs/src/pages/framed/Portal/ComposedModalPortal.svelte
@@ -0,0 +1,39 @@
+<script>
+  import {
+    Button,
+    ComposedModal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    Portal,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Stack gap={5} style="overflow: hidden; position: relative; height: 200px;">
+  <p>
+    This container hides overflowing content. Without a portal, the modal would
+    be clipped.
+  </p>
+  <div>
+    <Button on:click={() => (open = true)}>Open modal</Button>
+  </div>
+  <Portal>
+    <ComposedModal bind:open>
+      <ModalHeader title="Composed Modal in Portal" />
+      <ModalBody>
+        <p>
+          This composed modal is rendered in a portal, ensuring it appears above
+          all z-index stacking contexts and parent overflow constraints.
+        </p>
+      </ModalBody>
+      <ModalFooter
+        primaryButtonText="Confirm"
+        secondaryButtonText="Cancel"
+        on:click:button--secondary={() => (open = false)}
+      />
+    </ComposedModal>
+  </Portal>
+</Stack>
