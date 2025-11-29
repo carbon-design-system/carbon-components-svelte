0ad9ab2925e7b90bc3473fe6f6149c1c779379f0
Eric Liu <ericyl.us@gmail.com>
2025-11-29 12:26:27 -0800

    docs(notification-queue): add examples

---
+++ docs/src/pages/framed/NotificationQueue/NotificationQueueDeduplication.svelte
@@ -0,0 +1,40 @@
+<script>
+  import {
+    Button,
+    ButtonSet,
+    NotificationQueue,
+  } from "carbon-components-svelte";
+
+  let queue;
+  let count = 0;
+</script>
+
+<NotificationQueue bind:this={queue} />
+
+<ButtonSet>
+  <Button
+    on:click={() => {
+      queue.add({
+        id: "id",
+        kind: "warning",
+        title: "Duplicate notification",
+        subtitle:
+          "This notification has the same id. Click the button multiple times.",
+      });
+    }}
+  >
+    Add duplicate
+  </Button>
+  <Button
+    on:click={() => {
+      queue.add({
+        id: "id-" + count++,
+        kind: "info",
+        title: "Unique notification",
+        subtitle: "This has a unique id, so it will always appear.",
+      });
+    }}
+  >
+    Add unique
+  </Button>
+</ButtonSet>
---
+++ docs/src/pages/framed/NotificationQueue/NotificationQueueOffsets.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Button, NotificationQueue } from "carbon-components-svelte";
+
+  let queue;
+</script>
+
+<NotificationQueue
+  bind:this={queue}
+  position="top-right"
+  offsetTop="5rem"
+  offsetRight="2rem"
+/>
+
+<Button
+    on:click={() => {
+      queue.add({
+        kind: "success",
+        title: "Custom offset",
+      });
+    }}
+  >
+    Add notification
+  </Button>
+
