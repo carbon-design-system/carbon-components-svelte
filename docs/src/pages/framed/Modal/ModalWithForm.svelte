4e4bf1d50890bd7211bd4bccbedfa0ef28ffb6be
Eric Liu <ericyl.us@gmail.com>
2025-11-30 09:19:14 -0800

    docs(modal): add "With form submission" example
    Closes #310

---
+++ docs/src/pages/framed/Modal/ModalWithForm.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { Button, Modal, Stack, TextInput } from "carbon-components-svelte";
+
+  let open = false;
+  let name = "";
+  let email = "";
+
+  function handleSubmit() {
+    console.log("Form submitted:", { name, email });
+    open = false;
+  }
+</script>
+
+<Button on:click={() => (open = true)}>Create account</Button>
+
+<Modal
+  bind:open
+  modalHeading="Create account"
+  primaryButtonText="Submit"
+  secondaryButtonText="Cancel"
+  hasForm
+  formId="account-form"
+  on:click:button--secondary={() => (open = false)}
+>
+  <form id="account-form" on:submit|preventDefault={handleSubmit}>
+    <Stack gap={5}>
+      <TextInput bind:value={name} labelText="Name" placeholder="Enter name" />
+      <TextInput
+        bind:value={email}
+        labelText="Email"
+        type="email"
+        placeholder="Enter email"
+      />
+    </Stack>
+  </form>
+</Modal>
