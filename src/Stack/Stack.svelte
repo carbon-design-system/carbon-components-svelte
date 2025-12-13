b6a72633397aaa57eb0ef310f02899cc93610389
Eric Liu <ericyl.us@gmail.com>
2025-12-13 12:31:36 -0800

    chore(stack): include JSDoc description for `StackScale` type

--- src/Stack/Stack.svelte
+++ src/Stack/Stack.svelte
@@ -1,16 +1,16 @@
 <script>
   /**
    * The stack scale maps to the following `@carbon/layout` values:
-   * - 0 --> 0 (no gap)
-   * - 1 --> 0.125rem
-   * - 2 --> 0.25rem
-   * - 3 --> 0.5rem
-   * - 4 --> 0.75rem
-   * - 5 --> 1rem
-   * - 6 --> 1.5rem
-   * - 7 --> 2rem
-   * - 8 --> 2.5rem
-   * - 9 --> 3rem
+   * - 0  --> 0 (no gap)
+   * - 1  --> 0.125rem
+   * - 2  --> 0.25rem
+   * - 3  --> 0.5rem
+   * - 4  --> 0.75rem
+   * - 5  --> 1rem
+   * - 6  --> 1.5rem
+   * - 7  --> 2rem
+   * - 8  --> 2.5rem
+   * - 9  --> 3rem
    * - 10 --> 4rem
    * - 11 --> 5rem
    * - 12 --> 6rem
--- types/Stack/Stack.svelte.d.ts
+++ types/Stack/Stack.svelte.d.ts
@@ -1,6 +1,23 @@
 import { SvelteComponentTyped } from "svelte";
 import type { HTMLAttributes } from "svelte/elements";
 
+/**
+ * The stack scale maps to the following `@carbon/layout` values:
+ * - 0  --> 0 (no gap)
+ * - 1  --> 0.125rem
+ * - 2  --> 0.25rem
+ * - 3  --> 0.5rem
+ * - 4  --> 0.75rem
+ * - 5  --> 1rem
+ * - 6  --> 1.5rem
+ * - 7  --> 2rem
+ * - 8  --> 2.5rem
+ * - 9  --> 3rem
+ * - 10 --> 4rem
+ * - 11 --> 5rem
+ * - 12 --> 6rem
+ * - 13 --> 10rem
+ */
 export type StackScale =
   | 0
   | 1
