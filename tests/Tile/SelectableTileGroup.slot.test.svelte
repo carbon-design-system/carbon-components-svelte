1ed3b6a10af9ba6d65596256d3b36d38d93b5afa
Eric Liu <ericyl.us@gmail.com>
2025-12-13 11:50:38 -0800

    feat(selectable-tile-group): add legendChildren slot

---
+++ tests/Tile/SelectableTileGroup.slot.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { SelectableTileGroup } from "carbon-components-svelte";
+</script>
+
+<SelectableTileGroup legendText="Default legend">
+  <span slot="legendChildren">Custom legend content</span>
+</SelectableTileGroup>
