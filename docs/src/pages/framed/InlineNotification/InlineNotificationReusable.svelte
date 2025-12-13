8cd22af2bb537bf36bc98d7c4e874f9555c91601
Eric Liu <ericyl.us@gmail.com>
2025-12-13 06:41:45 -0800

    docs(inline-notification): add "Reusable notification" example
    Supports #630

---
+++ docs/src/pages/framed/InlineNotification/InlineNotificationReusable.svelte
@@ -0,0 +1,44 @@
+<script>
+  import {
+    Button,
+    ButtonSet,
+    InlineNotification,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let open = false;
+  let kind = "error";
+  let title = "Error:";
+  let subtitle = "An internal server error occurred.";
+
+  function showError() {
+    kind = "error";
+    title = "Error:";
+    subtitle = "An internal server error occurred.";
+    open = true;
+  }
+
+  function showSuccess() {
+    kind = "success";
+    title = "Success:";
+    subtitle = "Your settings have been saved.";
+    open = true;
+  }
+
+  function showWarning() {
+    kind = "warning";
+    title = "Warning:";
+    subtitle = "Please review your changes before continuing.";
+    open = true;
+  }
+</script>
+
+<Stack gap={5}>
+  <ButtonSet>
+    <Button on:click={showSuccess}>Show success</Button>
+    <Button kind="ghost" on:click={showWarning}>Show warning</Button>
+    <Button kind="danger-ghost" on:click={showError}>Show error</Button>
+  </ButtonSet>
+
+  <InlineNotification bind:open {kind} {title} {subtitle} />
+</Stack>
