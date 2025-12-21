b70f3c1b077696bec06d39d0808ab72a69dc48dd
Eric Liu <ericyl.us@gmail.com>
2025-12-21 13:59:05 -0800

    docs(accordion): add "Batch disable" example

---
+++ docs/src/pages/framed/Accordion/BatchDisableAccordion.svelte
@@ -0,0 +1,57 @@
+<script>
+  import {
+    Accordion,
+    AccordionItem,
+    Button,
+    ButtonSet,
+  } from "carbon-components-svelte";
+
+  let disabled = false;
+
+  const items = [
+    {
+      title: "Natural Language Classifier",
+      description:
+        "Natural Language Classifier uses advanced natural language processing and machine learning techniques to create custom classification models. Users train their data and the service predicts the appropriate category for the inputted text.",
+    },
+    {
+      title: "Natural Language Understanding",
+      description:
+        "Analyze text to extract meta-data from content such as concepts, entities, emotion, relations, sentiment and more.",
+    },
+    {
+      title: "Language Translator",
+      description:
+        "Translate text, documents, and websites from one language to another. Create industry or region-specific translations via the service's customization capability.",
+    },
+  ];
+
+  let open = false;
+</script>
+
+<ButtonSet>
+  <Button size="field" {disabled} on:click={() => (open = !open)}>
+    {open ? "Collapse" : "Expand"} all
+  </Button>
+  <Button
+    kind="ghost"
+    size="field"
+    on:click={() => {
+      disabled = !disabled;
+      if (disabled) {
+        open = false;
+      }
+    }}
+  >
+    {disabled ? "Enable" : "Disable"} all
+  </Button>
+</ButtonSet>
+
+<Accordion {disabled}>
+  {#each items as item}
+    <AccordionItem title={item.title} {open}>
+      <p>{item.description}</p>
+    </AccordionItem>
+  {/each}
+</Accordion>
+
