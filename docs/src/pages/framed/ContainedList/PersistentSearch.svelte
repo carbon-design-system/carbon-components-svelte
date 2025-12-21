d09c2ed1d438e1287d783bcfcd27779b66bc4036
Eric Liu <ericyl.us@gmail.com>
2025-12-21 12:28:44 -0800

    docs(contained-list): add examples

---
+++ docs/src/pages/framed/ContainedList/PersistentSearch.svelte
@@ -0,0 +1,26 @@
+<script>
+  import {
+    ContainedList,
+    ContainedListItem,
+    Search,
+  } from "carbon-components-svelte";
+
+  let searchTerm = "";
+  let searchResults = Array.from(
+    { length: 50 },
+    (_, i) => "List item " + (i + 1),
+  );
+
+  $: filteredResults = searchResults.filter((item) => {
+    const lowerItem = item.toLowerCase();
+    const lowerSearch = searchTerm.toLowerCase();
+    return lowerItem.includes(lowerSearch);
+  });
+</script>
+
+<ContainedList kind="disclosed" labelText="List title">
+  <Search placeholder="Filterable search" bind:value={searchTerm} />
+  {#each filteredResults as item}
+    <ContainedListItem>{item}</ContainedListItem>
+  {/each}
+</ContainedList>
