e945de4be99248a7732c72d40355f5d921bbd82a
Eric Liu <ericyl.us@gmail.com>
2025-12-23 14:48:26 -0800

    test(checkbox): add reactivity and dispatched events cases

---
+++ tests/Checkbox/CheckboxReactiveBind.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Checkbox } from "carbon-components-svelte";
+
+  export let checked = false;
+
+  export function setChecked(value: boolean) {
+    checked = value;
+  }
+</script>
+
+<Checkbox
+  bind:checked
+  labelText="Reactive bind test"
+  data-testid="checkbox-reactive"
+  on:check={() => console.log("check")}
+/>
+
