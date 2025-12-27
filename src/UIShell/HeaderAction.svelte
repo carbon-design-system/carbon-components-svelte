d67b92a99a2069b66bc6f11b9f9ef2b29396171a
Eric Liu <ericyl.us@gmail.com>
2025-12-27 09:11:36 -0800

    feat(ui-shell): support theming
    Closes #2457

--- src/UIShell/HeaderAction.svelte
+++ src/UIShell/HeaderAction.svelte
@@ -139,26 +139,3 @@
     <slot />
   </div>
 {/if}
-
-<style>
-  :global(.bx--header__action--text) {
-    display: inline-flex;
-    align-items: center;
-    width: auto;
-
-    /** 2px bottom padding aligns icon with `HeaderAction` */
-    padding: 0 1rem 2px 1rem;
-
-    /** `body-short-01` styles */
-    font-size: 0.875rem;
-    line-height: 1.28572;
-    letter-spacing: 0.16px;
-
-    /** Same color as `Header` platformName */
-    color: #f4f4f4;
-  }
-
-  :global(.bx--header__action-text) {
-    margin-left: 0.75rem;
-  }
-</style>
