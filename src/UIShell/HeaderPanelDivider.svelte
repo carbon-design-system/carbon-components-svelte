83af86b918402e8cd2f31a38faee233eca05afa3
Eric Liu <ericyl.us@gmail.com>
2025-12-27 09:11:36 -0800

    feat(ui-shell): support theming
    Closes #2457

--- src/UIShell/HeaderPanelDivider.svelte
+++ src/UIShell/HeaderPanelDivider.svelte
@@ -2,19 +2,3 @@
   <li class:bx--header-panel-divider={true}><slot /></li>
 {/if}
 <hr class:bx--switcher__item--divider={true}>
-
-<style>
-  /**
-   * Carbon does not support a divider with a subject.
-   * We apply custom styles using the switcher subject divider
-   * from https://carbondesignsystem.com/ as a reference.
-   */
-
-  :global(.bx--header-panel-divider) {
-    margin: 2rem 1rem 0;
-    font-size: 0.75rem;
-    line-height: 1.3;
-    letter-spacing: 0.02rem;
-    color: #c6c6c6;
-  }
-</style>
