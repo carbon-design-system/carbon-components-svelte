c7d8a9e3d5da4e8ec6b81eeefb53b0b49935b117
Eric Liu <ericyl.us@gmail.com>
2026-01-20 13:23:42 -0800

    docs(number-input): update "Preserve decimal input" example

---
+++ docs/src/pages/framed/NumberInput/NumberInputDecimal.svelte
@@ -0,0 +1,31 @@
+<script>
+  import {
+    Button,
+    ButtonSet,
+    NumberInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let value = 1.5;
+</script>
+
+<Stack gap={4}>
+  <NumberInput
+    allowDecimal
+    allowEmpty
+    bind:value
+    step={0.01}
+    labelText="Amount"
+  />
+
+  <ButtonSet>
+    <Button on:click={() => (value = null)}>Set to null</Button>
+    <Button on:click={() => (value = 0)}>Set to 0</Button>
+    <Button on:click={() => (value = 1.23)}>Set to 1.23</Button>
+  </ButtonSet>
+
+  <div>
+    <strong>Value:</strong>
+    {value}
+  </div>
+</Stack>
