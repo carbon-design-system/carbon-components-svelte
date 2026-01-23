86d4287dd8b824a8a6b6cce81ed3071c06bdd2ee
Eric Liu <ericyl.us@gmail.com>
2026-01-23 09:26:51 -0800

    docs: add sitemap

---
+++ docs/scripts/generate-sitemap.js
@@ -0,0 +1,39 @@
+// @ts-check
+import fs from "node:fs";
+
+const BASE_URL = "https://svelte.carbondesignsystem.com";
+const COMPONENTS_PATH = "./src/pages/components";
+const SITEMAP_PATH = "./public/sitemap.xml";
+
+const files = fs.readdirSync(COMPONENTS_PATH);
+
+/** @type {string[]} */
+const urls = [];
+
+// Root page
+urls.push(`
+  <url>
+    <loc>${BASE_URL}/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>`);
+
+// Component pages
+for (const file of files) {
+  const [componentName] = file.split(".");
+  urls.push(`
+  <url>
+    <loc>${BASE_URL}/components/${componentName}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`);
+}
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join("")}
+</urlset>
+`;
+
+fs.writeFileSync(SITEMAP_PATH, sitemap);
+
+console.log(`Sitemap generated with ${urls.length} URLs`);
