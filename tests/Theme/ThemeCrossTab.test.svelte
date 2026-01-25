37c91ef4227cfb0c1499ace6ce1628e99a5e289e
Eric Liu <ericyl.us@gmail.com>
2026-01-25 11:25:23 -0800

    test(theme): add auto-sync tests

---
+++ tests/Theme/ThemeCrossTab.test.svelte
@@ -0,0 +1,19 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import { Theme } from "carbon-components-svelte";
+  import type { CarbonTheme } from "carbon-components-svelte/Theme/Theme.svelte";
+
+  export let theme: CarbonTheme = "white";
+  export let persist = true;
+  export let persistKey = "theme";
+</script>
+
+<Theme
+  bind:theme
+  {persist}
+  {persistKey}
+  on:update={({ detail }) => {
+    console.log("update", detail);
+  }}
+/>
