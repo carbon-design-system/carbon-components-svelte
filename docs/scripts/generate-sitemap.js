d4c2defe1494477edde691204e14c2dcf5be7fd9
Eric Liu <ericyl.us@gmail.com>
2026-01-25 11:51:30 -0800

    docs: generate llms.txt

--- docs/scripts/generate-sitemap.js
+++ docs/scripts/generate-sitemap.js
@@ -29,6 +29,14 @@ for (const file of files) {
   </url>`);
 }
 
+// llms.txt
+urls.push(`
+  <url>
+    <loc>${BASE_URL}/llms.txt</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.5</priority>
+  </url>`);
+
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join("")}
 </urlset>
