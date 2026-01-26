e92d593a87fa99475fe2beac95c7c91755c035a0
Eric Liu <ericyl.us@gmail.com>
2026-01-25 16:58:36 -0800

    docs(data-table): add virtualization examples

---
+++ docs/src/pages/framed/DataTable/VirtualizeRowHeight.svelte
@@ -0,0 +1,46 @@
+<script>
+  import {
+    DataTable,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+  } from "carbon-components-svelte";
+
+  const rows = Array.from({ length: 10_000 }, (_, i) => ({
+    id: i,
+    name: "Load Balancer " + (i + 1),
+    protocol: "HTTP",
+    port: 3000 + i * 10,
+  }));
+
+  let filteredRowIds = [];
+</script>
+
+<DataTable
+  sortable
+  virtualize={{ itemHeight: 20 }}
+  rowClass="virtualize-tall-row"
+  headers={[
+    { key: "name", value: "Name" },
+    { key: "protocol", value: "Protocol" },
+    { key: "port", value: "Port" },
+  ]}
+  {rows}
+>
+  <Toolbar>
+    <ToolbarContent>
+      <ToolbarSearch
+        persistent
+        placeholder="Search 10,000 rows..."
+        shouldFilterRows
+        bind:filteredRowIds
+      />
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
+
+<style>
+  :global(.bx--data-table-container tbody tr.virtualize-tall-row) {
+    height: 24px;
+  }
+</style>
