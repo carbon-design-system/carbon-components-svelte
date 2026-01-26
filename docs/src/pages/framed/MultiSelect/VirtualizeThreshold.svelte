e4210023162e012d067835c4eb64baa8a63c6180
Eric Liu <ericyl.us@gmail.com>
2026-01-25 16:51:05 -0800

    docs(multi-select): add virtualization example

---
+++ docs/src/pages/framed/MultiSelect/VirtualizeThreshold.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { MultiSelect } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 100 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let selectedIds = [50];
+</script>
+
+<MultiSelect
+  virtualize={{ threshold: 200 }}
+  labelText="Custom threshold (threshold: 200)"
+  {items}
+  bind:selectedIds
+/>
