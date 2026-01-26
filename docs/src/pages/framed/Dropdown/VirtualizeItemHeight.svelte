239d5010362e0463c560e04174fb61a278b8b3ae
Eric Liu <ericyl.us@gmail.com>
2026-01-25 16:34:53 -0800

    docs(dropdown): add virtualization example

---
+++ docs/src/pages/framed/Dropdown/VirtualizeItemHeight.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Dropdown, Stack } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 10_000 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+    description: "Description for item " + (i + 1),
+  }));
+
+  let selectedId = 1000;
+</script>
+
+<Dropdown
+  virtualize={{ itemHeight: 60 }}
+  labelText="Custom item height (60px)"
+  {items}
+  bind:selectedId
+  let:item
+>
+  <Stack gap={2}>
+    <strong>{item.text}</strong>
+    <span>{item.description}</span>
+  </Stack>
+</Dropdown>
---
+++ docs/src/pages/framed/Dropdown/VirtualizedDropdown.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Dropdown, Stack } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 10_000 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let selectedId = 1000;
+  $: selectedItem = items.find((item) => item.id === selectedId);
+</script>
+
+<Stack gap={5}>
+  <Dropdown
+    virtualize={true}
+    labelText="Virtualized Dropdown (10,000 items)"
+    {items}
+    bind:selectedId
+  />
+  <div>
+    <strong>Selected:</strong>
+    {selectedItem?.text}
+  </div>
+</Stack>
