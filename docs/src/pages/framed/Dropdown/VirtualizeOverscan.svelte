2ff43fca0b87746b6434bf86ac56a97025d344f6
Eric Liu <ericyl.us@gmail.com>
2026-01-25 16:34:53 -0800

    docs(dropdown): add virtualization example

---
+++ docs/src/pages/framed/Dropdown/VirtualizeOverscan.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Dropdown } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 10_000 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let selectedId = 1000;
+</script>
+
+<Dropdown
+  virtualize={{ overscan: 100 }}
+  labelText="High overscan (10,000 items, overscan: 100)"
+  {items}
+  bind:selectedId
+/>
---
+++ docs/src/pages/framed/Dropdown/VirtualizeThreshold.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Dropdown } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 100 }, (_, i) => ({
+    id: i,
+    text: "Item " + (i + 1),
+  }));
+
+  let selectedId = 50;
+</script>
+
+<Dropdown
+  virtualize={{ threshold: 200 }}
+  labelText="Custom threshold (threshold: 200)"
+  {items}
+  bind:selectedId
+/>
