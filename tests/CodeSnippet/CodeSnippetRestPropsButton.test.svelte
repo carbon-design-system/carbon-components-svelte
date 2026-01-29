f12249762aa44b4e90ee9422ab18331d2575c651
Eric Liu <ericyl.us@gmail.com>
2026-01-29 09:31:29 -0800

    test(code-snippet): `$$restProps` regression tests

---
+++ tests/CodeSnippet/CodeSnippetRestPropsButton.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { CodeSnippet } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  const props: ComponentProps<CodeSnippet> = {
+    type: "inline",
+    code: "inline code",
+    "data-testid": "snippet-rest-btn",
+  };
+</script>
+
+<CodeSnippet {...props} />
---
+++ tests/CodeSnippet/CodeSnippetRestPropsSingle.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { CodeSnippet } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  const props: ComponentProps<CodeSnippet> = {
+    type: "single",
+    code: "single line",
+    "data-testid": "snippet-rest-div",
+  };
+</script>
+
+<CodeSnippet {...props} />
