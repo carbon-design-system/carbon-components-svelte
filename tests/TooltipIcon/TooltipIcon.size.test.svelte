7469b7d86fca4a041e9fe2a892aa582b229bbd66
Eric Liu <ericyl.us@gmail.com>
2026-01-28 20:18:58 -0800

    feat(tooltip-icon): add `size` prop
    Closes #2587

---
+++ tests/TooltipIcon/TooltipIcon.size.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { TooltipIcon } from "carbon-components-svelte";
+  import MockIcon from "./MockIcon.svelte";
+
+  export let size = 16;
+</script>
+
+<TooltipIcon tooltipText="Test" icon={MockIcon} {size} />
