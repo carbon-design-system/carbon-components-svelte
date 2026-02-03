c39318c74ed0099f267c5b5b71f8167e42fee447
Eric Liu <ericyl.us@gmail.com>
2026-02-02 17:52:57 -0800

    fix(header-search): don't render "Clear search" button if inactive

--- src/UIShell/HeaderSearch.svelte
+++ src/UIShell/HeaderSearch.svelte
@@ -160,20 +160,21 @@
       }}
       on:paste
     />
-    <button
-      type="button"
-      aria-label="Clear search"
-      tabindex={active ? "0" : "-1"}
-      class:bx--header__action={true}
-      class:bx--header-search-button={true}
-      class:bx--header-search-button--hidden={!active}
-      on:click={() => {
-        reset();
-        dispatch("clear");
-      }}
-    >
-      <Close size={20} title="Close" />
-    </button>
+    {#if active}
+      <button
+        type="button"
+        aria-label="Clear search"
+        tabindex={active ? "0" : "-1"}
+        class:bx--header__action={true}
+        class:bx--header-search-button={true}
+        on:click={() => {
+          reset();
+          dispatch("clear");
+        }}
+      >
+        <Close size={20} title="Close" />
+      </button>
+    {/if}
   </div>
 
   {#if active && results.length > 0}
