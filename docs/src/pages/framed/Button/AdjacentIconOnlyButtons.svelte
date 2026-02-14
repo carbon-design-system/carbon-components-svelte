e2960297bb1a1aea35af3ebacbd4d43f2941617d
Eric Liu <ericyl.us@gmail.com>
2026-02-14 10:29:10 -0800

    docs(button): add "Adjacent icon-only buttons" example

---
+++ docs/src/pages/framed/Button/AdjacentIconOnlyButtons.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Button } from "carbon-components-svelte";
+  import Add from "carbon-icons-svelte/lib/Add.svelte";
+  import Save from "carbon-icons-svelte/lib/Save.svelte";
+  import TrashCan from "carbon-icons-svelte/lib/TrashCan.svelte";
+
+  let lastAction = "";
+</script>
+
+<Button
+  kind="ghost"
+  iconDescription="Add"
+  tooltipPosition="right"
+  icon={Add}
+  on:click={() => (lastAction = "Add")}
+/>
+<Button
+  kind="ghost"
+  iconDescription="Delete"
+  tooltipPosition="right"
+  icon={TrashCan}
+  on:click={() => (lastAction = "Delete")}
+/>
+<Button
+  kind="ghost"
+  iconDescription="Save"
+  tooltipPosition="right"
+  icon={Save}
+  on:click={() => (lastAction = "Save")}
+/>
+
+{#if lastAction}
+  Clicked: {lastAction}
+{/if}
