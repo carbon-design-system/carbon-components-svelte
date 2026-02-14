532066a448a0ffa2ba89497c6d96327a24d8188e
Eric Liu <ericyl.us@gmail.com>
2026-02-14 08:41:14 -0800

    feat(floating-portal): add `FloatingPortal`

---
+++ tests/Portal/FloatingPortal.test.svelte
@@ -0,0 +1,20 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import FloatingPortal from "../../src/Portal/FloatingPortal.svelte";
+
+  export let open = false;
+  export let direction: "top" | "bottom" = "bottom";
+  export let zIndex = 9200;
+  export let anchor: HTMLElement | null = null;
+  export let ref: HTMLElement | null = null;
+  export let content = "Floating content";
+</script>
+
+<div data-testid="anchor" bind:this={anchor}>
+  Anchor element
+</div>
+
+<FloatingPortal {anchor} {direction} {open} {zIndex} bind:ref>
+  {content}
+</FloatingPortal>
