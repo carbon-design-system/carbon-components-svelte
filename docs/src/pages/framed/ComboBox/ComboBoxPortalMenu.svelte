c253a09256d1814b3afec688a6b7b70a6c8ecebd
Eric Liu <ericyl.us@gmail.com>
2026-02-14 09:25:09 -0800

    docs(combo-box): add "Portal menu" example

---
+++ docs/src/pages/framed/ComboBox/ComboBoxPortalMenu.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Button, ComboBox, Modal } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Modal
+  bind:open
+  size="sm"
+  modalHeading="Add a contact"
+  primaryButtonText="Add"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (open = false)}
+>
+  <ComboBox
+    portalMenu
+    light
+    labelText="Contact method"
+    placeholder="Select contact method"
+    items={[
+      { id: "0", text: "Slack" },
+      { id: "1", text: "Email" },
+      { id: "2", text: "Fax" },
+      { id: "3", text: "Teams" },
+      { id: "4", text: "Phone" },
+    ]}
+  />
+</Modal>
