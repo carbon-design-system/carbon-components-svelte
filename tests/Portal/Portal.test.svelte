48d1676f589c4fc77ef213101374e53fa26018ba
Eric Liu <ericyl.us@gmail.com>
2026-02-14 08:31:39 -0800

    feat(portal): add `ref` prop for element access

--- tests/Portal/Portal.test.svelte
+++ tests/Portal/Portal.test.svelte
@@ -1,13 +1,16 @@
+<svelte:options accessors />
+
 <script lang="ts">
   import { Portal } from "carbon-components-svelte";
 
   export let showPortal = true;
   export let portalContent = "Portal content";
   export let tag: keyof HTMLElementTagNameMap = "div";
+  export let ref: HTMLElement | null = null;
 </script>
 
 {#if showPortal}
-  <Portal {tag} {...$$restProps}>
+  <Portal {tag} bind:ref {...$$restProps}>
     {portalContent}
   </Portal>
 {/if}
