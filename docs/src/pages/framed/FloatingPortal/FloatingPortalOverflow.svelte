7949fb201c7ffb1d97c61d86f026625eba64e945
Eric Liu <ericyl.us@gmail.com>
2026-02-14 11:45:16 -0800

    docs(floating-portal): add docs

---
+++ docs/src/pages/framed/FloatingPortal/FloatingPortalOverflow.svelte
@@ -0,0 +1,22 @@
+<script>
+  import {
+    Button,
+    FloatingPortal,
+    Stack,
+    Tile,
+  } from "carbon-components-svelte";
+
+  let anchor = null;
+  let open = false;
+</script>
+
+<Stack gap={4} style="overflow: hidden; border: 1px dashed var(--cds-border-subtle); padding: 1rem">
+  <div>This container has hidden overflow.</div>
+  <Button bind:ref={anchor} on:click={() => (open = !open)}>
+    Toggle floating content
+  </Button>
+</Stack>
+
+<FloatingPortal {anchor} {open}>
+  <Tile>Content</Tile>
+</FloatingPortal>
