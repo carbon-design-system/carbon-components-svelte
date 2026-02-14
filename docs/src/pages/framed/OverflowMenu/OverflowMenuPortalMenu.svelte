7fff1f711d6de6c1e1ec5fcc903dfcaccff1bf10
Eric Liu <ericyl.us@gmail.com>
2026-02-14 10:32:56 -0800

    docs(overflow-menu): add "Portal menu" example

---
+++ docs/src/pages/framed/OverflowMenu/OverflowMenuPortalMenu.svelte
@@ -0,0 +1,59 @@
+<script>
+  import {
+    Button,
+    Modal,
+    OverflowMenu,
+    OverflowMenuItem,
+    StructuredList,
+    StructuredListBody,
+    StructuredListCell,
+    StructuredListHead,
+    StructuredListRow,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Modal
+  bind:open
+  size="sm"
+  modalHeading="API keys"
+  primaryButtonText="Done"
+  on:click:button--primary={() => (open = false)}
+>
+  <StructuredList condensed flush>
+    <StructuredListHead>
+      <StructuredListRow head>
+        <StructuredListCell head>Name</StructuredListCell>
+        <StructuredListCell head>Created</StructuredListCell>
+        <StructuredListCell head />
+      </StructuredListRow>
+    </StructuredListHead>
+    <StructuredListBody>
+      <StructuredListRow>
+        <StructuredListCell noWrap>my-api-key</StructuredListCell>
+        <StructuredListCell noWrap>2025-01-15</StructuredListCell>
+        <StructuredListCell>
+          <OverflowMenu portalMenu flipped>
+            <OverflowMenuItem text="Edit" />
+            <OverflowMenuItem text="Duplicate" />
+            <OverflowMenuItem danger text="Delete" />
+          </OverflowMenu>
+        </StructuredListCell>
+      </StructuredListRow>
+      <StructuredListRow>
+        <StructuredListCell noWrap>prod-key</StructuredListCell>
+        <StructuredListCell noWrap>2025-03-22</StructuredListCell>
+        <StructuredListCell>
+          <OverflowMenu portalMenu flipped>
+            <OverflowMenuItem text="Edit" />
+            <OverflowMenuItem text="Duplicate" />
+            <OverflowMenuItem danger text="Delete" />
+          </OverflowMenu>
+        </StructuredListCell>
+      </StructuredListRow>
+    </StructuredListBody>
+  </StructuredList>
+</Modal>
