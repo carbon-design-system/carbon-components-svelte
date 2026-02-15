1669604a4f810b2cd3c58c2a4aa2a65c2324d46a
Eric Liu <ericyl.us@gmail.com>
2026-02-14 16:21:04 -0800

    fix(ui-shell): collapse `SideNavMenu` items when rail is closed
    Fixes #746

---
+++ tests/SideNavMenu/SideNavMenu.rail.test.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    SideNavMenu,
+    SideNavMenuItem,
+  } from "carbon-components-svelte";
+
+  export let rail = true;
+  export let isOpen = false;
+</script>
+
+<SideNav {rail} {isOpen}>
+  <SideNavItems>
+    <SideNavLink text="Link 1" href="/" />
+    <SideNavMenu expanded text="Menu">
+      <SideNavMenuItem href="/" text="Submenu Link 1" />
+      <SideNavMenuItem href="/" text="Submenu Link 2" />
+    </SideNavMenu>
+  </SideNavItems>
+</SideNav>
