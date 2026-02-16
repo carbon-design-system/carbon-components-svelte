06193603b4a6e673d02e2b73a570402917dd6061
Eric Liu <ericyl.us@gmail.com>
2026-02-15 20:51:46 -0800

    fix(portal): transfer focus to active element

---
+++ tests/Portal/Portal.focus.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Portal } from "carbon-components-svelte";
+
+  export let showPortal = true;
+</script>
+
+{#if showPortal}
+  <Portal>
+    <!-- svelte-ignore a11y-autofocus -- required for focus preservation test -->
+    <input data-testid="portal-input" autofocus aria-label="Portal input" />
+  </Portal>
+{/if}
