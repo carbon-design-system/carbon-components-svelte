dea96bd10675746bc12dde34a67843cbc3ae0e49
Eric Liu <ericyl.us@gmail.com>
2026-02-15 16:44:06 -0800

    docs(checkbox): add `CheckboxGroup` examples

---
+++ docs/src/pages/framed/Checkbox/CheckboxGroupReactive.svelte
@@ -0,0 +1,31 @@
+<script>
+  import {
+    Button,
+    Checkbox,
+    CheckboxGroup,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let selected = ["email"];
+</script>
+
+<Stack inline gap={4}>
+  <CheckboxGroup
+    legendText="Notification preferences"
+    name="prefs"
+    bind:selected
+  >
+    <Checkbox value="email" labelText="Email" />
+    <Checkbox value="sms" labelText="SMS" />
+    <Checkbox value="push" labelText="Push notifications" />
+  </CheckboxGroup>
+
+  <Button on:click={() => (selected = ["email", "push"])}>
+    Select Email + Push
+  </Button>
+
+  <div>
+    <strong>selected:</strong>
+    {JSON.stringify(selected)}
+  </div>
+</Stack>
