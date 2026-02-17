62b957806fc465f1ce9cc9c2ca34c625eb697dd8
Eric Liu <ericyl.us@gmail.com>
2026-02-17 08:43:19 -0800

    docs(composed-modal): add "Danger modal" example

---
+++ docs/src/pages/framed/Modal/DangerComposedModal.svelte
@@ -0,0 +1,26 @@
+<script>
+  import {
+    Button,
+    ComposedModal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button kind="danger" on:click={() => (open = true)}>Delete all</Button>
+
+<ComposedModal danger bind:open on:open on:close>
+  <ModalHeader title="Delete all instances" />
+  <ModalBody>
+    <p>This is a permanent action and cannot be undone.</p>
+  </ModalBody>
+  <ModalFooter
+    primaryButtonText="Delete"
+    secondaryButtonText="Cancel"
+    danger
+    on:submit={() => (open = false)}
+  />
+</ComposedModal>
