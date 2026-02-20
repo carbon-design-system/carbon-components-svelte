eb2c120c56a87b8dcbee1081ae14795f62b73665
Eric Liu <ericyl.us@gmail.com>
2026-02-20 08:39:26 -0800

    docs(date-picker): update "Custom date format" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerCustomFormat.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {
+    Button,
+    DatePicker,
+    DatePickerInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let value = "";
+  let submittedValue = "";
+</script>
+
+<form on:submit|preventDefault={() => (submittedValue = value)}>
+  <Stack inline gap={4}>
+    <DatePicker bind:value datePickerType="single" dateFormat="Y-m-d">
+      <DatePickerInput
+        labelText="Date of birth"
+        placeholder="yyyy-mm-dd"
+        helperText={submittedValue ? `Submitted: ${submittedValue}` : ""}
+      />
+    </DatePicker>
+    <Button type="submit" disabled={!value}>Submit</Button>
+  </Stack>
+</form>
