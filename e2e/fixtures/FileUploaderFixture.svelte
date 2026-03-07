e03836084e00774226178451727049320e5703e2
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:56:05 -0800

    test(e2e): add `FileUploader` tests

---
+++ e2e/fixtures/FileUploaderFixture.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { FileUploader } from "carbon-components-svelte";
+
+  let files = [];
+</script>
+
+<div class="fixture-container">
+  <FileUploader
+    data-testid="file-uploader"
+    labelTitle="Upload files"
+    buttonLabel="Add file"
+    status="edit"
+    iconDescription="Remove file"
+    bind:files
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
