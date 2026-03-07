f044206c6d3203eb94cee2beac3f4ded7d164d90
Eric Liu <ericyl.us@gmail.com>
2026-03-07 11:46:04 -0800

    test(e2e): add `SessionStorage` tests

---
+++ e2e/fixtures/SessionStorageFixture.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { SessionStorage, TextInput } from "carbon-components-svelte";
+
+  let value = "initial";
+  let storage;
+</script>
+
+<SessionStorage key="e2e-session-storage-key" bind:value bind:this={storage} />
+
+<div data-testid="display-value">{value}</div>
+
+<TextInput
+  data-testid="value-input"
+  labelText="Value"
+  bind:value
+  hideLabel
+/>
+
+<button type="button" data-testid="clear-item" on:click={() => storage?.clearItem()}>
+  Clear item
+</button>
+<button type="button" data-testid="clear-all" on:click={() => storage?.clearAll()}>
+  Clear all
+</button>
