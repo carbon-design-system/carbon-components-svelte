e79d56d6b1cd3aa032f423a393491360821bbeeb
Eric Liu <ericyl.us@gmail.com>
2026-03-07 11:45:54 -0800

    test(e2e): add `Portal` tests

---
+++ e2e/fixtures/PortalFixture.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Portal } from "carbon-components-svelte";
+</script>
+
+<div data-testid="source">
+  <Portal data-testid="portal-content">
+    <span data-testid="portal-inner">Portal content</span>
+  </Portal>
+</div>
