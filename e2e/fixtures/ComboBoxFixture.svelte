281ba3e2df776d8e42fad24926e68dfb17fbc2d9
Eric Liu <ericyl.us@gmail.com>
2026-03-07 12:23:03 -0800

    test(e2e): add `ComboBox` tests

---
+++ e2e/fixtures/ComboBoxFixture.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { ComboBox } from "carbon-components-svelte";
+
+  const items = [
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ];
+
+  function shouldFilterItem(item, value) {
+    if (!value) return true;
+    return item.text.toLowerCase().includes(value.toLowerCase());
+  }
+</script>
+
+<ComboBox
+  data-testid="combobox-contact"
+  labelText="Contact"
+  placeholder="Select contact method"
+  {items}
+  {shouldFilterItem}
+/>
