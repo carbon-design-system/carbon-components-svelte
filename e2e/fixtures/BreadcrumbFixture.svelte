8923f0300dc239ad260dab647d1200b2976e5b92
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:55:08 -0800

    test(e2e): add `Breadcrumb` tests

---
+++ e2e/fixtures/BreadcrumbFixture.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+
+<div class="fixture-container">
+  <Breadcrumb data-testid="breadcrumb">
+    <BreadcrumbItem href="/">Dashboard</BreadcrumbItem>
+    <BreadcrumbItem href="/reports">Annual reports</BreadcrumbItem>
+    <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+  </Breadcrumb>
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
