71ec32d3e76a4547df07fff281db0b63c4f831fb
Eric Liu <ericyl.us@gmail.com>
2026-03-07 11:49:50 -0800

    test(e2e): add `ExpandableTile` tests

---
+++ e2e/fixtures/ExpandableTileFixture.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { ExpandableTile } from "carbon-components-svelte";
+</script>
+
+<ExpandableTile data-testid="expandable-tile">
+  <span slot="above" data-testid="above-fold">Above the fold</span>
+  <span slot="below" data-testid="below-fold">Below the fold</span>
+</ExpandableTile>
