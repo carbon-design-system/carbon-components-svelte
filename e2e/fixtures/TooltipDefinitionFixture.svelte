c9b7c53cf7006ff72f009ae0443108e8567775d9
Eric Liu <ericyl.us@gmail.com>
2026-03-07 14:04:48 -0800

    test(e2e): add `TooltipDefinition` tests

---
+++ e2e/fixtures/TooltipDefinitionFixture.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { TooltipDefinition } from "carbon-components-svelte";
+
+</script>
+
+<div class="fixture-container">
+  <TooltipDefinition
+    data-testid="tooltip-definition"
+    tooltipText="Definition tooltip text"
+  >
+    Definition trigger
+  </TooltipDefinition>
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
