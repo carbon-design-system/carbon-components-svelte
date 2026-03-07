1beb0d77712ac103a7233ce861456b39311f1852
Eric Liu <ericyl.us@gmail.com>
2026-03-07 14:04:49 -0800

    test(e2e): add `TooltipIcon` tests

---
+++ e2e/fixtures/TooltipIconFixture.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { TooltipIcon } from "carbon-components-svelte";
+  import Information from "carbon-icons-svelte/lib/Information.svelte";
+</script>
+
+<div class="fixture-container">
+  <TooltipIcon
+    data-testid="tooltip-icon"
+    tooltipText="Icon tooltip text"
+    icon={Information}
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
