b1e0fff998398383e8a80fe7bd728937c718a406
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:30:34 -0800

    test(e2e): add `NumberInput` tests

---
+++ e2e/fixtures/NumberInputFixture.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { NumberInput } from "carbon-components-svelte";
+
+  let value = 10;
+</script>
+
+<div class="fixture-container">
+  <NumberInput
+    data-testid="number-input-quantity"
+    labelText="Quantity"
+    bind:value
+    min={0}
+    max={100}
+    step={1}
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
---
+++ e2e/fixtures/number-input.ts
@@ -0,0 +1,4 @@
+import { mount } from "./mount";
+import NumberInputFixture from "./NumberInputFixture.svelte";
+
+mount(NumberInputFixture);
