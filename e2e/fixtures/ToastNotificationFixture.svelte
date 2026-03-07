39d8f6e60c8748090581be3c1b5a0905e9e3a0ce
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:56:52 -0800

    test(e2e): add `ToastNotification` tests

---
+++ e2e/fixtures/ToastNotificationFixture.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { ToastNotification } from "carbon-components-svelte";
+
+  let open = true;
+</script>
+
+<div class="fixture-container">
+  <ToastNotification
+    data-testid="toast-notification"
+    kind="success"
+    title="Toast title"
+    subtitle="Toast subtitle"
+    bind:open
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
