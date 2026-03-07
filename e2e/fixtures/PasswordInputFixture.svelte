eda0315df1812871bd1b003c415dcc5e4867bc93
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:47:06 -0800

    test(e2e): add `PasswordInput` tests

---
+++ e2e/fixtures/PasswordInputFixture.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { PasswordInput } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<div class="fixture-container">
+  <PasswordInput
+    data-testid="password-input"
+    labelText="Password"
+    placeholder="Enter password"
+    bind:value
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
