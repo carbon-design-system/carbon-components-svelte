7def0411a63e361f4d76299eab84843d7bd99297
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:49:14 -0800

    test(e2e): add `Tag` tests

---
+++ e2e/fixtures/TagFixture.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Tag } from "carbon-components-svelte";
+
+  let showTag = true;
+</script>
+
+<div class="fixture-container">
+  {#if showTag}
+    <Tag
+      data-testid="tag-filter"
+      filter={true}
+      title="Clear filter"
+      on:close={() => (showTag = false)}
+    >
+      Carbon
+    </Tag>
+  {/if}
+
+  <Tag data-testid="tag-static">Static tag</Tag>
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+</style>
---
+++ e2e/fixtures/tag.ts
@@ -0,0 +1,4 @@
+import { mount } from "./mount";
+import TagFixture from "./TagFixture.svelte";
+
+mount(TagFixture);
