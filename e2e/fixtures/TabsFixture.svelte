6e4330c21e848096b4658fc8e9cbd012b15beb38
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:48:47 -0800

    test(e2e): add `Tabs` tests

---
+++ e2e/fixtures/TabsFixture.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Tab, TabContent, Tabs } from "carbon-components-svelte";
+
+  let selected = 0;
+</script>
+
+<div class="fixture-container">
+  <Tabs data-testid="tabs" bind:selected>
+    <Tab label="Tab 1" />
+    <Tab label="Tab 2" />
+    <Tab label="Tab 3" />
+
+    <TabContent>
+      <p data-testid="tab-content-1">Content for tab 1</p>
+    </TabContent>
+    <TabContent>
+      <p data-testid="tab-content-2">Content for tab 2</p>
+    </TabContent>
+    <TabContent>
+      <p data-testid="tab-content-3">Content for tab 3</p>
+    </TabContent>
+  </Tabs>
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
---
+++ e2e/fixtures/tabs.ts
@@ -0,0 +1,4 @@
+import { mount } from "./mount";
+import TabsFixture from "./TabsFixture.svelte";
+
+mount(TabsFixture);
