39951b04e2f3dd33a87a8bff5179fd43aaebf26f
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:21:38 -0800

    test(e2e): add `Select` tests

---
+++ e2e/fixtures/SelectFixture.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Select, SelectItem } from "carbon-components-svelte";
+
+  let selected = "";
+</script>
+
+<div class="fixture-container">
+  <Select
+    data-testid="select-country"
+    labelText="Country"
+    bind:selected
+  >
+    <SelectItem value="us" text="United States" />
+    <SelectItem value="uk" text="United Kingdom" />
+    <SelectItem value="ca" text="Canada" />
+    <SelectItem value="de" text="Germany" />
+  </Select>
+
+  {#if selected}
+    <p data-testid="selected-value">Selected: {selected}</p>
+  {/if}
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
