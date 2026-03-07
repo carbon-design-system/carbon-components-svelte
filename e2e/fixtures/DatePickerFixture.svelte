dfdbc349947928ab78c9bbb005a3820aee00c759
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:07:57 -0800

    test(e2e): add `DatePicker` tests

---
+++ e2e/fixtures/DatePickerFixture.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker
+  datePickerType="single"
+  value="03/15/2024"
+  flatpickrProps={{
+    static: true,
+    minDate: "03/01/2024",
+    maxDate: "03/31/2024",
+  }}
+  on:change
+>
+  <DatePickerInput
+    data-testid="date-picker-meeting"
+    labelText="Meeting date"
+    placeholder="mm/dd/yyyy"
+  />
+</DatePicker>
