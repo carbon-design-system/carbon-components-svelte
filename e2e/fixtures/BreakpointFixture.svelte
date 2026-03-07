08bbd5a4dddb2ae98bb4ad001443dfb3b6df3d60
Eric Liu <ericyl.us@gmail.com>
2026-03-07 11:45:09 -0800

    test(e2e): add `Breakpoint` tests

---
+++ e2e/fixtures/BreakpointFixture.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Breakpoint } from "carbon-components-svelte";
+</script>
+
+<Breakpoint let:size let:sizes>
+  <div data-testid="current-size">{size}</div>
+  <div data-testid="is-sm">{sizes.sm}</div>
+  <div data-testid="is-md">{sizes.md}</div>
+  <div data-testid="is-lg">{sizes.lg}</div>
+  <div data-testid="is-xlg">{sizes.xlg}</div>
+  <div data-testid="is-max">{sizes.max}</div>
+</Breakpoint>
