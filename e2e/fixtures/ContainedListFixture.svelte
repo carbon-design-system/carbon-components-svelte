f513cd22538d44ed9dedbe4a701092c292a30813
Eric Liu <ericyl.us@gmail.com>
2026-03-07 14:04:10 -0800

    test(e2e): add `ContainedList` tests

---
+++ e2e/fixtures/ContainedListFixture.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { ContainedList, ContainedListItem } from "carbon-components-svelte";
+
+  let clickedItem = null;
+</script>
+
+<div class="fixture-container" data-testid="contained-list">
+  <ContainedList labelText="List title">
+    <ContainedListItem
+      interactive
+      on:click={() => (clickedItem = "1")}
+    >
+      Item 1
+    </ContainedListItem>
+    <ContainedListItem
+      interactive
+      on:click={() => (clickedItem = "2")}
+    >
+      Item 2
+    </ContainedListItem>
+  </ContainedList>
+  <div data-testid="clicked-item">{clickedItem ?? "none"}</div>
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
