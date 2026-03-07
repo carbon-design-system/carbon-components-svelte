d17f216ecb30ecce9feff02e32666305161bf3ff
Eric Liu <ericyl.us@gmail.com>
2026-03-07 13:49:40 -0800

    test(e2e): add `Pagination` tests

---
+++ e2e/fixtures/PaginationFixture.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Pagination } from "carbon-components-svelte";
+
+  let page = 1;
+  let pageSize = 10;
+</script>
+
+<div class="fixture-container">
+  <Pagination
+    data-testid="pagination"
+    totalItems={50}
+    pageSizes={[10, 20, 50]}
+    bind:page
+    bind:pageSize
+  />
+</div>
+
+<style>
+  .fixture-container {
+    padding: 2rem;
+  }
+</style>
---
+++ e2e/fixtures/pagination.ts
@@ -0,0 +1,4 @@
+import { mount } from "./mount";
+import PaginationFixture from "./PaginationFixture.svelte";
+
+mount(PaginationFixture);
