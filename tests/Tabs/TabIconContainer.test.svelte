b4e53fcd84bf5b24c13e40593a04c95807878122
Eric Liu <ericyl.us@gmail.com>
2026-03-08 15:22:00 -0700

    feat(tabs): container type supports icons
    Closes #2724

---
+++ tests/Tabs/TabIconContainer.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Tab, TabContent, Tabs } from "carbon-components-svelte";
+  import Calendar from "../../src/icons/Calendar.svelte";
+
+  export let icon = Calendar;
+  export let disabled = false;
+  export let label = "Test Tab";
+</script>
+
+<Tabs type="container">
+  <Tab {label} {icon} {disabled} />
+  <Tab label="No Icon" />
+  <svelte:fragment slot="content">
+    <TabContent>Icon content</TabContent>
+    <TabContent>Plain content</TabContent>
+  </svelte:fragment>
+</Tabs>
