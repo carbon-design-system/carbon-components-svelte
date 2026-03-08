8e2ddd095b8ccdfbe7ad24284ffca746d417bd09
Eric Liu <ericyl.us@gmail.com>
2026-03-08 16:58:57 -0700

    test(tabs): add tests for container type with icons and secondary labels

---
+++ tests/Tabs/TabIconSecondaryLabel.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Tab, TabContent, Tabs } from "carbon-components-svelte";
+  import Calendar from "../../src/icons/Calendar.svelte";
+  import Information from "../../src/icons/Information.svelte";
+  import Settings from "../../src/icons/Settings.svelte";
+</script>
+
+<Tabs type="container">
+  <Tab label="Calendar" icon={Calendar} secondaryLabel="(12 events)" />
+  <Tab label="Information" icon={Information} secondaryLabel="(3 new)" />
+  <Tab label="Settings" icon={Settings} secondaryLabel="(2 pending)" disabled />
+  <svelte:fragment slot="content">
+    <TabContent>Calendar content</TabContent>
+    <TabContent>Information content</TabContent>
+    <TabContent>Settings content</TabContent>
+  </svelte:fragment>
+</Tabs>
