7ec2e15f22b2cfdcaa2ce1c88a20ce5557ac6ce7
Eric Liu <ericyl.us@gmail.com>
2026-03-08 21:19:15 -0700

    test(e2e): more `CodeSnippet` tests

--- e2e/code-snippet.test.ts
+++ e2e/code-snippet.test.ts
@@ -43,4 +43,16 @@ test.describe("CodeSnippet", () => {
     await expect(snippet).not.toHaveClass(/bx--snippet--expand/);
     await expect(showMore).toBeVisible();
   });
+
+  test("multi snippet expanded content is visible and has greater height", async ({
+    page,
+  }) => {
+    const snippet = page.getByTestId("snippet-multi");
+    const codeBlock = snippet.locator("code");
+    const collapsedHeight = (await codeBlock.boundingBox())?.height ?? 0;
+    await snippet.getByRole("button", { name: /show more/i }).click();
+    await expect(snippet).toHaveClass(/bx--snippet--expand/);
+    const expandedHeight = (await codeBlock.boundingBox())?.height ?? 0;
+    expect(expandedHeight).toBeGreaterThanOrEqual(collapsedHeight);
+  });
 });
