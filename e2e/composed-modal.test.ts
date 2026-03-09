e37555fe7462e8c9f0daf604d13a4a84d3bd5290
Eric Liu <ericyl.us@gmail.com>
2026-03-08 21:09:59 -0700

    test(e2e): more `ComposedModal` tests

--- e2e/composed-modal.test.ts
+++ e2e/composed-modal.test.ts
@@ -24,4 +24,40 @@ test.describe("ComposedModal", () => {
     await page.getByTestId("close-modal").click();
     await expect(page.locator(".bx--modal")).not.toHaveClass(/is-visible/);
   });
+
+  test("closes when Escape is pressed", async ({ page }) => {
+    await page.getByTestId("open-modal").click();
+    await expect(page.locator(".bx--modal")).toHaveClass(/is-visible/);
+    await page.waitForTimeout(400);
+
+    await page.getByTestId("modal-primary-focus").focus();
+    await page.keyboard.press("Escape");
+    await expect(page.locator(".bx--modal")).not.toHaveClass(/is-visible/);
+  });
+
+  test("primary focus element is focusable when opened", async ({ page }) => {
+    await page.getByTestId("open-modal").click();
+    await expect(page.locator(".bx--modal")).toHaveClass(/is-visible/);
+
+    const primaryFocus = page.getByTestId("modal-primary-focus");
+    await primaryFocus.focus();
+    await expect(primaryFocus).toBeFocused();
+  });
+
+  test("traps focus with Tab", async ({ page }) => {
+    await page.getByTestId("open-modal").click();
+    await expect(page.locator(".bx--modal")).toHaveClass(/is-visible/);
+
+    const primaryFocus = page.getByTestId("modal-primary-focus");
+    await primaryFocus.focus();
+
+    await page.keyboard.press("Tab");
+    await expect(page.getByTestId("close-modal")).toBeFocused();
+
+    await page.keyboard.press("Tab");
+    await expect(page.locator(".bx--modal-close")).toBeFocused();
+
+    await page.keyboard.press("Tab");
+    await expect(primaryFocus).toBeFocused();
+  });
 });
