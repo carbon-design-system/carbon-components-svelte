5e3c78538f753626008a8e577cc349ddb0c51a5b
Eric Liu <ericyl.us@gmail.com>
2026-03-14 12:20:17 -0700

    docs(code-snippet): add "Inside a modal" example

---
+++ docs/src/pages/framed/CodeSnippet/CodeSnippetInModal.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Button, CodeSnippet, Modal } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Modal
+  bind:open
+  modalHeading="Install instructions"
+  primaryButtonText="Done"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (open = false)}
+>
+  <CodeSnippet code="npm i carbon-components-svelte" />
+</Modal>
