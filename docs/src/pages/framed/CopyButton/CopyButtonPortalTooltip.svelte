6ea66dfebc907fb6cce51009cc111b88ea4e2b40
Eric Liu <ericyl.us@gmail.com>
2026-03-14 12:24:02 -0700

    docs(copy-button): add "Portal tooltip" example

---
+++ docs/src/pages/framed/CopyButton/CopyButtonPortalTooltip.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { CopyButton, Stack } from "carbon-components-svelte";
+</script>
+
+<Stack
+  gap={4}
+  style="overflow: hidden; border: 1px dashed var(--cds-border-subtle); padding: 1rem; max-height: 120px;"
+>
+  <CopyButton text="Carbon svelte" feedback="Copied!" portalTooltip={true} />
+</Stack>
