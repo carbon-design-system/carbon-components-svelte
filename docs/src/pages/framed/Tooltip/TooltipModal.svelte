34d186afdadd50ab24154310b71bf15f15fa87d6
Eric Liu <ericyl.us@gmail.com>
2026-03-14 13:18:52 -0700

    docs(tooltip): add portal examples

---
+++ docs/src/pages/framed/Tooltip/TooltipModal.svelte
@@ -0,0 +1,29 @@
+<script>
+  import {
+    Button,
+    Modal,
+    Portal,
+    Stack,
+    Tooltip,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Portal>
+  <Modal
+    bind:open
+    size="sm"
+    modalHeading="Tooltip in modal"
+    primaryButtonText="Done"
+    secondaryButtonText="Cancel"
+    on:click:button--secondary={() => (open = false)}
+  >
+    <Tooltip triggerText="Top" direction="top"><p>Top</p></Tooltip>
+    <Tooltip triggerText="Right" direction="right"><p>Right</p></Tooltip>
+    <Tooltip triggerText="Bottom" direction="bottom"><p>Bottom</p></Tooltip>
+    <Tooltip triggerText="Left" direction="left"><p>Left</p></Tooltip>
+  </Modal>
+</Portal>
