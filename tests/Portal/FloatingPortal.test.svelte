272e27816c614fc86f29dd838efa2530f26f27ae
Eric Liu <ericyl.us@gmail.com>
2026-03-14 13:16:50 -0700

    feat(floating-portal): support left/right directions, gap props

--- tests/Portal/FloatingPortal.test.svelte
+++ tests/Portal/FloatingPortal.test.svelte
@@ -4,7 +4,7 @@
   import { FloatingPortal } from "carbon-components-svelte";
 
   export let open = false;
-  export let direction: "top" | "bottom" = "bottom";
+  export let direction: "top" | "bottom" | "left" | "right" = "bottom";
   export let zIndex = 9200;
   export let intrinsicWidth = false;
   export let anchor: HTMLElement | null = null;
@@ -12,6 +12,12 @@
   export let content = "Floating content";
   export let scrollableContainer = false;
   export let containerRef: HTMLElement | null = null;
+  export let gapTop = 0;
+  export let gapBottom = 0;
+  export let horizontalGapLeft = 0;
+  export let horizontalGapRight = 0;
+  export let verticalAlignOffsetLeft = 0;
+  export let verticalAlignOffsetRight = 0;
 </script>
 
 {#if scrollableContainer}
@@ -26,6 +32,19 @@
   <div data-testid="anchor" bind:this={anchor}>Anchor element</div>
 {/if}
 
-<FloatingPortal {anchor} {direction} {open} {zIndex} {intrinsicWidth} bind:ref>
+<FloatingPortal
+  {anchor}
+  {direction}
+  {open}
+  {zIndex}
+  {intrinsicWidth}
+  {gapTop}
+  {gapBottom}
+  {horizontalGapLeft}
+  {horizontalGapRight}
+  {verticalAlignOffsetLeft}
+  {verticalAlignOffsetRight}
+  bind:ref
+>
   {content}
 </FloatingPortal>
