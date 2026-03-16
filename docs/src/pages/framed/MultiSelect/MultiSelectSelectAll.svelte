ab08f3f216521a8c049f89e6131207f780542e6d
Eric Liu <ericyl.us@gmail.com>
2026-03-16 12:57:33 -0700

    docs(multi-select): add "Select all" example

---
+++ docs/src/pages/framed/MultiSelect/MultiSelectSelectAll.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { MultiSelect } from "carbon-components-svelte";
+</script>
+
+<MultiSelect
+  filterable
+  labelText="Roles"
+  placeholder="Filter roles..."
+  items={[
+    { id: "select-all", text: "All roles", isSelectAll: true },
+    { id: "editor", text: "Editor" },
+    { id: "owner", text: "Owner" },
+    { id: "uploader", text: "Uploader" },
+    { id: "reader", text: "Reader", disabled: true },
+  ]}
+/>
