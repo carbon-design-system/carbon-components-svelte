86a5c2a0043537b7d1b9d278251790d859bd4a26
Eric Liu <ericyl.us@gmail.com>
2026-03-21 16:06:12 -0700

    docs(file-uploader): revise "Maximum file size" example

--- docs/src/pages/framed/FileUploader/FileUploaderMaxFileSize.svelte
+++ docs/src/pages/framed/FileUploader/FileUploaderMaxFileSize.svelte
@@ -1,28 +1,37 @@
 <script>
-  import { FileUploader, InlineNotification } from "carbon-components-svelte";
+  import {
+    FileUploader,
+    FileUploaderItem,
+    Stack,
+  } from "carbon-components-svelte";
 
   let rejectedFiles = [];
 </script>
 
-<FileUploader
-  multiple
-  maxFileSize={1024 * 1024}
-  labelTitle="Upload files"
-  buttonLabel="Add files"
-  labelDescription="Maximum file size: 1 MB"
-  status="edit"
-  on:rejected={(e) => {
-    rejectedFiles = e.detail;
-  }}
-/>
-
-{#each rejectedFiles as { file, reason }}
-  <InlineNotification
-    kind="error"
-    title="Rejected:"
-    subtitle="{file.name} ({reason})"
-    on:close={() => {
-      rejectedFiles = rejectedFiles.filter((r) => r.file !== file);
+<Stack gap={2}>
+  <FileUploader
+    multiple
+    maxFileSize={1024 * 1024}
+    labelTitle="Upload files"
+    buttonLabel="Add files"
+    labelDescription="Maximum file size: 1 MB"
+    status="edit"
+    on:rejected={(e) => {
+      rejectedFiles = e.detail;
     }}
   />
-{/each}
+
+  {#each rejectedFiles as { file }, i (`${file.name}-${file.lastModified}-${i}`)}
+    <FileUploaderItem
+      invalid
+      id={`rejected-size-${i}`}
+      name={file.name}
+      errorSubject="File exceeds 1 MB limit"
+      errorBody="Please select a smaller file."
+      status="edit"
+      on:delete={() => {
+        rejectedFiles = rejectedFiles.filter((r) => r.file !== file);
+      }}
+    />
+  {/each}
+</Stack>
