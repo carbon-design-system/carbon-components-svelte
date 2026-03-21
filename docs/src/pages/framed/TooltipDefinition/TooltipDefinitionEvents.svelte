c45a85235913808bbe64431d1f2521a287c1e6ec
Eric Liu <ericyl.us@gmail.com>
2026-03-21 10:36:08 -0700

    docs(tooltip-definition): add more examples
    Closes #2762

---
+++ docs/src/pages/framed/TooltipDefinition/TooltipDefinitionEvents.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { TooltipDefinition } from "carbon-components-svelte";
+
+  let events = [];
+</script>
+
+<TooltipDefinition
+  align="start"
+  tooltipText="IBM Corporate Headquarters is based in Armonk, New York."
+  on:open={() => (events = [...events, "open"])}
+  on:close={() => (events = [...events, "close"])}
+>
+  Dispatched events: {events.join(", ")}
+</TooltipDefinition>
