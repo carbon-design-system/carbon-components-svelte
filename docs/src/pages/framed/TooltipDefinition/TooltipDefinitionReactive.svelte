6865ddc889c21531711453051fd8d67f6a39f867
Eric Liu <ericyl.us@gmail.com>
2026-03-21 10:36:08 -0700

    docs(tooltip-definition): add more examples
    Closes #2762

---
+++ docs/src/pages/framed/TooltipDefinition/TooltipDefinitionReactive.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Button, Stack, TooltipDefinition } from "carbon-components-svelte";
+
+  let open = true;
+</script>
+
+<Stack gap={12}>
+  <div>
+    <TooltipDefinition
+      align="start"
+      bind:open
+      tooltipText="IBM Corporate Headquarters is based in Armonk, New York."
+    >
+      Armonk
+    </TooltipDefinition>
+  </div>
+  <div>
+    <Button kind="tertiary" size="small" on:click={() => (open = !open)}>
+      {open ? "Close tooltip" : "Open tooltip"}
+    </Button>
+  </div>
+</Stack>
