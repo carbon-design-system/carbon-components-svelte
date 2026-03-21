82d3a3aaae631c6d31e27e4a2c96fbcf6e7c30d7
Eric Liu <ericyl.us@gmail.com>
2026-03-21 14:49:01 -0700

    docs(context-menu): add "Selectable (nested)" example

---
+++ docs/src/pages/framed/ContextMenu/ContextMenuSelectableNested.svelte
@@ -0,0 +1,26 @@
+<script>
+  import {
+    ContextMenu,
+    ContextMenuDivider,
+    ContextMenuGroup,
+    ContextMenuOption,
+  } from "carbon-components-svelte";
+
+  let selectedIds = ["guides"];
+</script>
+
+<ContextMenu>
+  <ContextMenuOption indented labelText="Open" />
+  <ContextMenuDivider />
+  <ContextMenuOption indented labelText="Layers">
+    <ContextMenuGroup labelText="Visible layers" bind:selectedIds>
+      <ContextMenuOption id="guides" labelText="Guides" />
+      <ContextMenuOption id="grid" labelText="Grid" />
+      <ContextMenuOption id="annotations" labelText="Annotations" />
+    </ContextMenuGroup>
+  </ContextMenuOption>
+</ContextMenu>
+
+<div data-centered>
+  <p>Right click anywhere on this page</p>
+</div>
