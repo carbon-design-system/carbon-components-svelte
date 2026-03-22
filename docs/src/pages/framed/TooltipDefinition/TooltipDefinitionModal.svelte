2d887028c727a2fd92ee844a788c3a0fa933b9f8
Eric Liu <ericyl.us@gmail.com>
2026-03-22 12:19:48 -0700

    docs(tooltip-definition): add portal examples

---
+++ docs/src/pages/framed/TooltipDefinition/TooltipDefinitionModal.svelte
@@ -0,0 +1,36 @@
+<script>
+  import {
+    Button,
+    Modal,
+    Portal,
+    TooltipDefinition,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Portal>
+  <Modal
+    bind:open
+    size="sm"
+    modalHeading="TooltipDefinition in modal"
+    primaryButtonText="Done"
+    secondaryButtonText="Cancel"
+    on:click:button--secondary={() => (open = false)}
+  >
+    <TooltipDefinition
+      direction="top"
+      tooltipText="IBM Corporate Headquarters is based in Armonk, New York."
+    >
+      Armonk (top)
+    </TooltipDefinition>
+    <TooltipDefinition
+      direction="bottom"
+      tooltipText="IBM Corporate Headquarters is based in Armonk, New York."
+    >
+      Armonk (bottom)
+    </TooltipDefinition>
+  </Modal>
+</Portal>
