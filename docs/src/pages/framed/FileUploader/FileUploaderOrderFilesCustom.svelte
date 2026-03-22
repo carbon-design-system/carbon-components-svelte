f4da20796527d2aea98ae1eff6db0f9ee3f7e428
Eric Liu <ericyl.us@gmail.com>
2026-03-22 10:18:04 -0700

    docs(file-uploader): add file ordering examples

---
+++ docs/src/pages/framed/FileUploader/FileUploaderOrderFilesCustom.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { FileUploader } from "carbon-components-svelte";
+</script>
+
+<FileUploader
+  multiple
+  orderFiles={(existing, added) => {
+    return [...existing, ...added].sort(
+      (a, b) => b.lastModified - a.lastModified,
+    );
+  }}
+  labelTitle="Upload files"
+  buttonLabel="Add files"
+  labelDescription="Files are sorted by last modified (newest first)."
+  status="edit"
+/>
