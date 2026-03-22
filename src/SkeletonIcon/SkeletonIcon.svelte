d91ed6aeac4c715d82c0924c7af8e5aa328ba6b2
Eric Liu <ericyl.us@gmail.com>
2026-03-22 13:56:08 -0700

    feat(skeleton-icon): add `SkeletonIcon` component

---
+++ src/SkeletonIcon/SkeletonIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+  /** @extends {"../SkeletonPlaceholder/SkeletonPlaceholder.svelte"} SkeletonPlaceholderProps */
+
+  import SkeletonPlaceholder from "../SkeletonPlaceholder/SkeletonPlaceholder.svelte";
+
+  /**
+   * Specify the icon size.
+   * Carbon icons use a 16/20/24/32 scale, but any number (pixels) can be used.
+   * @type {(16 | 20 | 24 | 32 | (number & {}))}
+   */
+  export let size = 16;
+</script>
+
+<SkeletonPlaceholder
+  {size}
+  {...$$restProps}
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+/>
