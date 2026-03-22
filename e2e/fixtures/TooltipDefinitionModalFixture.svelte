87d205ef8e40cf53d4d891d86bcb15c297d42ac4
Eric Liu <ericyl.us@gmail.com>
2026-03-22 13:27:26 -0700

    test(e2e): add tooltip definition/icon modals

---
+++ e2e/fixtures/TooltipDefinitionModalFixture.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Modal, TooltipDefinition } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<button type="button" data-testid="open-modal" on:click={() => (open = true)}>
+  Open modal
+</button>
+
+<Modal
+  data-testid="modal"
+  bind:open
+  modalHeading="Modal for tooltip test"
+  primaryButtonText="Save"
+  secondaryButtonText="Cancel"
+>
+  <div
+    data-testid="overflow-box"
+    style="overflow: hidden; max-height: 3rem; padding: 0.5rem;"
+  >
+    <TooltipDefinition
+      data-testid="tooltip-definition-modal"
+      tooltipText="Portaled definition text"
+    >
+      Definition in modal
+    </TooltipDefinition>
+  </div>
+</Modal>
