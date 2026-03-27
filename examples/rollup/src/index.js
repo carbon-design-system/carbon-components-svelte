70b3994d0602305c2f161413be7523dc0d826cad
Eric Liu <ericyl.us@gmail.com>
2026-03-27 09:16:17 -0700

    chore(examples): bump deps

--- examples/rollup/src/index.js
+++ examples/rollup/src/index.js
@@ -1,5 +1,6 @@
+import { mount } from "svelte";
 import App from "./App.svelte";
 
-const app = new App({ target: document.body });
+const app = mount(App, { target: document.body });
 
 export default app;
