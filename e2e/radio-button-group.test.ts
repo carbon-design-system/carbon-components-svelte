4794159fbf7459dd995a501350034aabb20e47c6
Eric Liu <ericyl.us@gmail.com>
2026-03-31 10:24:12 -0700

    test(e2e): load styles

--- e2e/radio-button-group.test.ts
+++ e2e/radio-button-group.test.ts
@@ -10,20 +10,28 @@ test.describe("RadioButtonGroup", () => {
   });
 
   test("selects radio button", async ({ page }) => {
-    await page.getByRole("radio", { name: "Option Two" }).click();
+    await page
+      .getByRole("radio", { name: "Option Two" })
+      .click({ force: true });
     await expect(page.getByTestId("selected-value")).toContainText("two");
   });
 
   test("can be located by data-testid", async ({ page }) => {
     const group = page.getByTestId("radio-group-choice");
     await expect(group).toBeVisible();
-    await group.getByRole("radio", { name: "Option Three" }).click();
+    await group
+      .getByRole("radio", { name: "Option Three" })
+      .click({ force: true });
     await expect(page.getByTestId("selected-value")).toContainText("three");
   });
 
   test("only one option can be selected at a time", async ({ page }) => {
-    await page.getByRole("radio", { name: "Option One" }).click();
-    await page.getByRole("radio", { name: "Option Two" }).click();
+    await page
+      .getByRole("radio", { name: "Option One" })
+      .evaluate((el) => el.click());
+    await page
+      .getByRole("radio", { name: "Option Two" })
+      .evaluate((el) => el.click());
 
     await expect(
       page.getByRole("radio", { name: "Option One" }),
