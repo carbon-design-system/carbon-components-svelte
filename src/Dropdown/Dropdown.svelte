f58fc9c5dd27b159c532616c3ba03e8e5667a82c
Eric Liu <ericyl.us@gmail.com>
2026-04-04 16:41:37 -0700

    fix(dropdown): prevent infinite loop when all items are disabled

--- src/Dropdown/Dropdown.svelte
+++ src/Dropdown/Dropdown.svelte
@@ -355,8 +355,9 @@
     }
 
     let disabled = items[index].disabled;
+    let attempts = 0;
 
-    while (disabled) {
+    while (disabled && attempts < items.length) {
       index = index + dir;
 
       if (index < 0) {
@@ -366,9 +367,10 @@
       }
 
       disabled = items[index].disabled;
+      attempts++;
     }
 
-    highlightedIndex = index;
+    if (!disabled) highlightedIndex = index;
   }
 
   function typeaheadSearch(char) {
