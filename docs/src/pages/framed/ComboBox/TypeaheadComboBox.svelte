83850e36cef7d12e2d554c7b7612628a9e16a965
Eric Liu <ericyl.us@gmail.com>
2026-04-06 19:12:20 -0700

    docs(combo-box): add auto-highlight examples

--- docs/src/pages/framed/ComboBox/TypeaheadComboBox.svelte
+++ docs/src/pages/framed/ComboBox/TypeaheadComboBox.svelte
@@ -2,16 +2,6 @@
   import { Button, ComboBox } from "carbon-components-svelte";
 
   let selectedId = undefined;
-
-  const items = [
-    "Apple",
-    "Apricot",
-    "Avocado",
-    "Banana",
-    "Blackberry",
-    "Blueberry",
-    "Cantaloupe",
-  ].map((item) => ({ id: item, text: item }));
 </script>
 
 <ComboBox
@@ -19,7 +9,18 @@
   placeholder="Select an item"
   bind:selectedId
   typeahead
-  {items}
+  items={[
+  {id: "0", text: "Apple"},
+  {id: "1", text: "Apricot"},
+  {id: "2", text: "Banana"},
+  {id: "3", text: "Blueberry"},
+  {id: "4", text: "Blackberry"},
+  {id: "5", text: "Cherry"},
+  {id: "6", text: "Cranberry"},
+  {id: "7", text: "Grape"},
+  {id: "8", text: "Mango"},
+  {id: "9", text: "Pineapple"},
+  ]}
 />
 <br>
 <Button on:click={() => (selectedId = undefined)}>
