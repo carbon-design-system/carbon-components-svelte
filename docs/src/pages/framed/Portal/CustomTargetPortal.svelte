4eb7d3a4d4b3928a20e1bc7ee177c4fb2d141dcb
Eric Liu <ericyl.us@gmail.com>
2026-04-24 20:09:27 -0700

    docs(portal): add "Custom target" example

---
+++ docs/src/pages/framed/Portal/CustomTargetPortal.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Portal, Stack, Tile } from "carbon-components-svelte";
+
+  let target = null;
+</script>
+
+<Stack gap={4}>
+  <Tile>
+    <div>Portal is declared here.</div>
+    <Portal {target}><strong>Portalled content</strong></Portal>
+  </Tile>
+  <Tile>
+    <div bind:this={target}>
+      But mounted into this container via <code>target</code>.
+    </div>
+  </Tile>
+</Stack>
