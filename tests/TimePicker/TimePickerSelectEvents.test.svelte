98bf73c7fd79d66bb0483e730044dff7dabc8814
Eric Liu <ericyl.us@gmail.com>
2026-05-08 16:10:59 -0700

    test(time-picker): add test for forwarded events

---
+++ tests/TimePicker/TimePickerSelectEvents.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { SelectItem, TimePickerSelect } from "carbon-components-svelte";
+
+  export let selectChange: () => void = () => {};
+  export let selectInput: () => void = () => {};
+  export let selectFocus: () => void = () => {};
+  export let selectBlur: () => void = () => {};
+</script>
+
+<TimePickerSelect
+  value="am"
+  labelText="Period"
+  on:change={selectChange}
+  on:input={selectInput}
+  on:focus={selectFocus}
+  on:blur={selectBlur}
+>
+  <SelectItem value="am" text="AM" />
+  <SelectItem value="pm" text="PM" />
+</TimePickerSelect>
