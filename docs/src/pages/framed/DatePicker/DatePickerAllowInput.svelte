d17792b8c4695ba1652fcfd3c06dd486a5697aa4
Eric Liu <ericyl.us@gmail.com>
2026-05-08 16:09:54 -0700

    docs(date-picker): add more examples

---
+++ docs/src/pages/framed/DatePicker/DatePickerAllowInput.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker
+  datePickerType="single"
+  flatpickrProps={{ allowInput: false }}
+  on:change
+>
+  <DatePickerInput labelText="Ship date" placeholder="mm/dd/yyyy" />
+</DatePicker>
