937a04b7dbafad5caf87b7ae0a974d5a3098893b
Eric Liu <ericyl.us@gmail.com>
2026-05-09 11:45:49 -0700

    docs(multi-select): add "Bind to sorted items" example

---
+++ docs/src/pages/framed/MultiSelect/MultiSelectSortedItems.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { MultiSelect } from "carbon-components-svelte";
+
+  let sortedItems = [];
+</script>
+
+<MultiSelect
+  selectionFeedback="top"
+  labelText="Contact"
+  label="Select contact methods..."
+  items={[
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ]}
+  bind:sortedItems
+/>
+
+<pre>{JSON.stringify(sortedItems, null, 2)}</pre>
