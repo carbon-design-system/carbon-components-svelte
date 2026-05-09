a378cc318056095293b1614065301a948f72201e
Eric Liu <ericyl.us@gmail.com>
2026-05-09 10:25:45 -0700

    fix(combo-box): set `aria-haspopup="listbox"` on input

--- src/ComboBox/ComboBox.svelte
+++ src/ComboBox/ComboBox.svelte
@@ -589,6 +589,7 @@
         tabindex="0"
         autocomplete="off"
         aria-autocomplete="list"
+        aria-haspopup="listbox"
         aria-expanded={open}
         aria-activedescendant={highlightedId}
         aria-labelledby={comboId}
