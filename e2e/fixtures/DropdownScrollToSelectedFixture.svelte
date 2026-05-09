6bc4d644cfbc248f69467b966070f242ae720022
Eric Liu <ericyl.us@gmail.com>
2026-05-09 12:19:23 -0700

    fix(dropdown): scroll selected option into view when menu overflows

---
+++ e2e/fixtures/DropdownScrollToSelectedFixture.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Dropdown } from "carbon-components-svelte";
+
+  const items = Array.from({ length: 50 }, (_, i) => ({
+    id: i,
+    text: `Item ${i + 1}`,
+  }));
+
+  let selectedId = 42;
+</script>
+
+<Dropdown labelText="Items" {items} bind:selectedId />
---
+++ e2e/fixtures/dropdown-scroll-to-selected.ts
@@ -0,0 +1,4 @@
+import DropdownScrollToSelectedFixture from "./DropdownScrollToSelectedFixture.svelte";
+import { mount } from "./mount";
+
+mount(DropdownScrollToSelectedFixture);
