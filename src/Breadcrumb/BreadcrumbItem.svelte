ba126be93f2170766cfc908b8a20c7e8429a2c67
Eric Liu <ericyl.us@gmail.com>
2026-05-09 19:34:54 -0700

    fix(breadcrumb-item): improve description for slot usage

--- src/Breadcrumb/BreadcrumbItem.svelte
+++ src/Breadcrumb/BreadcrumbItem.svelte
@@ -1,5 +1,13 @@
 <script>
   /**
+   * Spread `props` onto a custom element to inherit the link class
+   * and `aria-current` attribute when `isCurrentPage` is set.
+   * @example
+   * ```svelte
+   * <BreadcrumbItem let:props>
+   *   <a {...props} href="/">Home</a>
+   * </BreadcrumbItem>
+   * ```
    * @slot {{props?: { "aria-current"?: string; class: "bx--link"; }}}
    */
 
