eaaea14b1953579aeba9789106b06288474bf74b
Eric Liu <ericyl.us@gmail.com>
2026-05-12 20:45:15 -0700

    feat(breadcrumb): support small size

---
+++ tests/Breadcrumb/Breadcrumb.size.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+
+<Breadcrumb size="sm">
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/settings" isCurrentPage>Settings</BreadcrumbItem>
+</Breadcrumb>
