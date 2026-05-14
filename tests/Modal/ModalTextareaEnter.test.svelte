a277c1a5b5fa11ca918183a2d72032d1c980eae0
Eric Liu <ericyl.us@gmail.com>
2026-05-14 14:30:24 -0700

    fix(modal): skip Enter submit when target is textarea/select/contenteditable (#2994)

---
+++ tests/Modal/ModalTextareaEnter.test.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Modal } from "carbon-components-svelte";
+
+  export let open = true;
+  export let shouldSubmitOnEnter = true;
+  export let onsubmit: ((event: CustomEvent) => void) | undefined = undefined;
+  export let onclickbuttonprimary: ((event: CustomEvent) => void) | undefined =
+    undefined;
+</script>
+
+<Modal
+  bind:open
+  modalHeading="Textarea Modal"
+  primaryButtonText="Save"
+  secondaryButtonText="Cancel"
+  {shouldSubmitOnEnter}
+  on:submit={(e) => onsubmit?.(e)}
+  on:click:button--primary={(e) => onclickbuttonprimary?.(e)}
+>
+  <textarea data-testid="modal-textarea"></textarea>
+</Modal>
