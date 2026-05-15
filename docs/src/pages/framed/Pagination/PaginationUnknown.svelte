ede06f47dc368f988aefe2ff475cd7b113d45670
Eric Liu <ericyl.us@gmail.com>
2026-05-14 21:56:44 -0700

    docs(pagination): update "Unknown pages" example

---
+++ docs/src/pages/framed/Pagination/PaginationUnknown.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Pagination } from "carbon-components-svelte";
+
+  // Simulate a cursor-based API where the total is unknown.
+  const LAST_PAGE = 5;
+
+  let page = 1;
+  let pageSize = 10;
+
+  $: forwardButtonDisabled = page >= LAST_PAGE;
+</script>
+
+<Pagination pagesUnknown bind:page bind:pageSize {forwardButtonDisabled} />
