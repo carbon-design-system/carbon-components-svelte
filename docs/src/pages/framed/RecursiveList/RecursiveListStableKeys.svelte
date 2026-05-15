5a853e33b3e39084272d83cc9af8aa78d7515ef5
Eric Liu <ericyl.us@gmail.com>
2026-05-14 17:17:08 -0700

    docs(recursive-list): add "Stable keys" example

---
+++ docs/src/pages/framed/RecursiveList/RecursiveListStableKeys.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { RecursiveList } from "carbon-components-svelte";
+
+  const nodes = [
+    { id: "intro", text: "Introduction" },
+    { id: "guide", text: "Guide", nodes: [{ id: "guide-1", text: "Setup" }] },
+    { id: "api", text: "API reference" },
+  ];
+</script>
+
+<RecursiveList {nodes} />
