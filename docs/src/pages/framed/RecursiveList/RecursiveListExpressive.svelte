a57cb33cff20178350250d68858002399e756b3c
Eric Liu <ericyl.us@gmail.com>
2026-05-14 21:30:19 -0700

    docs(recursive-list): add "Expressive styles" example

---
+++ docs/src/pages/framed/RecursiveList/RecursiveListExpressive.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { RecursiveList } from "carbon-components-svelte";
+
+  const nodes = [
+    {
+      text: "Item 1",
+      nodes: [
+        {
+          text: "Item 1a",
+          nodes: [{ text: "Item 1a-i" }],
+        },
+      ],
+    },
+    {
+      text: "Item 2",
+      nodes: [{ text: "Item 2a" }],
+    },
+    { text: "Item 3" },
+  ];
+</script>
+
+<RecursiveList expressive {nodes} />
