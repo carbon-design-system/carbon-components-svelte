da2e81c4a28a07dcb8e580956ede86f69e630367
Eric Liu <ericyl.us@gmail.com>
2026-05-15 21:37:31 -0700

    fix(modal): implement body scroll lock

---
+++ tests/Modal/ModalSideNavBodyLock.test.svelte
@@ -0,0 +1,21 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import {
+    Modal,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+  } from "carbon-components-svelte";
+
+  export let modalOpen = false;
+  export let sideNavOpen = false;
+</script>
+
+<Modal bind:open={modalOpen} modalHeading="Heading" passiveModal />
+
+<SideNav bind:isOpen={sideNavOpen} ariaLabel="Nav">
+  <SideNavItems>
+    <SideNavLink href="#" text="Link" />
+  </SideNavItems>
+</SideNav>
