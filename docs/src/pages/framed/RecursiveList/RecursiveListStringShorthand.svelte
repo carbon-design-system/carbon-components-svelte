8e77a4e7fff2b1d61f4355d65274956f7b125393
Eric Liu <ericyl.us@gmail.com>
2026-05-16 16:32:55 -0700

    docs(recursive-list): add "String shorthand" example

---
+++ docs/src/pages/framed/RecursiveList/RecursiveListStringShorthand.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { RecursiveList } from "carbon-components-svelte";
+
+  const nodes = [
+    "Item 1",
+    {
+      text: "Item 2",
+      nodes: ["Item 2a", "Item 2b"],
+    },
+    "Item 3",
+  ];
+</script>
+
+<RecursiveList {nodes} />
