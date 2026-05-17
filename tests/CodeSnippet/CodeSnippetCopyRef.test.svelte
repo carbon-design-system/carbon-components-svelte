c311ea391a69b439c0e7428dc5b7966874726000
Eric Liu <ericyl.us@gmail.com>
2026-05-17 15:49:00 -0700

    feat(code-button): add `ref` prop for button reference

---
+++ tests/CodeSnippet/CodeSnippetCopyRef.test.svelte
@@ -0,0 +1,11 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import { CodeSnippet } from "carbon-components-svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let type: ComponentProps<CodeSnippet>["type"] = "inline";
+  export let copyRef: ComponentProps<CodeSnippet>["copyRef"] = null;
+</script>
+
+<CodeSnippet {type} code="npm install" bind:copyRef />
