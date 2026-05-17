43c8863193ad130d550956e39398310ad33a2a53
Eric Liu <ericyl.us@gmail.com>
2026-05-17 10:09:41 -0700

    feat(structured-list-skeleton): add `columns` prop

--- src/StructuredList/StructuredListSkeleton.svelte
+++ src/StructuredList/StructuredListSkeleton.svelte
@@ -1,6 +1,11 @@
 <script>
   /** Specify the number of rows */
   export let rows = 5;
+
+  /** Specify the number of columns */
+  export let columns = 3;
+
+  $: cols = Array.from({ length: columns }, (_, i) => i);
 </script>
 
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
@@ -19,17 +24,17 @@
       class:bx--structured-list-row={true}
       class:bx--structured-list-row--header-row={true}
     >
-      <div class:bx--structured-list-th={true}><span></span></div>
-      <div class:bx--structured-list-th={true}><span></span></div>
-      <div class:bx--structured-list-th={true}><span></span></div>
+      {#each cols as col (col)}
+        <div class:bx--structured-list-th={true}><span></span></div>
+      {/each}
     </div>
   </div>
   <div class:bx--structured-list-tbody={true}>
     {#each Array.from({ length: rows }, (_, i) => i) as row, i (row)}
       <div class:bx--structured-list-row={true}>
-        <div class:bx--structured-list-td={true}></div>
-        <div class:bx--structured-list-td={true}></div>
-        <div class:bx--structured-list-td={true}></div>
+        {#each cols as col (col)}
+          <div class:bx--structured-list-td={true}></div>
+        {/each}
       </div>
     {/each}
   </div>
---
+++ tests/StructuredList/StructuredListSkeleton.test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import { StructuredListSkeleton } from "carbon-components-svelte";
+</script>
+
+<StructuredListSkeleton {...$$props} />
