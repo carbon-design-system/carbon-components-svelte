faf3192ef517786da13c5b43ec1f48fd3337b831
Eric Liu <ericyl.us@gmail.com>
2026-05-17 09:55:53 -0700

    docs(toolbar): add "Programmatically clear the search" example

---
+++ docs/src/pages/framed/Toolbar/ToolbarSearchClear.svelte
@@ -0,0 +1,37 @@
+<script>
+  import {
+    Button,
+    Stack,
+    Toolbar,
+    ToolbarContent,
+    ToolbarSearch,
+  } from "carbon-components-svelte";
+
+  let toolbarSearch;
+  let value = "";
+</script>
+
+<Stack gap={6}>
+  <Toolbar>
+    <ToolbarContent>
+      <ToolbarSearch
+        bind:this={toolbarSearch}
+        bind:value
+        persistent
+        placeholder="Search..."
+      />
+      <Button
+        kind="ghost"
+        size="small"
+        disabled={value.length === 0}
+        on:click={() => toolbarSearch.clear()}
+      >
+        Clear search
+      </Button>
+    </ToolbarContent>
+  </Toolbar>
+  <div>
+    <strong>Search value:</strong>
+    {value}
+  </div>
+</Stack>
