2a6f097e1223516b8dc646d2478cd0266569a0ca
Eric Liu <ericyl.us@gmail.com>
2026-05-19 13:29:55 -0700

    docs(pagination): format large numbers in "Page window" example (#3021)

---
+++ docs/src/pages/framed/Pagination/PageWindow.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Pagination } from "carbon-components-svelte";
+
+  const formatNumber = (value) => {
+    return Intl.NumberFormat("en", { notation: "compact" })
+      .format(value)
+      .toLowerCase();
+  };
+</script>
+
+<Pagination
+  totalItems={100_000}
+  pageSizes={[10, 15, 20]}
+  itemRangeText={(min, max, total) =>
+    `${formatNumber(min)}–${formatNumber(max)} of ${formatNumber(total)} items`}
+  pageRangeText={(_current, total) =>
+    `of ${formatNumber(total)} pages`}
+/>
