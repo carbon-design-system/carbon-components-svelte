44780e3afa28dc33c7535638a17f59276701aa16
Eric Liu <ericyl.us@gmail.com>
2026-05-23 12:38:15 -0700

    fix(progress-indicator): remove unmounted steps from context (#3049)

---
+++ tests/ProgressIndicator/ProgressIndicatorConditional.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { ProgressIndicator, ProgressStep } from "carbon-components-svelte";
+
+  export let showOptional = true;
+  export let currentIndex = 0;
+</script>
+
+<ProgressIndicator
+  {currentIndex}
+  on:change={(e) => console.log("change", e.detail)}
+>
+  <ProgressStep label="Step 1" complete />
+  {#if showOptional}
+    <ProgressStep label="Optional" complete />
+  {/if}
+  <ProgressStep label="Step 3" complete />
+</ProgressIndicator>
