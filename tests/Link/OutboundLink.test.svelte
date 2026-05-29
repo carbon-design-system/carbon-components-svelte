3a0b20ae94015f00707d1aec86a949e86f5c0449
Brian West <157514993+b-r-i-a-n-w-e-s-t@users.noreply.github.com>
2026-05-29 18:15:31 -0500

    feat(outbound-link): add `assistiveText` prop for screen readers (#3105)

---
+++ tests/Link/OutboundLink.test.svelte
@@ -0,0 +1,29 @@
+<svelte:options accessors />
+
+<script lang="ts">
+  import { OutboundLink } from "carbon-components-svelte";
+</script>
+
+<!-- Default outbound link -->
+<div data-testid="default">
+  <OutboundLink href="https://www.carbondesignsystem.com/">
+    Carbon Design System
+  </OutboundLink>
+</div>
+
+<!-- Custom assistive text -->
+<div data-testid="custom">
+  <OutboundLink
+    href="https://www.carbondesignsystem.com/"
+    assistiveText="(opens in a new window)"
+  >
+    Carbon Design System
+  </OutboundLink>
+</div>
+
+<!-- Announcement opted out -->
+<div data-testid="empty">
+  <OutboundLink href="https://www.carbondesignsystem.com/" assistiveText="">
+    Carbon Design System
+  </OutboundLink>
+</div>
