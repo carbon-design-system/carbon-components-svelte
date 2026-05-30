2597629e39a19646a9c36729e85c6494904e23b3
Eric Liu <ericyl.us@gmail.com>
2026-05-29 20:56:13 -0700

    docs(session-storage): update "Reactive key" example

--- docs/src/pages/framed/SessionStorage/SessionStorageReactiveKey.svelte
+++ docs/src/pages/framed/SessionStorage/SessionStorageReactiveKey.svelte
@@ -3,47 +3,50 @@
     RadioButton,
     RadioButtonGroup,
     SessionStorage,
+    Stack,
   } from "carbon-components-svelte";
 
   let selectedUser = "user-1";
   let theme = "white";
+  let events = [];
 
   $: storageKey = `session-storage-reactive-example-${selectedUser}`;
 </script>
 
-<div style:margin-bottom="var(--cds-layout-01)">
-  Try selecting different users and changing their theme preferences. Each
-  user's preference is stored separately and persists within the session.
-</div>
-
-<SessionStorage key={storageKey} bind:value={theme} />
-
-<RadioButtonGroup legendText="Select user" bind:selected={selectedUser}>
-  <RadioButton labelText="User 1" value="user-1" />
-  <RadioButton labelText="User 2" value="user-2" />
-  <RadioButton labelText="User 3" value="user-3" />
-</RadioButtonGroup>
-
-<br>
-
-<RadioButtonGroup legendText="Theme preference" bind:selected={theme}>
-  <RadioButton labelText="White" value="white" />
-  <RadioButton labelText="Gray 10" value="g10" />
-  <RadioButton labelText="Gray 80" value="g80" />
-  <RadioButton labelText="Gray 90" value="g90" />
-  <RadioButton labelText="Gray 100" value="g100" />
-</RadioButtonGroup>
-
-<div
-  style="margin-top: var(--cds-layout-01)"
-  style:margin-bottom="var(--cds-layout-01)"
->
+<Stack gap={6}>
   <div>
-    <strong>Current key:</strong>
-    {storageKey}
+    Try selecting different users and changing their theme preferences. Each
+    user's preference is stored separately and persists within the session.
   </div>
-  <div>
-    <strong>Current theme:</strong>
-    {theme}
-  </div>
-</div>
+  <SessionStorage
+    key={storageKey}
+    bind:value={theme}
+    on:update={({ detail }) => {
+      events = [...events, { event: "on:update", detail }];
+    }}
+  />
+  <RadioButtonGroup legendText="Select user" bind:selected={selectedUser}>
+    <RadioButton labelText="User 1" value="user-1" />
+    <RadioButton labelText="User 2" value="user-2" />
+    <RadioButton labelText="User 3" value="user-3" />
+  </RadioButtonGroup>
+  <RadioButtonGroup legendText="Theme preference" bind:selected={theme}>
+    <RadioButton labelText="White" value="white" />
+    <RadioButton labelText="Gray 10" value="g10" />
+    <RadioButton labelText="Gray 80" value="g80" />
+    <RadioButton labelText="Gray 90" value="g90" />
+    <RadioButton labelText="Gray 100" value="g100" />
+  </RadioButtonGroup>
+  <Stack gap={2}>
+    <div>
+      <strong>Current key:</strong>
+      {storageKey}
+    </div>
+    <div>
+      <strong>Current theme:</strong>
+      {theme}
+    </div>
+  </Stack>
+  <strong> Updates: </strong>
+  <pre>{JSON.stringify(events, null, 2)}</pre>
+</Stack>
