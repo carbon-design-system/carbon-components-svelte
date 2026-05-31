9639d9327cd8758a4d956d864df9c7dc21a666c1
Eric Liu <ericyl.us@gmail.com>
2026-05-31 10:58:59 -0700

    refactor(modal): apply `trapFocus` utility

--- src/Modal/Modal.svelte
+++ src/Modal/Modal.svelte
@@ -114,6 +114,7 @@
   import { writable } from "svelte/store";
   import Button from "../Button/Button.svelte";
   import Close from "../icons/Close.svelte";
+  import { trapFocus } from "../utils/trapFocus.js";
   import { trackModal } from "./modalStore";
 
   const dispatch = createEventDispatcher();
@@ -204,56 +205,7 @@
       if (event.key === "Escape") {
         close("escape-key");
       } else if (event.key === "Tab") {
-        // trap focus
-
-        // taken from github.com/carbon-design-system/carbon/packages/react/src/internal/keyboard/navigation.js
-        const selectorTabbable = `
-  a[href], area[href], input:not([disabled]):not([tabindex='-1']),
-  button:not([disabled]):not([tabindex='-1']),select:not([disabled]):not([tabindex='-1']),
-  textarea:not([disabled]):not([tabindex='-1']),
-  iframe, object, embed, *[tabindex]:not([tabindex='-1']):not([disabled]), *[contenteditable=true]
-`;
-
-        const tabbable = Array.from(
-          ref.querySelectorAll(selectorTabbable)
-        ).filter((el) => {
-          // Filter out elements that are not visible.
-          const style = getComputedStyle(el);
-          if (style.visibility === "hidden" || style.display === "none") {
-            return false;
-          }
-
-          // Check for zero dimensions, but only if the element has been laid out
-          // (offsetParent is null for hidden elements or elements not in the DOM.
-          if (
-            el.offsetParent !== null &&
-            el.offsetWidth === 0 &&
-            el.offsetHeight === 0
-          ) {
-            return false;
-          }
-          
-          return true;
-        });
-
-        if (tabbable.length === 0) {
-          event.preventDefault();
-          return;
-        }
-
-        let index = tabbable.indexOf(document.activeElement);
-        if (index === -1) {
-          // Active element not in tabbable list, find closest tabbable element
-          // For forward Tab, start from beginning (-1 + 1 = 0)
-          // For Shift+Tab, start from end (length + -1 = length - 1)
-          index = event.shiftKey ? tabbable.length : -1;
-        }
-
-        index += event.shiftKey ? -1 : 1;
-        index = (index + tabbable.length) % tabbable.length;
-
-        tabbable[index].focus();
-        event.preventDefault();
+        trapFocus({ container: ref, event });
       } else if (
         shouldSubmitOnEnter &&
         event.key === "Enter" &&
