48d826c86d9ebc23d67fad83a6da405d7162ffa4
Eric Liu <ericyl.us@gmail.com>
2026-05-31 07:45:58 -0700

    docs(content-switcher): add "Conditional switches" example

---
+++ docs/src/pages/framed/ContentSwitcher/ContentSwitcherConditional.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {
+    Checkbox,
+    ContentSwitcher,
+    Stack,
+    Switch,
+  } from "carbon-components-svelte";
+
+  let selectedIndex = 0;
+  let showAdmin = true;
+  let showSettings = true;
+</script>
+
+<Stack gap={3}>
+  <div>
+    <Checkbox bind:checked={showAdmin} labelText="Show Admin switch" />
+    <Checkbox bind:checked={showSettings} labelText="Show Settings switch" />
+  </div>
+  <div>
+    <strong>Selected index:</strong>
+    {selectedIndex}
+  </div>
+  <ContentSwitcher bind:selectedIndex>
+    <Switch text="Dashboard" />
+    {#if showAdmin}
+      <Switch text="Admin" />
+    {/if}
+    {#if showSettings}
+      <Switch text="Settings" />
+    {/if}
+    <Switch text="Profile" />
+  </ContentSwitcher>
+</Stack>
