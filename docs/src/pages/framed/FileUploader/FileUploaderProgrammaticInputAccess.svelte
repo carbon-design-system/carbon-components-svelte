2c9bb3a6cfa08f37133e518ed6c795a89f62789d
Eric Liu <ericyl.us@gmail.com>
2026-05-31 20:36:43 -0700

    docs(file-uploader): add "Programmatic input access" example

---
+++ docs/src/pages/framed/FileUploader/FileUploaderProgrammaticInputAccess.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Button, FileUploader, Stack } from "carbon-components-svelte";
+
+  let inputRef = null;
+</script>
+
+<Stack gap={6}>
+  <Button kind="secondary" on:click={() => inputRef?.click()}>
+    Open file picker
+  </Button>
+  <FileUploader
+    bind:ref={inputRef}
+    multiple
+    accept={[".jpg", ".jpeg", ".png"]}
+    labelTitle="Upload files"
+    buttonLabel="Browse files"
+    labelDescription="Only image files are accepted."
+  />
+</Stack>
