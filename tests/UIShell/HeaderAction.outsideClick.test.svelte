ba50563031d0a17d7b7136c2649bb5d02f5f2812
Eric Liu <ericyl.us@gmail.com>
2026-06-02 12:13:26 -0700

    test(ui-shell): add `HeaderAction` outside-click tests

---
+++ tests/UIShell/HeaderAction.outsideClick.test.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    Header,
+    HeaderAction,
+    HeaderUtilities,
+  } from "carbon-components-svelte";
+
+  export let preventCloseOnClickOutside = false;
+
+  let isOpen = false;
+</script>
+
+<Header companyName="Test" platformName="Test">
+  <HeaderUtilities>
+    <HeaderAction
+      bind:isOpen
+      {preventCloseOnClickOutside}
+      transition={false}
+      iconDescription="Switcher"
+    >
+      <p data-testid="panel-content">Panel content</p>
+    </HeaderAction>
+  </HeaderUtilities>
+</Header>
