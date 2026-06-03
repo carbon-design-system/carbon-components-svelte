5c97a644dab470eb08731f7331d61a8054343234
Eric Liu <ericyl.us@gmail.com>
2026-06-02 19:01:03 -0700

    fix(copy-button): avoid dispatching "copy" event if already clicked (#3141)

---
+++ tests/CopyButton/CopyButtonDoubleClick.test.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { CopyButton } from "carbon-components-svelte";
+
+  export let copy: (text: string) => void;
+
+  let copyEventCount = 0;
+</script>
+Copy events: {copyEventCount}
+
+<CopyButton
+  text="text"
+  iconDescription="Copy"
+  {copy}
+  on:copy={() => {
+    copyEventCount += 1;
+  }}
+/>
