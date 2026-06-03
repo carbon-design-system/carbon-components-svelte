59a24783f80ee5002c04309ddb0c0410ec641d0b
Eric Liu <ericyl.us@gmail.com>
2026-06-02 19:01:42 -0700

    chore(utils): extract `copyFeedback` util

---
+++ src/utils/copyFeedback.d.ts
@@ -0,0 +1,13 @@
+export type CopyFeedbackAnimation = "fade-in" | "fade-out" | undefined;
+
+export type CopyFeedbackState = {
+  readonly animation: CopyFeedbackAnimation;
+  readonly feedbackOpen: boolean;
+  readonly timeout: ReturnType<typeof setTimeout> | undefined;
+  dismiss: () => void;
+  onClick: (performCopy: () => void, feedbackTimeout: number) => void;
+  onAnimationEnd: (event: { animationName: string }) => void;
+  cleanup: () => void;
+};
+
+export function createCopyFeedbackState(onSync?: () => void): CopyFeedbackState;
