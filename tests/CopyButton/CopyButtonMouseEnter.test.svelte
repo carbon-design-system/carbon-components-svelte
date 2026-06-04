6fa8f13e575fb78ca4999146d220c6fabdd864ea
Eric Liu <ericyl.us@gmail.com>
2026-06-03 18:56:28 -0700

    feat(copy-button): forward more events to button

---
+++ tests/CopyButton/CopyButtonMouseEnter.test.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { CopyButton } from "carbon-components-svelte";
+
+  export let onMouseEnter = () => {};
+</script>
+
+<CopyButton
+  text="hover me"
+  iconDescription="Hover test"
+  on:mouseenter={onMouseEnter}
+/>
