ed746b2d896e03068b15e97d92e6431c83a28119
Eric Liu <ericyl.us@gmail.com>
2026-06-04 20:45:02 -0700

    fix(ui-shell): `HeaderActionLink` uses correct focus outline color (#3154)

--- css/css.d.ts
+++ css/css.d.ts
@@ -1,6 +1,6 @@
-declare module "carbon-components-svelte/css/all.css";
 declare module "carbon-components-svelte/css/g10.css";
+declare module "carbon-components-svelte/css/all.css";
+declare module "carbon-components-svelte/css/white.css";
 declare module "carbon-components-svelte/css/g100.css";
 declare module "carbon-components-svelte/css/g80.css";
 declare module "carbon-components-svelte/css/g90.css";
-declare module "carbon-components-svelte/css/white.css";
