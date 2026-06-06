779a1d568f1d324ef73bb56b957565f3e9f917ea
Eric Liu <ericyl.us@gmail.com>
2026-06-06 15:55:46 -0700

    docs(code-snippet): update "Portal tooltip" example

--- docs/src/pages/framed/CodeSnippet/CodeSnippetPortalTooltip.svelte
+++ docs/src/pages/framed/CodeSnippet/CodeSnippetPortalTooltip.svelte
@@ -1,13 +1,32 @@
 <script>
   import { CodeSnippet, Stack } from "carbon-components-svelte";
+
+  let multiCode =
+    "export function add(a, b) {\n  return a + b;\n}\n\nexport function subtract(a, b) {\n  return a - b;\n}";
 </script>
 
 <Stack
   gap={4}
-  style="overflow: hidden; border: 1px dashed var(--cds-border-subtle); padding: 1rem; max-height: 120px;"
+  style="overflow: hidden; border: 1px dashed var(--cds-border-subtle); padding: 1rem; max-height: 200px;"
 >
+  <div>
+    <CodeSnippet
+      type="inline"
+      code="rm -rf node_modules/"
+      feedback="Copied!"
+      portalTooltip={true}
+    />
+  </div>
+
   <CodeSnippet
     code="npm i carbon-components-svelte"
+    feedback="Copied!"
+    portalTooltip={true}
+  />
+
+  <CodeSnippet
+    type="multi"
+    code={multiCode}
     feedback="Copied!"
     portalTooltip={true}
   />
