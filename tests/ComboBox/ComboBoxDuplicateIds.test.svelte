135bb17ceaaf23d7e8dc962cfd6a8f34fe9f94d6
Eric Liu <ericyl.us@gmail.com>
2026-06-06 20:41:43 -0700

    fix(combo-box): scope option ids per instance (#3176)

---
+++ tests/ComboBox/ComboBoxDuplicateIds.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import ComboBox from "carbon-components-svelte/ComboBox/ComboBox.svelte";
+
+  const items = [
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ];
+</script>
+
+<ComboBox id="combo-a" labelText="Contact A" {items} open />
+<ComboBox id="combo-b" labelText="Contact B" {items} open />
