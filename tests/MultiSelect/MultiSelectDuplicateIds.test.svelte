202af8868865bc3a7577c37fc0a0e0df3a155000
Eric Liu <ericyl.us@gmail.com>
2026-06-06 20:13:13 -0700

    fix(multi-select): scope checkbox ids per instance

---
+++ tests/MultiSelect/MultiSelectDuplicateIds.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { MultiSelect } from "carbon-components-svelte";
+
+  const items = [
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ];
+</script>
+
+<MultiSelect id="first" labelText="First" {items} open />
+<MultiSelect id="second" labelText="Second" {items} open />
