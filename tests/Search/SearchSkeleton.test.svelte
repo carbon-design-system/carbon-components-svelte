e5f570a208ec696a1c6abc99b055cf70aecdd76b
Eric Liu <ericyl.us@gmail.com>
2026-06-09 08:05:58 -0700

    feat(search-skeleton): add `hideLabel` prop

--- tests/Search/SearchSkeleton.test.svelte
+++ tests/Search/SearchSkeleton.test.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Search from "carbon-components-svelte/Search/Search.svelte";
+  import SearchSkeleton from "carbon-components-svelte/Search/SearchSkeleton.svelte";
 </script>
 
 <Search skeleton labelText="Default skeleton" />
@@ -7,3 +8,5 @@
 <Search size="lg" skeleton labelText="Large skeleton" />
 
 <Search size="sm" skeleton labelText="Small skeleton" />
+
+<SearchSkeleton hideLabel data-testid="skeleton-hide-label" />
