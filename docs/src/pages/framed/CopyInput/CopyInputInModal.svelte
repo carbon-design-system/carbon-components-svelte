7a6bf0ffe019fec249b9f410eafdec9203f674e6
Eric Liu <ericyl.us@gmail.com>
2026-06-08 19:59:11 -0700

    doc(copy-input): add examples

---
+++ docs/src/pages/framed/CopyInput/CopyInputInModal.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Button, CopyInput, Modal } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open modal</Button>
+
+<Modal
+  bind:open
+  modalHeading="API credentials"
+  primaryButtonText="Done"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (open = false)}
+>
+  <CopyInput labelText="API token" value="sk-1234567890abcdef" />
+</Modal>
