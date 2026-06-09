d07dc2e9bdbf7ad7a10da5b08bfe0238f2432835
Eric Liu <ericyl.us@gmail.com>
2026-06-09 08:41:56 -0700

    docs(ui-shell): add "Header with custom search results" example

---
+++ docs/src/pages/framed/UIShell/HeaderSearchSlot.svelte
@@ -0,0 +1,67 @@
+<script>
+  import {
+    Content,
+    Header,
+    HeaderSearch,
+    HeaderUtilities,
+    SkipToContent,
+  } from "carbon-components-svelte";
+
+  const data = [
+    {
+      id: "kubernetes-service",
+      href: "/",
+      text: "Kubernetes Service",
+      description: "Deploy secure, highly available apps.",
+    },
+    {
+      id: "red-hat-openshift",
+      href: "/",
+      text: "Red Hat OpenShift on IBM Cloud",
+      description: "Deploy and secure enterprise workloads.",
+    },
+    {
+      id: "container-registry",
+      href: "/",
+      text: "Container Registry",
+      description: "Securely store container images.",
+    },
+  ];
+
+  let active = false;
+  let value = "";
+  let selectedResultIndex = 0;
+
+  $: lowerCaseValue = value.toLowerCase();
+  $: results =
+    value.length > 0
+      ? data.filter((item) => item.text.toLowerCase().includes(lowerCaseValue))
+      : [];
+</script>
+
+<Header companyName="IBM" platformName="Carbon Svelte">
+  <svelte:fragment slot="skipToContent"> <SkipToContent /> </svelte:fragment>
+  <HeaderUtilities>
+    <HeaderSearch
+      bind:active
+      bind:value
+      bind:selectedResultIndex
+      placeholder="Search services"
+      {results}
+      let:result
+      let:selected
+    >
+      <div>
+        <strong>{result.text}</strong>
+        {#if selected}
+          <span>&nbsp;(selected)</span>
+        {/if}
+      </div>
+      <div>{result.description}</div>
+    </HeaderSearch>
+  </HeaderUtilities>
+</Header>
+
+<Content>
+  <p>Activate the search bar and type "service" to see custom results.</p>
+</Content>
