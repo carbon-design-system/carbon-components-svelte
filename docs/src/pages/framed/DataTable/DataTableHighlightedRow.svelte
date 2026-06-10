00c78f12c6ccb3dcf957a229075c6770fb829219
Eric Liu <ericyl.us@gmail.com>
2026-06-09 17:41:00 -0700

    docs(data-table): add "Highlight a row" example

---
+++ docs/src/pages/framed/DataTable/DataTableHighlightedRow.svelte
@@ -0,0 +1,46 @@
+<script>
+  import {
+    DataTable,
+    OverflowMenu,
+    OverflowMenuItem,
+  } from "carbon-components-svelte";
+
+  const headers = [
+    { key: "name", value: "Name" },
+    { key: "port", value: "Port" },
+    { key: "rule", value: "Rule" },
+    { key: "overflow", empty: true, width: "72px" },
+  ];
+
+  const rows = [
+    { id: "a", name: "Load Balancer 3", port: 3000, rule: "Round robin" },
+    { id: "b", name: "Load Balancer 1", port: 443, rule: "Round robin" },
+    { id: "c", name: "Load Balancer 2", port: 80, rule: "DNS delegation" },
+    { id: "d", name: "Load Balancer 6", port: 3000, rule: "Round robin" },
+    { id: "e", name: "Load Balancer 4", port: 443, rule: "Round robin" },
+    { id: "f", name: "Load Balancer 5", port: 80, rule: "DNS delegation" },
+  ];
+
+  let openRows = {};
+
+  $: highlightedRowIds = Object.entries(openRows)
+    .filter(([, open]) => open)
+    .map(([id]) => id);
+</script>
+
+<DataTable sortable {headers} {rows} {highlightedRowIds}>
+  <svelte:fragment slot="cell" let:row let:cell>
+    {#if cell.key === "overflow"}
+      <OverflowMenu portalMenu flipped bind:open={openRows[row.id]}>
+        <OverflowMenuItem text="Restart" />
+        <OverflowMenuItem
+          href="https://cloud.ibm.com/docs/loadbalancer-service"
+          text="API documentation"
+        />
+        <OverflowMenuItem danger text="Stop" />
+      </OverflowMenu>
+    {:else}
+      {cell.value}
+    {/if}
+  </svelte:fragment>
+</DataTable>
