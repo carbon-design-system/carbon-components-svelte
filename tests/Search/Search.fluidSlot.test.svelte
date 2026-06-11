a5885f96442cd2560ee7f7b1235a89853c3ce43f
Eric Liu <ericyl.us@gmail.com>
2026-06-10 21:19:33 -0700

    feat(search): add `fluid` prop for fluid form support

---
+++ tests/Search/Search.fluidSlot.test.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Search from "carbon-components-svelte/Search/Search.svelte";
+
+  export let fluid = true;
+</script>
+
+<Search {fluid} labelText="Default label">
+  <svelte:fragment slot="labelChildren">Custom label content</svelte:fragment>
+</Search>
