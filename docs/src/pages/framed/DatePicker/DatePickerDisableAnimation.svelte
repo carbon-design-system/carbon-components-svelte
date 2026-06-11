061bfd22d4d4167e8d82603450ff78abba2e1417
Eric Liu <ericyl.us@gmail.com>
2026-06-11 12:03:14 -0700

    docs(date-picker): add "Disable open animation" example (#3276)

---
+++ docs/src/pages/framed/DatePicker/DatePickerDisableAnimation.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="single" flatpickrProps={{ animate: false }}>
+  <DatePickerInput labelText="Date of birth" placeholder="mm/dd/yyyy" />
+</DatePicker>
