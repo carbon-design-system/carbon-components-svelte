d8c85eb00015fbb18b0ddb7c5598d8bf236c3722
Eric Liu <ericyl.us@gmail.com>
2026-06-10 23:24:14 -0700

    feat(dropdown): add `fluid` prop for fluid form support

---
+++ tests/Dropdown/Dropdown.fluidSlot.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Dropdown from "carbon-components-svelte/Dropdown/Dropdown.svelte";
+
+  export let fluid = true;
+
+  const items = [
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+  ];
+</script>
+
+<Dropdown
+  {fluid}
+  id="test-dropdown"
+  {items}
+  labelText="Default label"
+  label="Choose a contact method"
+>
+  <svelte:fragment slot="labelChildren">Custom label content</svelte:fragment>
+</Dropdown>
