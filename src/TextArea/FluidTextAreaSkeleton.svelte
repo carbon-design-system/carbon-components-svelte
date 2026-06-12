b23dfdd04dabfc66e8927e076c3933bf3e660dc3
Eric Liu <ericyl.us@gmail.com>
2026-06-11 18:02:14 -0700

    feat(text-area): add FluidTextAreaSkeleton component

---
+++ src/TextArea/FluidTextAreaSkeleton.svelte
@@ -0,0 +1,14 @@
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class:bx--form-item={true}
+  class:bx--text-area--fluid__skeleton={true}
+  {...$$restProps}
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+>
+  <span class:bx--label={true} class:bx--skeleton={true}></span>
+  <div class:bx--skeleton={true} class:bx--text-area={true}></div>
+</div>
