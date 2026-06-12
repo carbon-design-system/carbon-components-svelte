aa3c1b0e7c9c1048e245e00f987cae38c6d55122
Eric Liu <ericyl.us@gmail.com>
2026-06-11 18:30:50 -0700

    feat(time-picker): add FluidTimePickerSkeleton component

--- src/TimePicker/index.js
+++ src/TimePicker/index.js
@@ -1,2 +1,3 @@
+export { default as FluidTimePickerSkeleton } from "./FluidTimePickerSkeleton.svelte";
 export { default as TimePicker } from "./TimePicker.svelte";
 export { default as TimePickerSelect } from "./TimePickerSelect.svelte";
---
+++ tests/TimePicker/TimePicker.fluidSkeleton.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import FluidTimePickerSkeleton from "carbon-components-svelte/TimePicker/FluidTimePickerSkeleton.svelte";
+</script>
+
+<FluidTimePickerSkeleton
+  data-testid="fluid-time-picker-skeleton"
+  {...$$props}
+/>
