f61eb273a2aa3411cd107217668c93a8e6820bdc
Eric Liu <ericyl.us@gmail.com>
2026-06-11 18:11:18 -0700

    feat(dropdown): add FluidDropdownSkeleton component

---
+++ tests/Dropdown/Dropdown.fluidSkeleton.test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import FluidDropdownSkeleton from "carbon-components-svelte/Dropdown/FluidDropdownSkeleton.svelte";
+</script>
+
+<FluidDropdownSkeleton data-testid="fluid-dropdown-skeleton" />
