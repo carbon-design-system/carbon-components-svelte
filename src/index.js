1ba2da382f3a739e53e47ef6686d194e3103e054
Eric Liu <ericyl.us@gmail.com>
2026-06-11 18:17:51 -0700

    feat(combo-box): add FluidComboBoxSkeleton component

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ export { default as CheckboxSkeleton } from "./Checkbox/CheckboxSkeleton.svelte"
 export { default as CodeSnippet } from "./CodeSnippet/CodeSnippet.svelte";
 export { default as CodeSnippetSkeleton } from "./CodeSnippet/CodeSnippetSkeleton.svelte";
 export { default as ComboBox } from "./ComboBox/ComboBox.svelte";
+export { default as FluidComboBoxSkeleton } from "./ComboBox/FluidComboBoxSkeleton.svelte";
 export { default as ComposedModal } from "./ComposedModal/ComposedModal.svelte";
 export { default as ModalBody } from "./ComposedModal/ModalBody.svelte";
 export { default as ModalFooter } from "./ComposedModal/ModalFooter.svelte";
---
+++ tests/ComboBox/ComboBox.fluidSkeleton.test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import FluidComboBoxSkeleton from "carbon-components-svelte/ComboBox/FluidComboBoxSkeleton.svelte";
+</script>
+
+<FluidComboBoxSkeleton data-testid="fluid-combo-box-skeleton" />
