e8b2eb1941c93cee8a468ead8464cb53d3e8a933
Eric Liu <ericyl.us@gmail.com>
2026-06-12 10:26:48 -0700

    feat(password-input): add `PasswordInputSkeleton`
    Recycles the `bx--text-input` + `bx--skeleton` classes, mirroring
    `TextInputSkeleton` (the visibility toggle is omitted from the skeleton).

--- src/TextInput/index.js
+++ src/TextInput/index.js
@@ -1,4 +1,5 @@
 export { default as FluidTextInputSkeleton } from "./FluidTextInputSkeleton.svelte";
 export { default as PasswordInput } from "./PasswordInput.svelte";
+export { default as PasswordInputSkeleton } from "./PasswordInputSkeleton.svelte";
 export { default as TextInput } from "./TextInput.svelte";
 export { default as TextInputSkeleton } from "./TextInputSkeleton.svelte";
