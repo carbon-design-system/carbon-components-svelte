8a68a0499fb225ca7fd4b195c8478d87f820c390
Eric Liu <ericyl.us@gmail.com>
2026-06-11 17:50:14 -0700

    feat(text-input): add FluidTextInputSkeleton component

---
+++ tests/TextInput/TextInput.fluidSkeleton.test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import FluidTextInputSkeleton from "carbon-components-svelte/TextInput/FluidTextInputSkeleton.svelte";
+</script>
+
+<FluidTextInputSkeleton data-testid="fluid-text-input-skeleton" />
