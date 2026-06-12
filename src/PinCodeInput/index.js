c6fc2eb84931d7eed6545e2e508aaa6235ed4e25
Eric Liu <ericyl.us@gmail.com>
2026-06-12 10:26:32 -0700

    feat(pin-code-input): add `PinCodeInputSkeleton`
    Recycles the `bx--text-input` + `bx--skeleton` classes, matching
    `TextInputSkeleton` and the existing `FluidPinCodeInputSkeleton`.

--- src/PinCodeInput/index.js
+++ src/PinCodeInput/index.js
@@ -1,2 +1,3 @@
 export { default as FluidPinCodeInputSkeleton } from "./FluidPinCodeInputSkeleton.svelte";
 export { default as PinCodeInput } from "./PinCodeInput.svelte";
+export { default as PinCodeInputSkeleton } from "./PinCodeInputSkeleton.svelte";
---
+++ tests/PinCodeInput/PinCodeInput.skeleton.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import PinCodeInputSkeleton from "carbon-components-svelte/PinCodeInput/PinCodeInputSkeleton.svelte";
+
+  export let hideLabel = false;
+</script>
+
+<PinCodeInputSkeleton data-testid="pin-code-input-skeleton" {hideLabel} />
