367381b6e02abd881a586cb5f01170a049498ff1
Eric Liu <ericyl.us@gmail.com>
2026-06-13 12:46:02 -0700

    docs(data-table): add "Sticky header max height" example

---
+++ docs/src/pages/framed/DataTable/StickyHeaderMaxHeight.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { DataTable } from "carbon-components-svelte";
+
+  const rows = Array.from({ length: 20 }).map((_, i) => ({
+    id: i,
+    name: `Load Balancer ${i + 1}`,
+    protocol: "HTTP",
+    port: i % 3 ? (i % 2 ? 3000 : 80) : 443,
+    rule: i % 3 ? "Round robin" : "DNS delegation",
+  }));
+</script>
+
+<DataTable
+  title="Load balancers"
+  description="Your organization's active load balancers."
+  stickyHeader
+  stickyHeaderMaxHeight={500}
+  headers={[
+    { key: "name", value: "Name" },
+    { key: "protocol", value: "Protocol" },
+    { key: "port", value: "Port" },
+    { key: "rule", value: "Rule" },
+  ]}
+  {rows}
+/>
