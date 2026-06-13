9288ab0b6f4b10631d81937c48b6c87215798281
Eric Liu <ericyl.us@gmail.com>
2026-06-13 08:57:08 -0700

    feat(copy-input): add FluidCopyInputSkeleton component

--- src/CopyInput/index.js
+++ src/CopyInput/index.js
@@ -1,1 +1,3 @@
 export { default as CopyInput } from "./CopyInput.svelte";
+export { default as CopyInputSkeleton } from "./CopyInputSkeleton.svelte";
+export { default as FluidCopyInputSkeleton } from "./FluidCopyInputSkeleton.svelte";
---
+++ tests/CopyInput/CopyInput.fluidSkeleton.test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import FluidCopyInputSkeleton from "carbon-components-svelte/CopyInput/FluidCopyInputSkeleton.svelte";
+</script>
+
+<FluidCopyInputSkeleton data-testid="fluid-copy-input-skeleton" />
