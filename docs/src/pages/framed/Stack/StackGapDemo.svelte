778ab52301e18f6aca6aec668fa22b0705c35209
Eric Liu <ericyl.us@gmail.com>
2026-06-14 13:37:25 -0700

    docs: revise component page headings and descriptions

---
+++ docs/src/pages/framed/Stack/StackGapDemo.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Slider, Stack } from "carbon-components-svelte";
+
+  /** @type {"horizontal" | "vertical"} */
+  export let orientation = "vertical";
+
+  let gap = 5;
+</script>
+
+<div style="margin-bottom: 1.5rem">
+  <Slider
+    labelText="Gap"
+    min={0}
+    max={13}
+    minLabel="0"
+    maxLabel="13"
+    bind:value={gap}
+  />
+</div>
+
+<Stack {orientation} {gap}>
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+</Stack>
