5b3a26a3b158f1b69a85a63c639a58ec7e583fdf
Eric Liu <ericyl.us@gmail.com>
2026-06-13 21:31:38 -0700

    feat(content-switcher): support icon-only, low contrast variant

---
+++ tests/ContentSwitcher/ContentSwitcher.lowContrastIconOnly.test.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ContentSwitcher from "carbon-components-svelte/ContentSwitcher/ContentSwitcher.svelte";
+  import Switch from "carbon-components-svelte/ContentSwitcher/Switch.svelte";
+  import Dashboard from "carbon-icons-svelte/lib/Dashboard.svelte";
+  import List from "carbon-icons-svelte/lib/List.svelte";
+  import TableOfContents from "carbon-icons-svelte/lib/TableOfContents.svelte";
+</script>
+
+<ContentSwitcher lowContrast>
+  <Switch icon={TableOfContents} text="Table of contents" />
+  <Switch icon={Dashboard} text="Dashboard" />
+  <Switch icon={List} text="List" />
+</ContentSwitcher>
