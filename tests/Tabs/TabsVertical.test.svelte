02bbd96f3ee7a99b27e068b350868b17c3bc8ec4
Eric Liu <ericyl.us@gmail.com>
2026-06-15 20:01:51 -0700

    feat(tabs-vertical): add `TabsVertical` component
    Closes #2726
    
    Stack tabs in a left column on md and wider screens and collapse to a
    horizontal, scrollable row below md. Reuses the existing Tab and
    TabContent components.

---
+++ tests/Tabs/TabsVertical.test.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Tab from "carbon-components-svelte/Tabs/Tab.svelte";
+  import TabContent from "carbon-components-svelte/Tabs/TabContent.svelte";
+  import TabsVertical from "carbon-components-svelte/Tabs/TabsVertical.svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let selected = 0;
+  export let icon: ComponentProps<Tab>["icon"] = undefined;
+</script>
+
+<TabsVertical
+  {selected}
+  on:change={({ detail }) => {
+    console.log("change event", detail);
+  }}
+>
+  <Tab label="Tab 1" {icon} />
+  <Tab label="Tab 2" disabled />
+  <Tab label="Tab 3" />
+  <Tab label="Tab 4" />
+  <svelte:fragment slot="content">
+    <TabContent>Content 1</TabContent>
+    <TabContent>Content 2</TabContent>
+    <TabContent>Content 3</TabContent>
+    <TabContent>Content 4</TabContent>
+  </svelte:fragment>
+</TabsVertical>
