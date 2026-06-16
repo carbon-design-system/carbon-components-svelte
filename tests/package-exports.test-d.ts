11b99afbf66912ff76dda8dfd760f43e7287dd38
Eric Liu <ericyl.us@gmail.com>
2026-06-15 20:33:18 -0700

    test: assert package imports for new stores

--- tests/package-exports.test-d.ts
+++ tests/package-exports.test-d.ts
@@ -1,14 +1,28 @@
 import type {
   Button as BarrelButton,
   DataTable as BarrelDataTable,
+  breakpoint,
   breakpointObserver,
   breakpoints,
+  colorScheme,
+  containerQuery,
+  mediaQuery,
+  prefersContrast,
+  prefersDarkMode,
+  prefersReducedMotion,
 } from "carbon-components-svelte";
 import type breakpointObserverDefault from "carbon-components-svelte/src/Breakpoint/breakpointObserver.js";
 import type { BreakpointSize } from "carbon-components-svelte/src/Breakpoint/breakpoints.js";
 import type Button from "carbon-components-svelte/src/Button/Button.svelte";
 import type DataTable from "carbon-components-svelte/src/DataTable/DataTable.svelte";
 import type { DataTableRow } from "carbon-components-svelte/src/DataTable/DataTable.svelte";
+import type {
+  ColorSchemeOptions,
+  ColorSchemePreference,
+  ResolvedColorScheme,
+} from "carbon-components-svelte/src/stores/colorScheme.js";
+import type containerQueryDefault from "carbon-components-svelte/src/stores/containerQuery.js";
+import type mediaQueryDefault from "carbon-components-svelte/src/stores/mediaQuery.js";
 import type {
   CarbonTheme,
   ThemeProps,
@@ -36,3 +50,15 @@ type _DataTableRow = DataTableRow<{ id: string; name: string }>;
 type _HeaderSearchResult = HeaderSearchResult;
 type _ThemeProps = ThemeProps;
 type _Themes = typeof themes;
+type _MediaQuery = typeof mediaQuery;
+type _MediaQueryDefault = typeof mediaQueryDefault;
+type _PrefersDarkMode = typeof prefersDarkMode;
+type _PrefersReducedMotion = typeof prefersReducedMotion;
+type _PrefersContrast = typeof prefersContrast;
+type _Breakpoint = typeof breakpoint;
+type _ContainerQuery = typeof containerQuery;
+type _ContainerQueryDefault = typeof containerQueryDefault;
+type _ColorScheme = typeof colorScheme;
+type _ColorSchemeOptions = ColorSchemeOptions;
+type _ColorSchemePreference = ColorSchemePreference;
+type _ResolvedColorScheme = ResolvedColorScheme;
