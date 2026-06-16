f954521aa0f08d0817fe1d1c58de95ccfe975f6f
Eric Liu <ericyl.us@gmail.com>
2026-06-15 20:33:39 -0700

    feat(tabs-vertical): add `TabsVerticalSkeleton`

---
+++ src/Tabs/TabsVerticalSkeleton.svelte
@@ -0,0 +1,27 @@
+<script>
+  /** Specify the number of tabs to render */
+  export let count = 4;
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class:bx--tabs--vertical-container={true}>
+  <div
+    class:bx--tabs={true}
+    class:bx--tabs--vertical={true}
+    class:bx--skeleton={true}
+    {...$$restProps}
+    on:click
+    on:mouseover
+    on:mouseenter
+    on:mouseleave
+  >
+    <ul class:bx--tabs__nav={true}>
+      {#each Array.from({ length: count }, (_, i) => i) as item (item)}
+        <li class:bx--tabs__nav-item={true}>
+          <div class:bx--tabs__nav-link={true}><span></span></div>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
---
+++ tests/Tabs/TabsVerticalSkeleton.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TabsVerticalSkeleton from "carbon-components-svelte/Tabs/TabsVerticalSkeleton.svelte";
+
+  export let count = 4;
+</script>
+
+<TabsVerticalSkeleton {count} />
