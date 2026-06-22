1cbb15c3574d686deb00043a332f17c62bbab653
Eric Liu <ericyl.us@gmail.com>
2026-06-22 12:52:22 -0700

    docs(code-snippet): add "Overflow detection" example

---
+++ docs/src/pages/framed/CodeSnippet/CodeSnippetOverflow.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { CodeSnippet, Stack, Toggle } from "carbon-components-svelte";
+
+  let expandable = false;
+
+  $: length = expandable ? 24 : 4;
+  $: code = Array.from(
+    { length },
+    (_, i) => `const line${i + 1} = ${i + 1};`,
+  ).join("\n");
+</script>
+
+<Stack gap={5}>
+  <Toggle bind:toggled={expandable} size="sm" labelText="Grow content" />
+  <CodeSnippet type="multi" {code} />
+</Stack>
