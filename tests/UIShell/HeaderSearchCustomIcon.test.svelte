4b0a656897413844e2984a7c5e16dba5c9524f03
Eric Liu <ericyl.us@gmail.com>
2026-06-26 12:51:46 -0700

    feat(header-search): support icon prop override

---
+++ tests/UIShell/HeaderSearchCustomIcon.test.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let size = 16;
+  export let title: string | undefined = undefined;
+</script>
+
+<svg data-testid="custom-icon" width={size} height={size}>
+  {#if title}
+    <title>{title}</title>
+  {/if}
+</svg>
