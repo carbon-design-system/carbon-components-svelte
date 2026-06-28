a7344bd8ec4c2e191b185e0f4c7bd09257af6ef1
Eric Liu <ericyl.us@gmail.com>
2026-06-27 21:23:34 -0700

    docs(ui-shell): add `ProfileMenu` examples

---
+++ docs/src/pages/framed/UIShell/ProfileMenuDefaultAvatar.svelte
@@ -0,0 +1,42 @@
+<script>
+  import {
+    Content,
+    Header,
+    HeaderUtilities,
+    ProfileMenu,
+    ProfileMenuDivider,
+    ProfileMenuHeader,
+    ProfileMenuItem,
+    ProfileMenuList,
+    SkipToContent,
+  } from "carbon-components-svelte";
+  import Logout from "carbon-icons-svelte/lib/Logout.svelte";
+</script>
+
+<Header companyName="IBM" platformName="Carbon Svelte">
+  <svelte:fragment slot="skipToContent"> <SkipToContent /> </svelte:fragment>
+  <HeaderUtilities>
+    <ProfileMenu iconDescription="Profile">
+      <ProfileMenuHeader
+        name="Richard Hendricks"
+        username="rhendricks"
+        href="/"
+      />
+      <ProfileMenuDivider />
+      <ProfileMenuList>
+        <ProfileMenuItem href="/">Settings</ProfileMenuItem>
+        <ProfileMenuItem href="/">Privacy</ProfileMenuItem>
+      </ProfileMenuList>
+      <ProfileMenuDivider />
+      <ProfileMenuList>
+        <ProfileMenuItem icon={Logout} on:click={() => {}}>
+          Log out
+        </ProfileMenuItem>
+      </ProfileMenuList>
+    </ProfileMenu>
+  </HeaderUtilities>
+</Header>
+
+<Content>
+  <h1>Welcome</h1>
+</Content>
---
+++ docs/src/pages/framed/UIShell/ProfileMenuSimple.svelte
@@ -0,0 +1,53 @@
+<script>
+  import {
+    Content,
+    Header,
+    HeaderUtilities,
+    ProfileMenu,
+    ProfileMenuDivider,
+    ProfileMenuHeader,
+    ProfileMenuItem,
+    ProfileMenuList,
+    SkipToContent,
+    UserAvatar,
+  } from "carbon-components-svelte";
+  import Logout from "carbon-icons-svelte/lib/Logout.svelte";
+</script>
+
+<Header companyName="IBM" platformName="Carbon Svelte">
+  <svelte:fragment slot="skipToContent"> <SkipToContent /> </svelte:fragment>
+  <HeaderUtilities>
+    <ProfileMenu iconDescription="Profile">
+      <UserAvatar
+        slot="avatar"
+        size="md"
+        name="Monica Hall"
+        backgroundColor="purple"
+      />
+
+      <ProfileMenuHeader name="Monica Hall" username="mhall" text="">
+        <UserAvatar
+          slot="avatar"
+          size="lg"
+          name="Monica Hall"
+          backgroundColor="purple"
+        />
+      </ProfileMenuHeader>
+      <ProfileMenuDivider />
+      <ProfileMenuList>
+        <ProfileMenuItem href="/">Settings</ProfileMenuItem>
+        <ProfileMenuItem href="/">Privacy</ProfileMenuItem>
+      </ProfileMenuList>
+      <ProfileMenuDivider />
+      <ProfileMenuList>
+        <ProfileMenuItem icon={Logout} on:click={() => {}}>
+          Log out
+        </ProfileMenuItem>
+      </ProfileMenuList>
+    </ProfileMenu>
+  </HeaderUtilities>
+</Header>
+
+<Content>
+  <h1>Welcome</h1>
+</Content>
