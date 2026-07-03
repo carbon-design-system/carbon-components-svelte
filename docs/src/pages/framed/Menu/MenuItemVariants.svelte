3d1e3682c53e67c0cddf2491f881f35c0c8f836e
Eric Liu <ericyl.us@gmail.com>
2026-07-02 18:14:14 -0700

    docs(menu): add size and item variants examples
    Size was added to Menu without a docs section. Item variants covers
    MenuItem's icon and danger kind, MenuDivider, and a nested submenu
    item, with a cross-link to MenuButton's fuller nested-item writeup.

---
+++ docs/src/pages/framed/Menu/MenuItemVariants.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {
+    Button,
+    Menu,
+    MenuDivider,
+    MenuItem,
+  } from "carbon-components-svelte";
+  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
+  import Cut from "carbon-icons-svelte/lib/Cut.svelte";
+  import TrashCan from "carbon-icons-svelte/lib/TrashCan.svelte";
+
+  let anchor;
+  let open = false;
+</script>
+
+<Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
+
+<Menu {anchor} bind:open labelText="Actions menu">
+  <MenuItem icon={Cut} on:click={() => console.log("Cut")}>Cut</MenuItem>
+  <MenuItem icon={Copy} on:click={() => console.log("Copy")}>Copy</MenuItem>
+  <MenuItem labelText="Export as">
+    <MenuItem on:click={() => console.log("PDF")}>PDF</MenuItem>
+    <MenuItem on:click={() => console.log("JPG")}>JPG</MenuItem>
+  </MenuItem>
+  <MenuDivider />
+  <MenuItem
+    icon={TrashCan}
+    kind="danger"
+    on:click={() => console.log("Delete")}
+  >
+    Delete
+  </MenuItem>
+</Menu>
