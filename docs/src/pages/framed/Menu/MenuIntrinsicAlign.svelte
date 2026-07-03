6b3aca2c3c134378b263ec0b4e66cc1527ba40e8
Eric Liu <ericyl.us@gmail.com>
2026-07-03 11:43:51 -0700

    docs(menu): reorganize into grouped sections mirroring MenuButton
    Group Menu.svx sections under Size, Menu items, and Placement headings
    with descriptive subsections, splitting item-variant and placement
    examples to match MenuButton.svx's structure and voice.

---
+++ docs/src/pages/framed/Menu/MenuIntrinsicAlign.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+
+  let anchor;
+  let open = false;
+</script>
+
+<div style="width: 320px;">
+  <Button bind:ref={anchor} on:click={() => (open = !open)}>
+    Actions on this wide button
+  </Button>
+</div>
+
+<Menu
+  {anchor}
+  bind:open
+  intrinsicWidth
+  intrinsicAlign="end"
+  labelText="Actions menu"
+>
+  <MenuItem on:click={() => console.log("Cut")}>Cut</MenuItem>
+  <MenuItem on:click={() => console.log("Copy")}>Copy</MenuItem>
+  <MenuItem on:click={() => console.log("Paste")}>Paste</MenuItem>
+</Menu>
---
+++ docs/src/pages/framed/Menu/MenuItemIcons.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
+  import Cut from "carbon-icons-svelte/lib/Cut.svelte";
+
+  let anchor;
+  let open = false;
+</script>
+
+<Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
+
+<Menu {anchor} bind:open labelText="Actions menu">
+  <MenuItem icon={Cut} on:click={() => console.log("Cut")}>Cut</MenuItem>
+  <MenuItem icon={Copy} on:click={() => console.log("Copy")}>Copy</MenuItem>
+</Menu>
