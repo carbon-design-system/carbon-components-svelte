44b3ed4a3527359ca0ea1f12db20319356504719
Eric Liu <ericyl.us@gmail.com>
2026-07-03 13:15:05 -0700

    docs(date-picker): add "month" examples
    Basic month calendar, custom format/placeholder, programmatic
    month decrement, and a fluid variant example.

---
+++ docs/src/pages/framed/DatePicker/DatePickerMonth.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="month" dateFormat="F Y" on:change>
+  <DatePickerInput labelText="Billing month" placeholder="Month Year" />
+</DatePicker>
---
+++ docs/src/pages/framed/DatePicker/DatePickerMonthProgrammatic.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {
+    Button,
+    DatePicker,
+    DatePickerInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let value = "03/2026";
+
+  function previousMonth() {
+    var [month, year] = value.split("/").map(Number);
+    var d = new Date(year, month - 2, 1);
+    var mm = String(d.getMonth() + 1).padStart(2, "0");
+    value = `${mm}/${d.getFullYear()}`;
+  }
+</script>
+
+<Stack gap={4}>
+  <DatePicker datePickerType="month" dateFormat="m/Y" bind:value on:change>
+    <DatePickerInput labelText="Billing month" placeholder="mm/yyyy" />
+  </DatePicker>
+  <Button on:click={previousMonth}>Previous month</Button>
+</Stack>
