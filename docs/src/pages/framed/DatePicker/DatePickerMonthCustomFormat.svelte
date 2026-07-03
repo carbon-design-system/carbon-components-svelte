a7a4354cf1a254b8f0a7a509c5b964ac5d5f6fb7
Eric Liu <ericyl.us@gmail.com>
2026-07-03 13:15:05 -0700

    docs(date-picker): add "month" examples
    Basic month calendar, custom format/placeholder, programmatic
    month decrement, and a fluid variant example.

---
+++ docs/src/pages/framed/DatePicker/DatePickerMonthCustomFormat.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="month" dateFormat="Y-m" on:change>
+  <DatePickerInput labelText="Billing month" placeholder="yyyy-mm" />
+</DatePicker>
