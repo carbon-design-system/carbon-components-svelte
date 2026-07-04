66334af68050da9fd25894ad3a962a3209a7f301
Eric Liu <ericyl.us@gmail.com>
2026-07-03 18:25:25 -0700

    feat(combo-button): add `ComboButton` component
    Closes #2488

---
+++ tests/ComboButton/ComboButton.test.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import ComboButton from "carbon-components-svelte/ComboButton/ComboButton.svelte";
+  import MenuItem from "carbon-components-svelte/Menu/MenuItem.svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let disabled: ComponentProps<ComboButton>["disabled"] = false;
+  export let size: ComponentProps<ComboButton>["size"] = undefined;
+  export let direction: ComponentProps<ComboButton>["direction"] = undefined;
+  export let tooltipPosition: ComponentProps<ComboButton>["tooltipPosition"] =
+    undefined;
+  export let iconDescription: ComponentProps<ComboButton>["iconDescription"] =
+    undefined;
+
+  export let open = false;
+
+  const items = ["Save as", "Save a copy"];
+</script>
+
+<ComboButton
+  labelText="Save"
+  {disabled}
+  {size}
+  {direction}
+  {tooltipPosition}
+  {iconDescription}
+  bind:open
+  on:click={() => console.log("click")}
+  on:close={(e) => console.log("close", e.detail)}
+>
+  {#each items as text}
+    <MenuItem on:click={() => console.log("select", text)}>{text}</MenuItem>
+  {/each}
+</ComboButton>
