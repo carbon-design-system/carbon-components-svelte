b25f7592b5fa2e2bfee0f1b0185a382099ad74e2
Eric Liu <ericyl.us@gmail.com>
2026-08-01 11:58:49 -0700

    docs(menu-button): add selectable and radio group examples

---
+++ docs/src/pages/framed/MenuButton/MenuButtonRadioGroup.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuItemRadioGroup,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let selectedId = "comfortable";
+</script>
+
+<Stack gap={5}>
+  <MenuButton labelText="View">
+    <MenuItem on:click={() => console.log("Reset")}>Reset</MenuItem>
+    <MenuDivider />
+    <MenuItemRadioGroup labelText="Density" bind:selectedId>
+      <MenuItem id="compact" labelText="Compact" />
+      <MenuItem id="comfortable" labelText="Comfortable" />
+      <MenuItem id="spacious" labelText="Spacious" />
+    </MenuItemRadioGroup>
+  </MenuButton>
+
+  <p>Density: {selectedId}</p>
+</Stack>
---
+++ docs/src/pages/framed/MenuButton/MenuButtonSelectable.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {
+    MenuButton,
+    MenuItem,
+    MenuItemGroup,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let selectedIds = ["name"];
+
+  $: visibleColumns = selectedIds.join(", ") || "None";
+</script>
+
+<Stack gap={5}>
+  <MenuButton labelText="View">
+    <MenuItemGroup labelText="Columns" bind:selectedIds>
+      <MenuItem id="name" labelText="Name" />
+      <MenuItem id="size" labelText="Size" />
+      <MenuItem id="modified" labelText="Last modified" />
+    </MenuItemGroup>
+  </MenuButton>
+
+  <p>Visible columns: {visibleColumns}</p>
+</Stack>
