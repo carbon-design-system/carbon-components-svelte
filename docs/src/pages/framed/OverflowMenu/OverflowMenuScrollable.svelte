3bcd328cae595869fa807c874597d350cff6242b
Eric Liu <ericyl.us@gmail.com>
2026-08-01 11:44:02 -0700

    docs(overflow-menu): add "Scrollable" example

---
+++ docs/src/pages/framed/OverflowMenu/OverflowMenuScrollable.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { OverflowMenu, OverflowMenuItem } from "carbon-components-svelte";
+
+  const workspaces = Array.from(
+    { length: 20 },
+    (_, index) => `Workspace ${index + 1}`,
+  );
+</script>
+
+<OverflowMenu maxHeight={240}>
+  {#each workspaces as workspace (workspace)}
+    <OverflowMenuItem text={workspace} />
+  {/each}
+  <OverflowMenuItem hasDivider danger text="Delete service" />
+</OverflowMenu>
