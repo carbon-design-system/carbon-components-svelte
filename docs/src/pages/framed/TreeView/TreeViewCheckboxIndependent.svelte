4564caae4d99344d3fb3e960f04a88a4452cd5cc
Eric Liu <ericyl.us@gmail.com>
2026-08-01 11:56:03 -0700

    docs(tree-view): document checkbox selection

--- docs/src/pages/framed/TreeView/TreeViewCheckboxIndependent.svelte
+++ docs/src/pages/framed/TreeView/TreeViewCheckboxIndependent.svelte
@@ -1,7 +1,7 @@
 <script>
   import { Stack, TreeView } from "carbon-components-svelte";
 
-  let checkedIds = [3];
+  let checkedIds = [1, 3];
   let indeterminateIds = [];
   let expandedIds = [1, 2, 7];
   let nodes = [
@@ -34,7 +34,8 @@
   <div>
     <TreeView
       selectionMode="checkbox"
-      labelText="Cloud Products"
+      checkMode="node"
+      labelText="Cloud Products (independent)"
       {nodes}
       bind:checkedIds
       bind:indeterminateIds
