25ea68cd83349b877dbac8684b15b5ab19562012
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:38:29 -0700

    docs(tabs): document `selectedId`

--- docs/src/pages/framed/Tabs/TabsDismissibleIcons.svelte
+++ docs/src/pages/framed/Tabs/TabsDismissibleIcons.svelte
@@ -4,7 +4,7 @@
   import Dashboard from "carbon-icons-svelte/lib/Dashboard.svelte";
   import Settings from "carbon-icons-svelte/lib/Settings.svelte";
 
-  let selected = 0;
+  let selectedId = "dashboard";
   let tabs = [
     { id: "dashboard", label: "Dashboard", icon: Dashboard },
     { id: "schedule", label: "Schedule", icon: Calendar },
@@ -13,16 +13,10 @@
 
   function handleDismiss({ detail }) {
     tabs = tabs.filter((tab) => tab.id !== detail.id);
-
-    if (selected > detail.index) {
-      selected -= 1;
-    } else if (selected >= tabs.length) {
-      selected = Math.max(tabs.length - 1, 0);
-    }
   }
 </script>
 
-<Tabs bind:selected dismissible on:dismiss={handleDismiss}>
+<Tabs bind:selectedId dismissible on:dismiss={handleDismiss}>
   {#each tabs as tab (tab.id)}
     <Tab
       id={tab.id}
