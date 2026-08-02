47e75710658504b9d68b9f845e93242e23c4cd76
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:37:11 -0700

    feat(tabs): add extra slot for tab bar actions

---
+++ tests/Tabs/TabsExtra.test.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Tab from "carbon-components-svelte/Tabs/Tab.svelte";
+  import TabContent from "carbon-components-svelte/Tabs/TabContent.svelte";
+  import Tabs from "carbon-components-svelte/Tabs/Tabs.svelte";
+
+  export let withExtra = true;
+</script>
+
+{#if withExtra}
+  <Tabs>
+    <Tab label="Tab 1" />
+    <Tab label="Tab 2" />
+    <Tab label="Tab 3" />
+    <button type="button" slot="extra">New tab</button>
+    <svelte:fragment slot="content">
+      <TabContent>Content 1</TabContent>
+      <TabContent>Content 2</TabContent>
+      <TabContent>Content 3</TabContent>
+    </svelte:fragment>
+  </Tabs>
+{:else}
+  <Tabs>
+    <Tab label="Tab 1" />
+    <Tab label="Tab 2" />
+    <Tab label="Tab 3" />
+    <svelte:fragment slot="content">
+      <TabContent>Content 1</TabContent>
+      <TabContent>Content 2</TabContent>
+      <TabContent>Content 3</TabContent>
+    </svelte:fragment>
+  </Tabs>
+{/if}
