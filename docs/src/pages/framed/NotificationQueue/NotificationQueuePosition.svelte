0f3c348b29a0cf01615b24c961a037c64bdbb719
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:38:07 -0700

    docs(notification): document queue placements

--- docs/src/pages/framed/NotificationQueue/NotificationQueuePosition.svelte
+++ docs/src/pages/framed/NotificationQueue/NotificationQueuePosition.svelte
@@ -5,17 +5,45 @@
     NotificationQueue,
   } from "carbon-components-svelte";
 
-  let topQueue;
-  let bottomQueue;
+  let topRightQueue;
+  let topCenterQueue;
+  let topLeftQueue;
+  let bottomRightQueue;
+  let bottomCenterQueue;
+  let bottomLeftQueue;
 </script>
 
-<NotificationQueue bind:this={topQueue} position="top-right" />
-<NotificationQueue bind:this={bottomQueue} position="bottom-right" />
+<NotificationQueue bind:this={topRightQueue} position="top-right" />
+<NotificationQueue bind:this={topCenterQueue} position="top-center" />
+<NotificationQueue bind:this={topLeftQueue} position="top-left" />
+<NotificationQueue bind:this={bottomRightQueue} position="bottom-right" />
+<NotificationQueue bind:this={bottomCenterQueue} position="bottom-center" />
+<NotificationQueue bind:this={bottomLeftQueue} position="bottom-left" />
 
 <ButtonSet>
   <Button
     on:click={() => {
-      topQueue.add({
+      topLeftQueue.add({
+        kind: "info",
+        title: "Top left",
+      });
+    }}
+  >
+    Add top left
+  </Button>
+  <Button
+    on:click={() => {
+      topCenterQueue.add({
+        kind: "success",
+        title: "Top center",
+      });
+    }}
+  >
+    Add top center
+  </Button>
+  <Button
+    on:click={() => {
+      topRightQueue.add({
         kind: "success",
         title: "Top right",
       });
@@ -23,9 +51,32 @@
   >
     Add top right
   </Button>
+</ButtonSet>
+<br>
+<ButtonSet>
   <Button
     on:click={() => {
-      bottomQueue.add({
+      bottomLeftQueue.add({
+        kind: "warning",
+        title: "Bottom left",
+      });
+    }}
+  >
+    Add bottom left
+  </Button>
+  <Button
+    on:click={() => {
+      bottomCenterQueue.add({
+        kind: "info",
+        title: "Bottom center",
+      });
+    }}
+  >
+    Add bottom center
+  </Button>
+  <Button
+    on:click={() => {
+      bottomRightQueue.add({
         kind: "info",
         title: "Bottom right",
       });
