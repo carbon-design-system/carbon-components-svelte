45710105be157c6b0869f99b833f0662da0ab223
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:58:28 -0700

    docs(menu): document maxHeight

---
+++ docs/src/pages/framed/Menu/MenuScrollable.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+
+  let anchor;
+  let open = false;
+
+  const actions = Array.from(
+    { length: 20 },
+    (_, index) => `Action ${index + 1}`,
+  );
+</script>
+
+<Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
+
+<Menu {anchor} bind:open labelText="Actions menu" maxHeight={240}>
+  {#each actions as action (action)}
+    <MenuItem on:click={() => console.log(action)}>{action}</MenuItem>
+  {/each}
+</Menu>
