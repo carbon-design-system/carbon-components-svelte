97cf97d65eddbba904542db92fa6c520e5e48e6c
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:30:33 -0700

    docs(modal): document non-dismissible close-button hiding

---
+++ docs/src/pages/framed/Modal/ModalHideCloseButton.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Button, Modal } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Review terms</Button>
+
+<Modal
+  bind:open
+  hideCloseButton
+  preventCloseOnClickOutside
+  modalHeading="Accept terms of use"
+  primaryButtonText="Accept"
+  secondaryButtonText="Decline"
+  on:click:button--secondary={() => (open = false)}
+  on:submit={() => (open = false)}
+>
+  <p>
+    You must accept or decline the terms. The header close button is hidden, so
+    use the footer actions to leave this dialog.
+  </p>
+</Modal>
