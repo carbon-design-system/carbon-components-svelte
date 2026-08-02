5cff76021bbc7ea2e3c0c207abfe4424aba72f29
Eric Liu <ericyl.us@gmail.com>
2026-08-01 20:12:58 -0700

    docs(code-snippet): document row count props

---
+++ docs/src/pages/framed/CodeSnippet/CodeSnippetRowCounts.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { CodeSnippet } from "carbon-components-svelte";
+
+  let code =
+    "export function add(a: number, b: number) {\n  return a + b;\n}\n\nexport function subtract(a: number, b: number) {\n  return a - b;\n}\n\nexport function multiply(a: number, b: number) {\n  return a * b;\n}\n\nexport function divide(a: number, b: number) {\n  return a / b;\n}\n\nexport function modulo(a: number, b: number) {\n  return a % b;\n}\n\nexport function power(a: number, b: number) {\n  return a ** b;\n}\n\nexport function negate(a: number) {\n  return -a;\n}";
+</script>
+
+<CodeSnippet type="multi" {code} maxCollapsedNumberOfRows={6} />
