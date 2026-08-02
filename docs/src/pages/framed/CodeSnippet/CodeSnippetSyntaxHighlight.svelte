b4f528cdc67989a307282a1446bbfa795d66457b
Eric Liu <ericyl.us@gmail.com>
2026-08-01 20:28:33 -0700

    docs(code-snippet): add syntax highlighting composition example

---
+++ docs/src/pages/framed/CodeSnippet/CodeSnippetSyntaxHighlight.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { CodeSnippet } from "carbon-components-svelte";
+  import Prism from "prismjs";
+  import "prismjs/components/prism-typescript";
+
+  const code = `export function add(a: number, b: number) {
+  return a + b;
+}
+
+export function subtract(a: number, b: number) {
+  return a - b;
+}`;
+
+  const highlighted = Prism.highlight(
+    code,
+    Prism.languages.typescript,
+    "typescript",
+  );
+</script>
+
+<CodeSnippet type="multi" {code}> {@html highlighted} </CodeSnippet>
