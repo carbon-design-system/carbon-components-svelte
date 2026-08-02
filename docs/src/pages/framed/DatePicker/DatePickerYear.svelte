efff3bea3e08b7b1f5871e086cfc3251297a1e7a
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:37:43 -0700

    docs(date-picker): add year examples

---
+++ docs/src/pages/framed/DatePicker/DatePickerYear.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="year" dateFormat="Y" on:change>
+  <DatePickerInput labelText="Fiscal year" placeholder="yyyy" />
+</DatePicker>
---
+++ docs/src/pages/framed/DatePicker/DatePickerYearProgrammatic.svelte
@@ -0,0 +1,39 @@
+<script>
+  import {
+    Button,
+    DatePicker,
+    DatePickerInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let value = "";
+  let calendar = null;
+
+  function selectCurrentYear() {
+    value = String(new Date().getFullYear());
+  }
+
+  function viewNextDecade() {
+    calendar?.changeYear(calendar.currentYear + 10);
+  }
+</script>
+
+<Stack gap={4}>
+  <DatePicker
+    datePickerType="year"
+    dateFormat="Y"
+    bind:value
+    bind:calendar
+    on:change
+  >
+    <DatePickerInput labelText="Fiscal year" placeholder="yyyy" />
+  </DatePicker>
+  <Stack gap={3} orientation="horizontal">
+    <Button kind="tertiary" on:click={selectCurrentYear}>
+      Select current year
+    </Button>
+    <Button kind="tertiary" on:click={viewNextDecade}>
+      View next decade
+    </Button>
+  </Stack>
+</Stack>
