95f022e8fc34493247f817b24b2e935b17b811f5
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:37:28 -0700

    docs(contained-list): document search slot

---
+++ docs/src/pages/framed/ContainedList/FilterableMembers.svelte
@@ -0,0 +1,31 @@
+<script>
+  import {
+    ContainedList,
+    ContainedListItem,
+    Search,
+  } from "carbon-components-svelte";
+
+  const members = [
+    "Amelia Chen",
+    "Diego Morales",
+    "Harper Patel",
+    "Jordan Lee",
+    "Maya Okonkwo",
+    "Noah Bergman",
+    "Priya Shah",
+    "Samir Haddad",
+  ];
+
+  let searchTerm = "";
+
+  $: filteredMembers = members.filter((member) =>
+    member.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+</script>
+
+<ContainedList labelText="Team members">
+  <Search slot="search" placeholder="Filter members" bind:value={searchTerm} />
+  {#each filteredMembers as member (member)}
+    <ContainedListItem>{member}</ContainedListItem>
+  {/each}
+</ContainedList>
