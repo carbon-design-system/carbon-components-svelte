7f2b462e293dc45c8ce099366732a512b14bc254
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:32:57 -0700

    docs(dialog): document close triggers

---
+++ docs/src/pages/framed/Dialog/DialogPreventOutsideClick.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Button, Dialog, Stack } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Button on:click={() => (open = true)}>Open dialog</Button>
+
+<Dialog
+  preventCloseOnClickOutside
+  bind:open
+  modal
+  aria-label="Prevent outside click example"
+>
+  <Stack gap={5}>
+    <p>
+      Clicking the backdrop will not close this dialog. Use
+      <kbd>Escape</kbd>
+      or the close button.
+    </p>
+    <form method="dialog">
+      <Button type="submit">Close</Button>
+    </form>
+  </Stack>
+</Dialog>
