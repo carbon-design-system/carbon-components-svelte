29431234a5529c0faf7c7b46a3a3ded3af54237a
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:37:08 -0700

    docs(notification): document promise helper

---
+++ docs/src/pages/framed/NotificationQueue/NotificationQueuePromise.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Button, NotificationQueue } from "carbon-components-svelte";
+
+  let queue;
+
+  async function save() {
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+  }
+</script>
+
+<NotificationQueue bind:this={queue} />
+
+<Button
+  on:click={() => {
+    queue.promise(save(), {
+      loading: "Saving...",
+      success: "Changes saved",
+      error: "Failed to save",
+    });
+  }}
+>
+  Save
+</Button>
