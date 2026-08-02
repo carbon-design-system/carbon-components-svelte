5c90f6445aee6fccc4ad0b54d379a5b87930e4d8
Eric Liu <ericyl.us@gmail.com>
2026-08-01 19:32:54 -0700

    docs(menu): document shortcut hints

---
+++ docs/src/pages/framed/Menu/MenuItemShortcuts.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
+  import Cut from "carbon-icons-svelte/lib/Cut.svelte";
+  import Paste from "carbon-icons-svelte/lib/Paste.svelte";
+
+  let anchor;
+  let open = false;
+</script>
+
+<Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
+
+<Menu {anchor} bind:open labelText="Actions menu">
+  <MenuItem icon={Cut} shortcutText="⌘X" on:click={() => console.log("Cut")}>
+    Cut
+  </MenuItem>
+  <MenuItem icon={Copy} shortcutText="⌘C" on:click={() => console.log("Copy")}>
+    Copy
+  </MenuItem>
+  <MenuItem
+    icon={Paste}
+    shortcutText="⌘V"
+    on:click={() => console.log("Paste")}
+  >
+    Paste
+  </MenuItem>
+</Menu>
