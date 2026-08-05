d5a61e8ba87b0bd284866e8f90ddc23fe47fe2cd
Eric Liu <ericyl.us@gmail.com>
2026-08-04 20:32:16 -0700

    fix(tag)!: remove forwarded `on:click` event from non-interactive TagSkeleton
    Same fix as Tag.svelte in #3540, deliberately deferred at the time to
    keep that PR's diff scoped.

---
+++ tests/Tag/TagSkeleton.test.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import TagSkeleton from "carbon-components-svelte/Tag/TagSkeleton.svelte";
+  import type { ComponentProps } from "svelte";
+
+  export let size: ComponentProps<TagSkeleton>["size"] = "default";
+</script>
+
+<TagSkeleton
+  {size}
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+/>
