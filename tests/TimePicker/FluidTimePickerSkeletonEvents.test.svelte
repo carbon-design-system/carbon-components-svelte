acfdc43f30fe561c14d016fa3bf793ceafe15b3d
Eric Liu <ericyl.us@gmail.com>
2026-08-04 20:32:16 -0700

    fix(time-picker)!: remove forwarded `on:click` event from non-interactive FluidTimePickerSkeleton

---
+++ tests/TimePicker/FluidTimePickerSkeletonEvents.test.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import FluidTimePickerSkeleton from "carbon-components-svelte/TimePicker/FluidTimePickerSkeleton.svelte";
+
+  export let isOnlyTwo = false;
+</script>
+
+<FluidTimePickerSkeleton
+  data-testid="skeleton"
+  {isOnlyTwo}
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+/>
