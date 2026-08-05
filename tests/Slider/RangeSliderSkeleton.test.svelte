5cf7962e178a2ee1bfe9a98cbfca2a29c9a27401
Eric Liu <ericyl.us@gmail.com>
2026-08-04 20:32:05 -0700

    fix(range-slider)!: remove forwarded `on:click` event from non-interactive RangeSliderSkeleton

---
+++ tests/Slider/RangeSliderSkeleton.test.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import RangeSliderSkeleton from "carbon-components-svelte/Slider/RangeSliderSkeleton.svelte";
+</script>
+
+<RangeSliderSkeleton
+  data-testid="range-slider-skeleton"
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+/>
