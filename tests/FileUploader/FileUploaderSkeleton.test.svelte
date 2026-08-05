f2b50488159b74c27b274b4441ae9f42a1959696
Eric Liu <ericyl.us@gmail.com>
2026-08-04 20:31:53 -0700

    fix(file-uploader)!: remove forwarded `on:click` event from non-interactive FileUploaderSkeleton

---
+++ tests/FileUploader/FileUploaderSkeleton.test.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import FileUploaderSkeleton from "carbon-components-svelte/FileUploader/FileUploaderSkeleton.svelte";
+</script>
+
+<FileUploaderSkeleton
+  data-testid="file-uploader-skeleton"
+  {...$$props}
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+/>
