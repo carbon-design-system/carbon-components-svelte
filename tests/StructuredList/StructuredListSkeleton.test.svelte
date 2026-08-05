c66c947b75e70064109378487c83aad80cdbd218
Eric Liu <ericyl.us@gmail.com>
2026-08-04 20:32:15 -0700

    fix(structured-list)!: remove forwarded `on:click` event from non-interactive StructuredListSkeleton

--- tests/StructuredList/StructuredListSkeleton.test.svelte
+++ tests/StructuredList/StructuredListSkeleton.test.svelte
@@ -2,4 +2,19 @@
   import StructuredListSkeleton from "carbon-components-svelte/StructuredList/StructuredListSkeleton.svelte";
 </script>
 
-<StructuredListSkeleton {...$$props} />
+<StructuredListSkeleton
+  data-testid="structured-list-skeleton"
+  on:click={() => {
+    console.log("click");
+  }}
+  on:mouseover={() => {
+    console.log("mouseover");
+  }}
+  on:mouseenter={() => {
+    console.log("mouseenter");
+  }}
+  on:mouseleave={() => {
+    console.log("mouseleave");
+  }}
+  {...$$props}
+/>
