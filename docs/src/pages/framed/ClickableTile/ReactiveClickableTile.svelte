b94a35b363edd4171b7b8fb9f9e14352d10e6c80
Eric Liu <ericyl.us@gmail.com>
2026-08-07 17:06:02 -0700

    docs(clickable-tile): add reactive clicked example (#3605)
    The bindable, readonly clicked prop was listed in the API table but
    never demonstrated. Add a framed example that binds it and verified
    with a scripted click that the bound value flips from false to true.

---
+++ docs/src/pages/framed/ClickableTile/ReactiveClickableTile.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { ClickableTile, Stack } from "carbon-components-svelte";
+
+  let clicked = false;
+</script>
+
+<Stack gap={5}>
+  <div>Clicked: <strong>{clicked}</strong></div>
+  <ClickableTile bind:clicked>Click this tile</ClickableTile>
+</Stack>
