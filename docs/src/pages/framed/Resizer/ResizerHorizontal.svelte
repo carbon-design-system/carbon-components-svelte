d24a08992dd2a2573b115568515eadbdbbdadc76
Eric Liu <ericyl.us@gmail.com>
2026-08-17 06:20:23 -0700

    docs(resizer): add examples

---
+++ docs/src/pages/framed/Resizer/ResizerHorizontal.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Resizer } from "carbon-components-svelte";
+</script>
+
+<div
+  style="display: flex; flex-direction: column; width: 300px; border: 1px solid var(--cds-border-subtle);"
+>
+  <div style="height: 120px; padding: 1rem; background: var(--cds-layer);">
+    Top panel
+  </div>
+  <Resizer orientation="horizontal" />
+  <div style="flex: 1; padding: 1rem;">Bottom panel</div>
+</div>
