3a217098d6709a9bdccf46d56df98d22a9e98208
Eric Liu <ericyl.us@gmail.com>
2026-08-19 15:44:34 -0700

    docs(combo-button): re-organize sections and fill in gaps
    Group Trigger and Events sections. Add a reactive bind:open example
    (new framed file), the labelChildren slot, trigger event coverage,
    and the close event's trigger detail shape.

---
+++ docs/src/pages/framed/ComboButton/ComboButtonReactive.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {
+    Button,
+    ComboButton,
+    MenuItem,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Stack gap={6} align="start">
+  <ComboButton labelText="Save" bind:open>
+    <MenuItem on:click={() => console.log("Save as")}>Save as</MenuItem>
+    <MenuItem on:click={() => console.log("Save a copy")}>Save a copy</MenuItem>
+  </ComboButton>
+  <Button kind="tertiary" size="small" on:click={() => (open = !open)}>
+    {open ? "Close menu" : "Open menu"}
+  </Button>
+</Stack>
