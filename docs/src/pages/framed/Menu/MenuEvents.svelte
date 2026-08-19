6fc096a316b7cecef2539eced0564302c6cf552e
Eric Liu <ericyl.us@gmail.com>
2026-08-19 16:09:18 -0700

    docs(menu): re-organize sections and fill in gaps
    Add size="xs", MenuItemGroup (Selectable) and MenuItemRadioGroup
    (Radio group) coverage with new framed examples, the labelChildren
    slot on nested MenuItem, and an Events section (open/close, new
    framed example). Register MenuItemGroup/MenuItemRadioGroup in the
    page's component API list. Verified outbound anchors into
    MenuButton.svx still resolve.

---
+++ docs/src/pages/framed/Menu/MenuEvents.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+
+  let anchor;
+  let open = false;
+</script>
+
+<Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
+
+<Menu
+  {anchor}
+  bind:open
+  labelText="Actions menu"
+  on:open={(e) => console.log("open", e.detail)}
+  on:close={(e) => console.log("close", e.detail.trigger)}
+>
+  <MenuItem on:click={() => console.log("Cut")}>Cut</MenuItem>
+  <MenuItem on:click={() => console.log("Copy")}>Copy</MenuItem>
+  <MenuItem on:click={() => console.log("Paste")}>Paste</MenuItem>
+</Menu>
