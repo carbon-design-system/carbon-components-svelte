d0fd6aacd56fb93e430e182913e7c973b1eadaff
Eric Liu <ericyl.us@gmail.com>
2026-08-21 16:29:03 -0700

    docs(ui-shell): add "Fixed rail" example

---
+++ docs/src/pages/framed/UIShell/HeaderNavRailFixed.svelte
@@ -0,0 +1,67 @@
+<script>
+  import {
+    Column,
+    Content,
+    Grid,
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    Row,
+    SideNav,
+    SideNavDivider,
+    SideNavItems,
+    SideNavLink,
+    SideNavMenu,
+    SideNavMenuItem,
+    SkipToContent,
+  } from "carbon-components-svelte";
+  import Activity from "carbon-icons-svelte/lib/Activity.svelte";
+  import Dashboard from "carbon-icons-svelte/lib/Dashboard.svelte";
+  import Kubernetes from "carbon-icons-svelte/lib/Kubernetes.svelte";
+  import ListBoxes from "carbon-icons-svelte/lib/ListBoxes.svelte";
+  import Settings from "carbon-icons-svelte/lib/Settings.svelte";
+</script>
+
+<Header companyName="IBM" platformName="Cloud">
+  <svelte:fragment slot="skipToContent"> <SkipToContent /> </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/catalog" text="Catalog" />
+    <HeaderNavItem href="/docs" text="Docs" />
+    <HeaderNavItem href="/support" text="Support" />
+    <HeaderNavMenu text="Manage">
+      <HeaderNavItem href="/account" text="Account" />
+      <HeaderNavItem href="/iam" text="Access (IAM)" />
+      <HeaderNavItem href="/billing" text="Billing and usage" />
+    </HeaderNavMenu>
+    <HeaderNavItem href="/status" text="Status" />
+  </HeaderNav>
+</Header>
+
+<SideNav rail fixed>
+  <SideNavItems>
+    <SideNavLink
+      icon={Dashboard}
+      text="Dashboard"
+      href="/dashboard"
+      isSelected
+    />
+    <SideNavLink icon={ListBoxes} text="Resource list" href="/resources" />
+    <SideNavLink icon={Activity} text="Activity tracker" href="/activity" />
+    <SideNavMenu icon={Kubernetes} text="Kubernetes">
+      <SideNavMenuItem href="/kubernetes/clusters" text="Clusters" />
+      <SideNavMenuItem href="/kubernetes/worker-pools" text="Worker pools" />
+      <SideNavMenuItem href="/kubernetes/registry" text="Container registry" />
+    </SideNavMenu>
+    <SideNavDivider />
+    <SideNavLink icon={Settings} text="Account settings" href="/settings" />
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column> <h1>Dashboard</h1> </Column>
+    </Row>
+  </Grid>
+</Content>
