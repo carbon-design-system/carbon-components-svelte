97236bbe894256ca6e4e65b8937b0925bc639ac0
Eric Liu <ericyl.us@gmail.com>
2026-08-22 15:36:30 -0700

    docs(pagination): add "Custom page select" example

---
+++ docs/src/pages/framed/Pagination/PaginationPageSelect.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { NumberInput, Pagination } from "carbon-components-svelte";
+
+  let page = 1;
+</script>
+
+<Pagination bind:page totalItems={102} pageSizes={[10, 15, 20]}>
+  <svelte:fragment slot="pageSelect" let:totalPages let:selectLabelText>
+    <NumberInput
+      size="sm"
+      hideLabel
+      hideSteppers
+      labelText={selectLabelText}
+      min={1}
+      max={totalPages}
+      bind:value={page}
+    />
+  </svelte:fragment>
+</Pagination>
