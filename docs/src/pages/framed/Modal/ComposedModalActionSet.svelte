0f0cf233005cdc4d4c8027def3bd765b3ed9e88d
Eric Liu <ericyl.us@gmail.com>
2026-08-30 15:59:31 -0700

    docs(composed-modal): show ActionSet in the footer slot

---
+++ docs/src/pages/framed/Modal/ComposedModalActionSet.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {
+    ActionSet,
+    Button,
+    ComposedModal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+  } from "carbon-components-svelte";
+
+  let open = true;
+</script>
+
+<Button on:click={() => (open = true)}>Continue setup</Button>
+
+<ComposedModal bind:open>
+  <ModalHeader title="Continue setup" />
+  <ModalBody>
+    <p>Review this step, go back, or continue to the next one.</p>
+  </ModalBody>
+  <ModalFooter>
+    <ActionSet size="lg" disableStacking>
+      <Button kind="ghost" on:click={() => (open = false)}>Cancel</Button>
+      <Button kind="secondary">Previous</Button>
+      <Button on:click={() => (open = false)}>Next</Button>
+    </ActionSet>
+  </ModalFooter>
+</ComposedModal>
