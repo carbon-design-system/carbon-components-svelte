b013188a53a4b0019d73cf32dadc9f2c63ff76fa
Eric Liu <ericyl.us@gmail.com>
2026-08-30 12:14:06 -0700

    docs(date-picker): add "Locale with year-first order" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerYearFirstLocale.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+  import { Japanese } from "flatpickr/dist/l10n/ja";
+</script>
+
+<DatePicker datePickerType="single" locale={Japanese} on:change>
+  <DatePickerInput labelText="日付" placeholder="yyyy/mm/dd" />
+</DatePicker>
