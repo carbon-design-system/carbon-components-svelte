ba33314ddc06bde7954b9f84ad3d8aa3247a7d1d
Eric Liu <ericyl.us@gmail.com>
2026-09-04 20:09:17 -0700

    docs(404): apply `FullPageError`

--- docs/src/pages/[...404].svelte
+++ docs/src/pages/[...404].svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-  import { Column, Content, Grid, Link, Row } from "carbon-components-svelte";
+  import { Content, FullPageError, Link } from "carbon-components-svelte";
 </script>
 
 <svelte:head> <title>404</title> </svelte:head>
 
 <Content style="min-height: calc(100vh - 6rem - 1px);">
-  <Grid>
-    <Row>
-      <Column>
-        <h1>404</h1>
-        <div>
-          Page not found.
-          <Link href="/">Return home</Link>
-        </div>
-      </Column>
-    </Row>
-  </Grid>
+  <FullPageError kind="404" labelText="Error 404" title="Page not found">
+    <Link size="lg" href="/">Return home</Link>
+  </FullPageError>
 </Content>
