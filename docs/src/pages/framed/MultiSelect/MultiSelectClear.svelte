2808464034b8be1c784c24525ae76a73e4cb641f
Eric Liu <ericyl.us@gmail.com>
2026-09-10 14:48:01 -0700

    docs(multi-select): document clear() and openOnClear

---
+++ docs/src/pages/framed/MultiSelect/MultiSelectClear.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Button, MultiSelect } from "carbon-components-svelte";
+
+  let ref;
+</script>
+
+<MultiSelect
+  labelText="Contact"
+  label="Select contact methods..."
+  selectedIds={["0", "1"]}
+  bind:this={ref}
+  items={[
+    { id: "0", text: "Slack" },
+    { id: "1", text: "Email" },
+    { id: "2", text: "Fax" },
+  ]}
+/>
+<br>
+<Button on:click={() => ref.clear()}>Clear</Button>
+<Button on:click={() => ref.clear({ focus: false })}>Clear (no focus)</Button>
+<Button on:click={() => ref.clear({ open: true })}>Clear (reopen menu)</Button>
