be5a87c881fc228dc3d1b661de3cefcab4bcefd6
Eric Liu <ericyl.us@gmail.com>
2026-09-13 16:59:30 -0700

    docs(relative-time): add examples

---
+++ docs/src/pages/framed/RelativeTime/RelativeTimeLive.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Button, RelativeTime, Stack } from "carbon-components-svelte";
+
+  let savedAt = Date.now();
+
+  function handleSave() {
+    savedAt = Date.now();
+  }
+</script>
+
+<Stack gap={5}>
+  <Button on:click={handleSave}>Save</Button>
+  <RelativeTime date={savedAt} let:formatted>Saved {formatted}</RelativeTime>
+</Stack>
