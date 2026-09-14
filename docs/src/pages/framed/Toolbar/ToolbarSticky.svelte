619d0396daa1469d9553ddf151d3ebe731f964fd
Eric Liu <ericyl.us@gmail.com>
2026-09-14 05:16:24 -0700

    docs(toolbar): document debounce and sticky

---
+++ docs/src/pages/framed/Toolbar/ToolbarSticky.svelte
@@ -0,0 +1,49 @@
+<script>
+  import {
+    Button,
+    DataTable,
+    Toolbar,
+    ToolbarBatchActions,
+    ToolbarContent,
+    ToolbarSearch,
+  } from "carbon-components-svelte";
+  import TrashCan from "carbon-icons-svelte/lib/TrashCan.svelte";
+
+  let rows = Array.from({ length: 40 }).map((_, i) => ({
+    id: i,
+    name: `Load Balancer ${i + 1}`,
+    protocol: "HTTP",
+    port: 3000 + i * 10,
+    rule: i % 2 ? "Round robin" : "DNS delegation",
+  }));
+
+  let selectedRowIds = [];
+</script>
+
+<DataTable
+  batchSelection
+  bind:selectedRowIds
+  headers={[
+    { key: "name", value: "Name" },
+    { key: "protocol", value: "Protocol" },
+    { key: "port", value: "Port" },
+    { key: "rule", value: "Rule" },
+  ]}
+  {rows}
+>
+  <Toolbar sticky>
+    <ToolbarBatchActions>
+      <Button
+        icon={TrashCan}
+        on:click={() => {
+          rows = rows.filter((row) => !selectedRowIds.includes(row.id));
+        }}
+      >
+        Delete
+      </Button>
+    </ToolbarBatchActions>
+    <ToolbarContent>
+      <ToolbarSearch persistent shouldFilterRows />
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
