4bdae6b820d4ef2e9cd494357b36674d34f72387
Eric Liu <ericyl.us@gmail.com>
2026-09-14 18:01:43 -0700

    docs(data-table): document ToolbarColumnVisibility

---
+++ docs/src/pages/framed/DataTable/DataTableToolbarColumnVisibility.svelte
@@ -0,0 +1,56 @@
+<script>
+  import {
+    DataTable,
+    Toolbar,
+    ToolbarColumnVisibility,
+    ToolbarContent,
+  } from "carbon-components-svelte";
+
+  let headers = [
+    { key: "name", value: "Name" },
+    { key: "protocol", value: "Protocol" },
+    { key: "port", value: "Port" },
+    { key: "rule", value: "Rule" },
+    { key: "overflow", empty: true },
+  ];
+
+  const rows = [
+    {
+      id: "a",
+      name: "Load Balancer 3",
+      protocol: "HTTP",
+      port: 3000,
+      rule: "Round robin",
+    },
+    {
+      id: "b",
+      name: "Load Balancer 1",
+      protocol: "HTTP",
+      port: 443,
+      rule: "Round robin",
+    },
+    {
+      id: "c",
+      name: "Load Balancer 2",
+      protocol: "HTTP",
+      port: 80,
+      rule: "DNS delegation",
+    },
+  ];
+</script>
+
+<DataTable
+  title="Load balancers"
+  description="Your organization's active load balancers."
+  {headers}
+  {rows}
+>
+  <Toolbar>
+    <ToolbarContent>
+      <ToolbarColumnVisibility
+        bind:headers
+        toggleableKeys={["protocol", "port", "rule"]}
+      />
+    </ToolbarContent>
+  </Toolbar>
+</DataTable>
