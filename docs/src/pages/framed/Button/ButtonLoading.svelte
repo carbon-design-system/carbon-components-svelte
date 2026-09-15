ee5a99c484023876a4ab5e1bf9965c642cd10ab4
Eric Liu <ericyl.us@gmail.com>
2026-09-14 18:05:04 -0700

    docs(button): add "Loading" examples

---
+++ docs/src/pages/framed/Button/ButtonLoading.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Button, Stack } from "carbon-components-svelte";
+  import Add from "carbon-icons-svelte/lib/Add.svelte";
+
+  let submitting = false;
+  let adding = false;
+
+  async function simulate(duration = 2000) {
+    await new Promise((resolve) => setTimeout(resolve, duration));
+  }
+
+  async function onSubmit() {
+    submitting = true;
+    await simulate();
+    submitting = false;
+  }
+
+  async function onAdd() {
+    adding = true;
+    await simulate();
+    adding = false;
+  }
+</script>
+
+<Stack orientation="horizontal" gap={5}>
+  <Button loading={submitting} on:click={onSubmit}>Submit</Button>
+  <Button
+    loading={adding}
+    loadingDescription="Adding item"
+    iconDescription="Add item"
+    icon={Add}
+    on:click={onAdd}
+  />
+</Stack>
