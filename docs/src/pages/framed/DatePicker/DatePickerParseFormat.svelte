962312469aa40be1888659ac53510cf7be33a018
Eric Liu <ericyl.us@gmail.com>
2026-09-19 06:33:52 -0700

    docs(date-picker): document more flatpickrProps options

---
+++ docs/src/pages/framed/DatePicker/DatePickerParseFormat.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const flatpickrProps = {
+    formatDate: (date) => formatter.format(date),
+    // Must accept whatever `formatDate` returns.
+    parseDate: (dateStr) => new Date(dateStr),
+  };
+</script>
+
+<DatePicker datePickerType="single" {flatpickrProps} on:change>
+  <DatePickerInput labelText="Meeting date" placeholder="Select a date" />
+</DatePicker>
