6117dd28f716200c6f6c4d830409f9ffe3eed9a8
Eric Liu <ericyl.us@gmail.com>
2026-09-19 10:16:33 -0700

    docs(date-picker): use `disabledDates` and `enabledDates` props

--- docs/src/pages/framed/DatePicker/DatePickerEnableDates.svelte
+++ docs/src/pages/framed/DatePicker/DatePickerEnableDates.svelte
@@ -2,9 +2,9 @@
   import { DatePicker, DatePickerInput } from "carbon-components-svelte";
 
   // Deliveries only run on Monday, Wednesday, and Friday.
-  const enable = [(date) => [1, 3, 5].includes(date.getDay())];
+  const enabledDates = [(date) => [1, 3, 5].includes(date.getDay())];
 </script>
 
-<DatePicker datePickerType="single" flatpickrProps={{ enable }} on:change>
+<DatePicker datePickerType="single" {enabledDates} on:change>
   <DatePickerInput labelText="Delivery date" placeholder="mm/dd/yyyy" />
 </DatePicker>
