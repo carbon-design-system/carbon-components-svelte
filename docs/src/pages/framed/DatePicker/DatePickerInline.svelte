6e3e63516b9b034e972ba08566f1a45a8dd68095
Eric Liu <ericyl.us@gmail.com>
2026-09-19 07:03:05 -0700

    docs(date-picker): add "Inline calendar" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerInline.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker datePickerType="single" flatpickrProps={{ inline: true }} on:change>
+  <DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
+</DatePicker>
