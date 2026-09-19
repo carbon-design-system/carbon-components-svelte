08d6a19160baa3257e73abfb034cc9f634caab05
Eric Liu <ericyl.us@gmail.com>
2026-09-19 06:50:44 -0700

    docs(date-picker): add "Default date" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerDefaultDate.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<DatePicker
+  datePickerType="single"
+  bind:value
+  flatpickrProps={{ defaultDate: "today" }}
+>
+  <DatePickerInput
+    labelText="Start date"
+    placeholder="mm/dd/yyyy"
+    helperText="Value: {value || 'none'}"
+  />
+</DatePicker>
