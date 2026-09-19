4d8a53039d4c0a032f0015ce0584910c59d40717
Eric Liu <ericyl.us@gmail.com>
2026-09-18 19:47:30 -0700

    docs(date-picker): add "Multiple" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerMultiple.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+
+  let selectedCount = 0;
+
+  function handleChange(event) {
+    selectedCount = event.detail.selectedDates.length;
+  }
+</script>
+
+<DatePicker datePickerType="multiple" on:change={handleChange}>
+  <DatePickerInput
+    labelText="Blackout dates"
+    placeholder="mm/dd/yyyy"
+    helperText={`${selectedCount} date${selectedCount === 1 ? "" : "s"} selected`}
+  />
+</DatePicker>
