4bc4ca71dba028208a417a6bde76312996d0f55d
Eric Liu <ericyl.us@gmail.com>
2026-09-19 07:05:55 -0700

    docs(date-picker): add "Abbreviated month label" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerShorthandMonth.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+</script>
+
+<DatePicker
+  datePickerType="single"
+  flatpickrProps={{ shorthandCurrentMonth: true }}
+  on:change
+>
+  <DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
+</DatePicker>
