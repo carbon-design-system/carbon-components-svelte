bb58570dc5df45f256d5eca6025eacf12e96596d
Eric Liu <ericyl.us@gmail.com>
2026-09-19 06:35:11 -0700

    docs(date-picker): add "Multiple date separator" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerConjunction.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<DatePicker
+  datePickerType="multiple"
+  bind:value
+  flatpickrProps={{ conjunction: "; " }}
+>
+  <DatePickerInput labelText="Blackout dates" placeholder="mm/dd/yyyy" />
+</DatePicker>
+
+<p style:margin-top="1rem">Value: {value || "none"}</p>
