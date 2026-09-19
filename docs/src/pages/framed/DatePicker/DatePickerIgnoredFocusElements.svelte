1f259c8107850933e45cd7258335fc88bab473d8
Eric Liu <ericyl.us@gmail.com>
2026-09-19 07:05:26 -0700

    docs(date-picker): add "Keep open for outside controls" example

---
+++ docs/src/pages/framed/DatePicker/DatePickerIgnoredFocusElements.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {
+    Button,
+    DatePicker,
+    DatePickerInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let calendar = null;
+  let todayButton = null;
+</script>
+
+<Stack gap={4}>
+  <DatePicker
+    datePickerType="single"
+    bind:calendar
+    flatpickrProps={{
+      closeOnSelect: false,
+      ignoredFocusElements: todayButton ? [todayButton] : [],
+    }}
+    on:change
+  >
+    <DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
+  </DatePicker>
+  <Button
+    kind="tertiary"
+    bind:ref={todayButton}
+    on:click={() => calendar?.setDate(new Date(), true)}
+  >
+    Jump to today
+  </Button>
+</Stack>
