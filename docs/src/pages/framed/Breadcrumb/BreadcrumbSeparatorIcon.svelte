5daa480588236e6ff2b583f6566bff264c2fd61b
Eric Liu <ericyl.us@gmail.com>
2026-09-19 21:31:54 -0700

    docs(breadcrumb): add "Separator" example

---
+++ docs/src/pages/framed/Breadcrumb/BreadcrumbSeparatorIcon.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+  import ChevronRight from "carbon-icons-svelte/lib/ChevronRight.svelte";
+</script>
+
+<Breadcrumb separator={ChevronRight} noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/reports">Reports</BreadcrumbItem>
+  <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+</Breadcrumb>
+
+<Breadcrumb separator={ChevronRight} size="sm" noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/reports">Reports</BreadcrumbItem>
+  <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+</Breadcrumb>
