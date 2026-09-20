d5d5a36c8f5487b2fde8f81b4ed9d25a6dfce540
Eric Liu <ericyl.us@gmail.com>
2026-09-20 13:45:59 -0700

    feat(viz-chart): add ChartRuler and ChartTooltip
    The tooltip is an overlay inside the plot, hidden from assistive
    technology because the chart's live region announces the focused point.
    It stays open while hovered and closes on Escape.

--- src/viz/Chart/index.js
+++ src/viz/Chart/index.js
@@ -2,4 +2,7 @@ export { default as Chart } from "./Chart.svelte";
 export { default as ChartAxis } from "./ChartAxis.svelte";
 export { default as ChartGrid } from "./ChartGrid.svelte";
 export { default as ChartLine } from "./ChartLine.svelte";
+export { default as ChartRuler } from "./ChartRuler.svelte";
+export { default as ChartTooltip } from "./ChartTooltip.svelte";
+export { default as ChartTooltipRow } from "./ChartTooltipRow.svelte";
 export { CHART_CONTEXT } from "./context.js";
