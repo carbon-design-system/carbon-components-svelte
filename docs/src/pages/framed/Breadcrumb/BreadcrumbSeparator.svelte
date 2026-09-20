4137858bc332e61687df7fbc9bebd1c797f19714
Eric Liu <ericyl.us@gmail.com>
2026-09-19 21:31:54 -0700

    docs(breadcrumb): add "Separator" example

---
+++ docs/src/pages/framed/Breadcrumb/BreadcrumbSeparator.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Breadcrumb, BreadcrumbItem } from "carbon-components-svelte";
+</script>
+
+<Breadcrumb separator="›" noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/reports">Reports</BreadcrumbItem>
+  <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+</Breadcrumb>
+
+<Breadcrumb separator="→" noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/reports">Reports</BreadcrumbItem>
+  <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+</Breadcrumb>
+
+<Breadcrumb separator="·" noTrailingSlash>
+  <BreadcrumbItem href="/">Home</BreadcrumbItem>
+  <BreadcrumbItem href="/reports">Reports</BreadcrumbItem>
+  <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+</Breadcrumb>
