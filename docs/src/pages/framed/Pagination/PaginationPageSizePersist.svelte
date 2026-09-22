0e53a9f966806f8443c1ed30610ac413fb79d361
Eric Liu <ericyl.us@gmail.com>
2026-09-21 17:00:38 -0700

    docs(pagination): remember the page size with LocalStorage
    The page size reset on reload. The example binds it to LocalStorage.

---
+++ docs/src/pages/framed/Pagination/PaginationPageSizePersist.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { LocalStorage, Pagination } from "carbon-components-svelte";
+
+  let pageSize = 10;
+</script>
+
+<LocalStorage key="pagination-page-size" bind:value={pageSize} />
+
+<Pagination totalItems={102} pageSizes={[10, 20, 50]} bind:pageSize />
