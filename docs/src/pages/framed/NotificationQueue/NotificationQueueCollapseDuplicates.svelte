81b9895feae9dbb7c81793bc9f6f0fa5aef2b5af
Eric Liu <ericyl.us@gmail.com>
2026-09-23 03:24:19 -0700

    docs(notification): document collapseDuplicates

---
+++ docs/src/pages/framed/NotificationQueue/NotificationQueueCollapseDuplicates.svelte
@@ -0,0 +1,38 @@
+<script>
+  import {
+    Button,
+    ButtonSet,
+    NotificationQueue,
+  } from "carbon-components-svelte";
+
+  let queue;
+</script>
+
+<NotificationQueue bind:this={queue} collapseDuplicates />
+
+<ButtonSet>
+  <Button
+    on:click={() => {
+      queue.add({
+        kind: "error",
+        title: "Connection lost",
+        subtitle: "Retrying in the background.",
+        timeout: 5_000,
+      });
+    }}
+  >
+    Add duplicate
+  </Button>
+  <Button
+    kind="secondary"
+    on:click={() => {
+      queue.add({
+        kind: "success",
+        title: "Connection restored",
+        timeout: 5_000,
+      });
+    }}
+  >
+    Add different
+  </Button>
+</ButtonSet>
