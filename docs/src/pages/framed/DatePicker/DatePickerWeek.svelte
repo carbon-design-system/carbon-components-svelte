0f2b53a25ef7afbef8b8ec1272d4ba8456739a2e
Eric Liu <ericyl.us@gmail.com>
2026-09-24 15:00:29 -0700

    docs(date-picker): show the week date picker

---
+++ docs/src/pages/framed/DatePicker/DatePickerWeek.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+
+  let value = "";
+</script>
+
+<DatePicker
+  datePickerType="week"
+  displayFormat={"\\W\\e\\e\\k W, Y"}
+  bind:value
+  on:change
+>
+  <DatePickerInput labelText="Sprint week" placeholder="Pick a week" />
+</DatePicker>
+
+<p style="margin-top: 1rem">value: {value || "none"}</p>
