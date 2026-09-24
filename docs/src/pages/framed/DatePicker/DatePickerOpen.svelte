8ed508acb8a28b53f8adff97d42ef14480d52c0e
Eric Liu <ericyl.us@gmail.com>
2026-09-24 15:00:28 -0700

    docs(date-picker): open the calendar with bind:open

---
+++ docs/src/pages/framed/DatePicker/DatePickerOpen.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {
+    Button,
+    DatePicker,
+    DatePickerInput,
+    Stack,
+  } from "carbon-components-svelte";
+
+  let open = false;
+</script>
+
+<Stack gap={5}>
+  <div>
+    <Button kind="tertiary" size="small" on:click={() => (open = true)}>
+      Open calendar
+    </Button>
+  </div>
+  <DatePicker datePickerType="single" bind:open>
+    <DatePickerInput
+      labelText="Follow-up date (open: {open})"
+      placeholder="mm/dd/yyyy"
+    />
+  </DatePicker>
+</Stack>
