cd28ba097fa210313925e731da0bdd6c09f66b2f
Eric Liu <ericyl.us@gmail.com>
2026-09-25 15:02:46 -0700

    docs(toggle): add "Warning" example

---
+++ docs/src/pages/framed/Toggle/ToggleWarning.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Toggle } from "carbon-components-svelte";
+
+  let toggled = true;
+</script>
+
+<Toggle
+  labelText="Public link sharing"
+  bind:toggled
+  warn={toggled}
+  warnText="Anyone with the link can view this workspace."
+/>
