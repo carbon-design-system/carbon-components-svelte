6538423d0c15584095bce09497eb176d710951ed
Eric Liu <ericyl.us@gmail.com>
2026-09-25 04:33:40 -0700

    docs(grid): add a subgrid section to the CssGrid page

---
+++ docs/src/pages/framed/CssGrid/CssSubgrid.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { CssColumn, CssGrid } from "carbon-components-svelte";
+</script>
+
+<CssGrid withRowGap>
+  <CssColumn sm={4} md={8} lg={4}>Outer column</CssColumn>
+  <CssColumn sm={4} md={8} lg={12}>
+    <CssGrid withRowGap>
+      <CssColumn sm={2} md={4} lg={4}>Subgrid column</CssColumn>
+      <CssColumn sm={2} md={4} lg={4}>Subgrid column</CssColumn>
+      <CssColumn sm={2} md={4} lg={4}>Subgrid column</CssColumn>
+    </CssGrid>
+  </CssColumn>
+  <CssColumn sm={2} md={4} lg={4}>Outer column</CssColumn>
+  <CssColumn sm={2} md={4} lg={4}>Outer column</CssColumn>
+  <CssColumn sm={2} md={4} lg={4}>Outer column</CssColumn>
+  <CssColumn sm={2} md={4} lg={4}>Outer column</CssColumn>
+</CssGrid>
---
+++ docs/src/pages/framed/CssGrid/CssSubgridModes.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { CssColumn, CssGrid } from "carbon-components-svelte";
+</script>
+
+<CssGrid withRowGap>
+  <CssColumn sm={4} md={8} lg={16}>
+    <CssGrid mode="wide">
+      <CssColumn sm={2} md={4} lg={8}>mode: wide</CssColumn>
+      <CssColumn sm={2} md={4} lg={8}>mode: wide</CssColumn>
+    </CssGrid>
+  </CssColumn>
+  <CssColumn sm={4} md={8} lg={16}>
+    <CssGrid mode="narrow">
+      <CssColumn sm={2} md={4} lg={8}>mode: narrow</CssColumn>
+      <CssColumn sm={2} md={4} lg={8}>mode: narrow</CssColumn>
+    </CssGrid>
+  </CssColumn>
+  <CssColumn sm={4} md={8} lg={16}>
+    <CssGrid mode="condensed">
+      <CssColumn sm={2} md={4} lg={8}>mode: condensed</CssColumn>
+      <CssColumn sm={2} md={4} lg={8}>mode: condensed</CssColumn>
+    </CssGrid>
+  </CssColumn>
+</CssGrid>
