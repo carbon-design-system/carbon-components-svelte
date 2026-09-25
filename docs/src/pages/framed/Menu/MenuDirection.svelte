faa495da6dd7cc6b80d5a1031d5e99306d895a1b
Eric Liu <ericyl.us@gmail.com>
2026-09-25 15:52:27 -0700

    docs(box): add single-side margin example

--- docs/src/pages/framed/Menu/MenuDirection.svelte
+++ docs/src/pages/framed/Menu/MenuDirection.svelte
@@ -1,13 +1,13 @@
 <script>
-  import { Button, Menu, MenuItem } from "carbon-components-svelte";
+  import { Box, Button, Menu, MenuItem } from "carbon-components-svelte";
 
   let anchor;
   let open = false;
 </script>
 
-<div style="margin-top: 220px;">
+<Box marginTop="220px">
   <Button bind:ref={anchor} on:click={() => (open = !open)}>Actions</Button>
-</div>
+</Box>
 
 <Menu {anchor} direction="top" bind:open labelText="Actions menu">
   <MenuItem on:click={() => console.log("Cut")}>Cut</MenuItem>
