bd87340753a49cffcede59512d2f13a385be30b8
Eric Liu <ericyl.us@gmail.com>
2026-09-25 03:57:18 -0700

    feat(box): add left and right position offsets
    top and bottom already covered two of the four edges a position:
    relative/sticky box can offset from, using the shared 0-13 layout
    scale. left and right round it out for sidebars and horizontal
    insets, reusing the existing offsetClass/offsetStyle helpers.

--- tests/Box/Box.height.test.svelte
+++ tests/Box/Box.height.test.svelte
@@ -8,3 +8,7 @@
 <Box height={7} data-testid="scale-height">Scale height</Box>
 <Box height="24rem" data-testid="custom-height">Custom height</Box>
 <Box position="sticky" top={0} data-testid="sticky-top">Sticky top</Box>
+<Box position="sticky" left={0} data-testid="sticky-left">Sticky left</Box>
+<Box position="relative" right="2rem" data-testid="relative-right"
+  >Relative right</Box
+>
