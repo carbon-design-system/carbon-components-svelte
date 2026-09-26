0c28ff32b129b76a72c8455816c0030d97d8512f
Eric Liu <ericyl.us@gmail.com>
2026-09-25 21:19:51 -0700

    docs(search-menu): add "Max height" example

---
+++ docs/src/pages/framed/SearchMenu/MenuMaxHeight.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { SearchMenu, SearchMenuItem } from "carbon-components-svelte";
+
+  const services = [
+    "Databases for PostgreSQL",
+    "Databases for MongoDB",
+    "Databases for Redis",
+    "Databases for Elasticsearch",
+    "Db2 on Cloud",
+    "Data Store for Memcache",
+    "Cloud Object Storage",
+    "Watson Machine Learning",
+    "Key Protect",
+    "Secrets Manager",
+    "Certificate Manager",
+    "Activity Tracker",
+  ];
+</script>
+
+<SearchMenu menuMaxHeight={200} labelText="Search" placeholder="Search...">
+  {#each services as service (service)}
+    <SearchMenuItem text={service} />
+  {/each}
+</SearchMenu>
